969aeb141a0655bd0151c6af632dde377b8f135d
irof <irof@hogedriven.net>
2018-02-18 00:45:21 +0900

    fix: でっかいファイルを出せるようにする

--- application/web/src/main/java/jig/Application.java
+++ application/web/src/main/java/jig/Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
+        System.setProperty("PLANTUML_LIMIT_SIZE", "65536");
         SpringApplication.run(Application.class, args);
     }
 }

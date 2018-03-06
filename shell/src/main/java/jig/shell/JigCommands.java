89f7beb8b6e5fabed99e1485e2383e92a712be48
irof <irof@hogedriven.net>
2018-03-06 10:07:29 +0900

    feat: シェルプロジェクトの作成

---
+++ shell/src/main/java/jig/shell/JigCommands.java
@@ -0,0 +1,12 @@
+package jig.shell;
+
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+
+@ShellComponent
+public class JigCommands {
+
+    @ShellMethod("環境テスト用")
+    public void test() {
+    }
+}

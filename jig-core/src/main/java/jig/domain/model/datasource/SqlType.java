beb70afacf8820ae9f710a8884e5983f4c1f50fc
irof <irof@hogedriven.net>
2018-03-20 01:41:56 +0900

    feat: postgresqlのシーケンスのSELECTに対応

--- jig-core/src/main/java/jig/domain/model/datasource/SqlType.java
+++ jig-core/src/main/java/jig/domain/model/datasource/SqlType.java
@@ -1,29 +1,37 @@
 package jig.domain.model.datasource;
 
+import java.util.List;
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
 
 public enum SqlType {
     INSERT("insert +into +([^\\s(]+).+"),
-    SELECT("select.+ from +([^\\s(]+)\\b.*"),
+    SELECT("select.+ from +([^\\s(]+)\\b.*", "select +(nextval\\('.+'\\)).*"),
     UPDATE("update +([^\\s(]+) .+"),
     DELETE("delete +from +([^\\s(]+)\\b.*");
 
     private static final Logger LOGGER = Logger.getLogger(SqlType.class.getName());
-    private final Pattern pattern;
+    private final List<Pattern> patterns;
 
-    SqlType(String regex) {
-        pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+    SqlType(String... patterns) {
+        this.patterns = Stream.of(patterns)
+                .map(pattern -> Pattern.compile(pattern, Pattern.CASE_INSENSITIVE))
+                .collect(toList());
     }
 
     public String extractTable(String sql) {
-        Matcher matcher = pattern.matcher(sql.replaceAll("\n", " "));
-        if (matcher.matches()) {
-            return matcher.group(1);
+        for (Pattern pattern : patterns) {
+            Matcher matcher = pattern.matcher(sql.replaceAll("\n", " "));
+            if (matcher.matches()) {
+                return matcher.group(1);
+            }
         }
 
-        LOGGER.warning("テーブル名が解析できないSQLです。 " + sql);
+        LOGGER.warning("テーブル名が解析できませんでした。 [" + sql + "]");
         return unexpectedTable();
     }
 

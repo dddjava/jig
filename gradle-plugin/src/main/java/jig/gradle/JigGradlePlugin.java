cd67a5bb16c80b844038190c6314e09ab601f806
haljik <seiji.kawakami@sora-works.com>
2018-04-16 13:15:29 +0900

    fix: JigListタスクへの依存タスク設定を一旦止める

--- gradle-plugin/src/main/java/jig/gradle/JigGradlePlugin.java
+++ gradle-plugin/src/main/java/jig/gradle/JigGradlePlugin.java
@@ -2,15 +2,13 @@ package jig.gradle;
 
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
-import org.gradle.api.plugins.JavaPlugin;
 
 public class JigGradlePlugin implements Plugin<Project> {
 
     @Override
     public void apply(Project project) {
         project.getExtensions().create("jigList", JigListExtension.class);
-        JigListTask jigList = project.getTasks().create("jigList", JigListTask.class);
-        jigList.dependsOn(project.getTasks().findByName(JavaPlugin.PROCESS_RESOURCES_TASK_NAME));
+        project.getTasks().create("jigList", JigListTask.class);
     }
 
 }

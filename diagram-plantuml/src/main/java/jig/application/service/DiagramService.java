0f8cafe5167947ade71c72155c1fdfc967cc5915
irof <irof@hogedriven.net>
2018-04-20 02:03:09 +0900

    refactor: メソッドをインライン化

--- diagram-plantuml/src/main/java/jig/application/service/DiagramService.java
+++ diagram-plantuml/src/main/java/jig/application/service/DiagramService.java
@@ -2,10 +2,7 @@ package jig.application.service;
 
 import jig.domain.model.diagram.*;
 import jig.domain.model.relation.dependency.PackageDependencies;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
-
-import java.util.concurrent.CompletableFuture;
 
 @Service
 public class DiagramService {
@@ -20,34 +17,12 @@ public class DiagramService {
         this.diagramConverter = diagramConverter;
     }
 
-    public void generate(DiagramIdentifier identifier) {
+    public Diagram generateFrom(PackageDependencies packageDependencies) {
+        DiagramSource diagramSource = diagramConverter.toDiagramSource(packageDependencies);
+        DiagramIdentifier identifier = repository.registerSource(diagramSource);
         DiagramSource source = repository.getSource(identifier);
         Diagram diagram = maker.make(source);
         repository.register(identifier, diagram);
-    }
-
-    public DiagramIdentifier request(DiagramSource source) {
-        return repository.registerSource(source);
-    }
-
-    @Async
-    public CompletableFuture<DiagramIdentifier> generateAsync(DiagramIdentifier identifier) {
-        generate(identifier);
-        return CompletableFuture.completedFuture(identifier);
-    }
-
-    public Diagram get(DiagramIdentifier identifier) {
         return repository.get(identifier);
-    }
-
-    public DiagramSource toDiagramSource(PackageDependencies things) {
-        return diagramConverter.toDiagramSource(things);
-    }
-
-    public Diagram generateFrom(PackageDependencies packageDependencies) {
-        DiagramSource diagramSource = toDiagramSource(packageDependencies);
-        DiagramIdentifier identifier = request(diagramSource);
-        generate(identifier);
-        return get(identifier);
     }
 }

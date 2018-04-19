1e17d5b7dbbc7bc71c85710695d05d3f67fb874d
irof <irof@hogedriven.net>
2018-04-20 01:34:26 +0900

    refactor: 不要になったクラスと処理を削除

--- jig-core/src/main/java/jig/domain/model/relation/dependency/PackageDependencies.java
+++ jig-core/src/main/java/jig/domain/model/relation/dependency/PackageDependencies.java
@@ -3,9 +3,7 @@ package jig.domain.model.relation.dependency;
 import jig.domain.model.identifier.namespace.PackageDepth;
 import jig.domain.model.identifier.namespace.PackageIdentifiers;
 
-import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
 
@@ -13,20 +11,6 @@ public class PackageDependencies {
 
     PackageIdentifiers packages;
     List<PackageDependency> dependencies;
-
-    // TODO jdepsがいらなくなったら不要になる
-    public PackageDependencies(List<PackageDependency> dependencies) {
-        this.dependencies = dependencies;
-        // クラス名昇順、メソッド名昇順
-        dependencies.sort(Comparator.<PackageDependency, String>comparing(relation -> relation.from().value())
-                .thenComparing(relation -> relation.to().value()));
-
-        packages = new PackageIdentifiers(Stream.concat(
-                list().stream().map(PackageDependency::from),
-                list().stream().map(PackageDependency::to))
-                .distinct()
-                .collect(toList()));
-    }
 
     public PackageDependencies(List<PackageDependency> dependencies, PackageIdentifiers packages) {
         this.dependencies = dependencies;
@@ -49,11 +33,6 @@ public class PackageDependencies {
 
     public PackageIdentifiers allPackages() {
         return packages;
-    }
-
-    // TODO jdepsがいらなくなったら不要になる
-    public PackageDependencies withAllPackage(PackageIdentifiers allPackages) {
-        return new PackageDependencies(this.dependencies, allPackages);
     }
 
     public DependencyNumber number() {

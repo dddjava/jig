e34327b024dbdec296f440482173af061bea443f
irof <irof@hogedriven.net>
2018-04-22 01:58:47 +0900

    refactor: コメントなしを登録しない

--- jig-core/src/main/java/jig/infrastructure/javaparser/PackageInfoReader.java
+++ jig-core/src/main/java/jig/infrastructure/javaparser/PackageInfoReader.java
@@ -4,7 +4,8 @@ import com.github.javaparser.JavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.comments.JavadocComment;
 import com.github.javaparser.ast.nodeTypes.NodeWithName;
-import com.github.javaparser.ast.visitor.GenericVisitorAdapter;
+import com.github.javaparser.javadoc.Javadoc;
+import com.github.javaparser.javadoc.description.JavadocDescription;
 import jig.domain.model.identifier.namespace.PackageIdentifier;
 import jig.domain.model.japanese.JapaneseName;
 import jig.domain.model.japanese.PackageJapaneseName;
@@ -19,22 +20,28 @@ class PackageInfoReader {
     Optional<PackageJapaneseName> execute(Path path) {
         try {
             CompilationUnit cu = JavaParser.parse(path);
-            return cu.getPackageDeclaration()
-                    .map(NodeWithName::getNameAsString)
-                    .map(PackageIdentifier::new)
-                    .map(packageIdentifier -> {
-                        JapaneseName japaneseName = cu.accept(new GenericVisitorAdapter<JapaneseName, Void>() {
-                            @Override
-                            public JapaneseName visit(JavadocComment n, Void arg) {
-                                String text = n.parse().getDescription().toText();
-                                return new JapaneseName(text);
-                            }
-                        }, null);
 
-                        return new PackageJapaneseName(packageIdentifier, japaneseName);
-                    });
+            Optional<PackageIdentifier> optPackageIdentifier = cu.getPackageDeclaration()
+                    .map(NodeWithName::getNameAsString)
+                    .map(PackageIdentifier::new);
+
+            Optional<JapaneseName> optJapaneseName = getJavadoc(cu)
+                    .map(Javadoc::getDescription)
+                    .map(JavadocDescription::toText)
+                    .map(JapaneseName::new);
+
+            return optPackageIdentifier.flatMap(packageIdentifier -> optJapaneseName.map(japaneseName ->
+                    new PackageJapaneseName(packageIdentifier, japaneseName)));
         } catch (IOException e) {
             throw new UncheckedIOException(e);
         }
+    }
+
+    private Optional<Javadoc> getJavadoc(CompilationUnit cu) {
+        // NodeWithJavadoc#getJavadocでやってることと同じことをする
+        return cu.getComment()
+                .filter(comment -> comment instanceof JavadocComment)
+                .map(comment -> (JavadocComment) comment)
+                .map(JavadocComment::parse);
     }
 }

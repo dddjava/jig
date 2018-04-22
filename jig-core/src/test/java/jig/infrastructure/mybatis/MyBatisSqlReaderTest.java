cf271f110fd337d62ddddb93b2c823fed065bb4f
irof <irof@hogedriven.net>
2018-04-22 15:13:57 +0900

    refactor: Repositoryを使う必要がないので除去

--- jig-core/src/test/java/jig/infrastructure/mybatis/MyBatisSqlReaderTest.java
+++ jig-core/src/test/java/jig/infrastructure/mybatis/MyBatisSqlReaderTest.java
@@ -1,7 +1,6 @@
 package jig.infrastructure.mybatis;
 
 import jig.domain.model.datasource.*;
-import jig.infrastructure.onmemoryrepository.OnMemorySqlRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -28,14 +27,15 @@ class MyBatisSqlReaderTest {
     @ParameterizedTest
     @MethodSource
     void 標準的なパターン(String methodName, String tableName, SqlType sqlType) {
-        SqlRepository repository = new OnMemorySqlRepository();
         MyBatisSqlReader sut = new MyBatisSqlReader();
 
         Sqls sqls = sut.readFrom(new SqlSources(TestSupport.getTestResourceRootURLs(), Collections.singletonList("stub.infrastructure.datasource.CanonicalMapper")));
-        repository.register(sqls);
 
-        SqlIdentifier sqlIdentifier = new SqlIdentifier("stub.infrastructure.datasource.CanonicalMapper." + methodName);
-        Sql sql = repository.get(sqlIdentifier);
+        Sql sql = sqls.list().stream()
+                .filter(current -> current.identifier().equals(new SqlIdentifier("stub.infrastructure.datasource.CanonicalMapper." + methodName)))
+                .findFirst()
+                .orElseThrow(AssertionError::new);
+
         assertThat(sql.tables().asText()).isEqualTo("[" + tableName + "]");
         assertThat(sql.sqlType()).isEqualTo(sqlType);
     }

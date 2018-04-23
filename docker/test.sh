611a603c926918a9867927a2cdc53e153c6e2c9d
irof <irof@hogedriven.net>
2018-04-23 16:17:41 +0900

    chore: ダイアグラム出力で使用する日本語フォントをインストールする

--- docker/test.sh
+++ docker/test.sh
@@ -2,6 +2,6 @@
 
 git clone https://github.com/irof/Jig.git
 cd Jig
-./gradlew test
-
+./gradlew build
+java -jar package-diagram-cli/build/libs/package-diagram-cli.jar --output.diagram.name=package-diagram.png
 

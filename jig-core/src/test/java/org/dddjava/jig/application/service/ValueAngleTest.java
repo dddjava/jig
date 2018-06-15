ef2194acb67af6fc083f0b9c7f5080dcffcef541
irof <irof@hogedriven.net>
2018-06-15 15:45:44 +0900

    refactor: ValueAngleのテストになってたので分離

---
+++ jig-core/src/test/java/org/dddjava/jig/application/service/ValueAngleTest.java
@@ -0,0 +1,84 @@
+package org.dddjava.jig.application.service;
+
+import org.dddjava.jig.domain.model.declaration.type.TypeIdentifier;
+import org.dddjava.jig.domain.model.implementation.ProjectData;
+import org.dddjava.jig.domain.model.values.ValueAngle;
+import org.dddjava.jig.domain.model.values.ValueAngles;
+import org.dddjava.jig.domain.model.values.ValueKind;
+import org.dddjava.jig.infrastructure.Layout;
+import org.dddjava.jig.infrastructure.LocalProject;
+import org.dddjava.jig.infrastructure.PropertyCharacterizedTypeFactory;
+import org.dddjava.jig.infrastructure.asm.AsmByteCodeFactory;
+import org.dddjava.jig.infrastructure.javaparser.JavaparserJapaneseReader;
+import org.dddjava.jig.infrastructure.mybatis.MyBatisSqlReader;
+import org.dddjava.jig.infrastructure.onmemoryrepository.OnMemoryJapaneseNameRepository;
+import org.junit.jupiter.api.Test;
+import stub.domain.model.type.*;
+import stub.domain.model.type.fuga.FugaIdentifier;
+import stub.domain.model.type.fuga.FugaName;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class ValueAngleTest {
+
+    // テストのためにSpringを起動したくないので自分でインスタンス生成する
+    ImplementationService implementationService = new ImplementationService(
+            new AsmByteCodeFactory(),
+            new GlossaryService(
+                    new JavaparserJapaneseReader(),
+                    new OnMemoryJapaneseNameRepository()
+            ),
+            new MyBatisSqlReader(),
+            new PropertyCharacterizedTypeFactory()
+    );
+
+    AngleService angleService = new AngleService();
+
+    @Test
+    void readProjectData() throws URISyntaxException {
+        // テストクラスのルートを取得する
+        URI location = ValueAngleTest.class.getProtectionDomain().getCodeSource().getLocation().toURI();
+
+        Layout layoutMock = mock(Layout.class);
+        when(layoutMock.extractClassPath()).thenReturn(new Path[]{Paths.get(location)});
+        when(layoutMock.extractSourcePath()).thenReturn(new Path[0]);
+
+        LocalProject localProject = new LocalProject(layoutMock);
+        ProjectData projectData = implementationService.readProjectData(localProject);
+
+        ValueAngles identifiers = angleService.valueAngles(ValueKind.IDENTIFIER, projectData);
+        assertThat(identifiers.list()).extracting(ValueAngle::typeIdentifier).contains(
+                new TypeIdentifier(SimpleIdentifier.class),
+                new TypeIdentifier(FugaIdentifier.class),
+                new TypeIdentifier(FugaName.class)
+        );
+
+        ValueAngles numbers = angleService.valueAngles(ValueKind.NUMBER, projectData);
+        assertThat(numbers.list()).extracting(ValueAngle::typeIdentifier).contains(
+                new TypeIdentifier(SimpleNumber.class)
+        );
+
+        ValueAngles dates = angleService.valueAngles(ValueKind.DATE, projectData);
+        assertThat(dates.list()).extracting(ValueAngle::typeIdentifier).contains(
+                new TypeIdentifier(SimpleDate.class)
+        );
+
+        ValueAngles terms = angleService.valueAngles(ValueKind.TERM, projectData);
+        assertThat(terms.list()).extracting(ValueAngle::typeIdentifier).contains(
+                new TypeIdentifier(SimpleTerm.class)
+        );
+
+        ValueAngles collections = angleService.valueAngles(ValueKind.COLLECTION, projectData);
+        assertThat(collections.list()).extracting(ValueAngle::typeIdentifier).contains(
+                new TypeIdentifier(SimpleCollection.class),
+                new TypeIdentifier(SetCollection.class)
+        );
+    }
+}

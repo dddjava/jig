8ec429f819e0924265d83bd60c5bea1d4ecd2134
irof <irof@hogedriven.net>
2018-06-17 00:18:52 +0900

    refactor: メソッドのテストが膨らむので隔離

--- jig-core/src/test/java/org/dddjava/jig/infrastructure/asm/AsmByteCodeReaderTest.java
+++ jig-core/src/test/java/org/dddjava/jig/infrastructure/asm/AsmByteCodeReaderTest.java
@@ -14,14 +14,12 @@ import stub.domain.model.kind.*;
 import stub.domain.model.relation.ClassDefinition;
 import stub.domain.model.relation.EnumDefinition;
 import stub.domain.model.relation.FieldDefinition;
-import stub.domain.model.relation.MethodInstruction;
 import stub.domain.model.relation.annotation.VariableAnnotation;
 import stub.domain.model.relation.clz.*;
 import stub.domain.model.relation.enumeration.ClassReference;
 import stub.domain.model.relation.enumeration.ConstructorArgument;
 import stub.domain.model.relation.enumeration.EnumField;
 import stub.domain.model.relation.field.*;
-import stub.domain.model.relation.method.*;
 import stub.misc.DecisionClass;
 
 import java.net.URISyntaxException;
@@ -97,72 +95,6 @@ public class AsmByteCodeReaderTest {
                         new TypeIdentifier(ArrayField.class),
                         new TypeIdentifier(ReferenceConstantOwnerAtFieldDefinition.class),
                         new TypeIdentifier(ReferenceConstantAtFieldDefinition.class)
-                );
-    }
-
-    @Test
-    void メソッドで使用している型が取得できる() throws Exception {
-        ByteCode actual = exercise(MethodInstruction.class);
-
-        TypeIdentifiers identifiers = actual.useTypes();
-        assertThat(identifiers.list())
-                .containsExactlyInAnyOrder(
-                        // 標準
-                        new TypeIdentifier(Object.class),
-                        new TypeIdentifier(String.class),
-                        new TypeIdentifier(List.class),
-                        new TypeIdentifier(Stream.class),
-                        new TypeIdentifier("void"),
-                        new TypeIdentifier(Exception.class),
-                        // 自身への参照（コンストラクタ？）
-                        new TypeIdentifier(MethodInstruction.class),
-                        // メソッド定義
-                        new TypeIdentifier(MethodAnnotation.class),
-                        new TypeIdentifier(MethodArgument.class),
-                        new TypeIdentifier(MethodReturn.class),
-                        new TypeIdentifier(ArgumentGenericsParameter.class),
-                        new TypeIdentifier(CheckedException.class),
-                        // メソッド内部
-                        new TypeIdentifier(InstructionField.class),
-                        new TypeIdentifier(UsedInstructionMethodReturn.class),
-                        // TODO メソッドから戻ってくるだけの型は「使用している」から除外すべきかも
-                        new TypeIdentifier(UnusedInstructionMethodReturn.class),
-                        new TypeIdentifier(Instantiation.class),
-                        new TypeIdentifier(ReferenceConstantOwnerInMethod.class),
-                        new TypeIdentifier(ReferenceConstantInMethod.class),
-                        new TypeIdentifier(UseInLambda.class),
-                        new TypeIdentifier(MethodReference.class),
-                        new TypeIdentifier(UncheckedExceptionA.class),
-                        new TypeIdentifier(EnclosedClass.NestedClass.class)
-                )
-                .doesNotContain(
-                        // ローカル変数宣言だけで使用されている型は取得できない（コンパイルされたら消える）
-                        new TypeIdentifier(LocalValue.class),
-                        // ネストされた型のエンクローズド型は名前空間を提供しているだけなので取得できない
-                        new TypeIdentifier(EnclosedClass.class),
-                        // 呼び出し先のメソッドで宣言されているだけの例外
-                        // throwsなどにも登場しなければ検出されない
-                        new TypeIdentifier(CheckedExceptionB.class),
-                        new TypeIdentifier(UncheckedExceptionB.class)
-                );
-    }
-
-    @Test
-    void メソッドの使用しているメソッドが取得できる() throws Exception {
-        ByteCode actual = exercise(MethodInstruction.class);
-
-        assertThat(actual.instanceMethodByteCodes())
-                .extracting(
-                        methodByteCode -> methodByteCode.methodDeclaration.asSignatureSimpleText(),
-                        methodByteCode -> methodByteCode.usingMethods().asSimpleText()
-                )
-                .filteredOn(tuple -> {
-                    Object methodDeclaration = tuple.toArray()[0];
-                    return methodDeclaration.equals("method(MethodArgument)") || methodDeclaration.equals("lambda()");
-                })
-                .containsExactlyInAnyOrder(
-                        tuple("method(MethodArgument)", "[InstructionField.invokeMethod(), UsedInstructionMethodReturn.chainedInvokeMethod()]"),
-                        tuple("lambda()", "[MethodInstruction.lambda$lambda$0(Object), Stream.empty(), Stream.forEach(Consumer)]")
                 );
     }
 
---
+++ jig-core/src/test/java/org/dddjava/jig/infrastructure/asm/MethodInstructionTest.java
@@ -0,0 +1,94 @@
+package org.dddjava.jig.infrastructure.asm;
+
+import org.dddjava.jig.domain.model.declaration.type.TypeIdentifier;
+import org.dddjava.jig.domain.model.declaration.type.TypeIdentifiers;
+import org.dddjava.jig.domain.model.implementation.bytecode.ByteCode;
+import org.dddjava.jig.domain.model.implementation.bytecode.ByteCodeSource;
+import org.junit.jupiter.api.Test;
+import stub.domain.model.relation.MethodInstruction;
+import stub.domain.model.relation.method.*;
+
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
+
+public class MethodInstructionTest {
+
+    @Test
+    void メソッドで使用している型が取得できる() throws Exception {
+        ByteCode actual = exercise(MethodInstruction.class);
+
+        TypeIdentifiers identifiers = actual.useTypes();
+        assertThat(identifiers.list())
+                .containsExactlyInAnyOrder(
+                        // 標準
+                        new TypeIdentifier(Object.class),
+                        new TypeIdentifier(String.class),
+                        new TypeIdentifier(List.class),
+                        new TypeIdentifier(Stream.class),
+                        new TypeIdentifier("void"),
+                        new TypeIdentifier(Exception.class),
+                        // 自身への参照（コンストラクタ？）
+                        new TypeIdentifier(MethodInstruction.class),
+                        // メソッド定義
+                        new TypeIdentifier(MethodAnnotation.class),
+                        new TypeIdentifier(MethodArgument.class),
+                        new TypeIdentifier(MethodReturn.class),
+                        new TypeIdentifier(ArgumentGenericsParameter.class),
+                        new TypeIdentifier(CheckedException.class),
+                        // メソッド内部
+                        new TypeIdentifier(InstructionField.class),
+                        new TypeIdentifier(UsedInstructionMethodReturn.class),
+                        // TODO メソッドから戻ってくるだけの型は「使用している」から除外すべきかも
+                        new TypeIdentifier(UnusedInstructionMethodReturn.class),
+                        new TypeIdentifier(Instantiation.class),
+                        new TypeIdentifier(ReferenceConstantOwnerInMethod.class),
+                        new TypeIdentifier(ReferenceConstantInMethod.class),
+                        new TypeIdentifier(UseInLambda.class),
+                        new TypeIdentifier(MethodReference.class),
+                        new TypeIdentifier(UncheckedExceptionA.class),
+                        new TypeIdentifier(EnclosedClass.NestedClass.class)
+                )
+                .doesNotContain(
+                        // ローカル変数宣言だけで使用されている型は取得できない（コンパイルされたら消える）
+                        new TypeIdentifier(LocalValue.class),
+                        // ネストされた型のエンクローズド型は名前空間を提供しているだけなので取得できない
+                        new TypeIdentifier(EnclosedClass.class),
+                        // 呼び出し先のメソッドで宣言されているだけの例外
+                        // throwsなどにも登場しなければ検出されない
+                        new TypeIdentifier(CheckedExceptionB.class),
+                        new TypeIdentifier(UncheckedExceptionB.class)
+                );
+    }
+
+    @Test
+    void メソッドの使用しているメソッドが取得できる() throws Exception {
+        ByteCode actual = exercise(MethodInstruction.class);
+
+        assertThat(actual.instanceMethodByteCodes())
+                .extracting(
+                        methodByteCode -> methodByteCode.methodDeclaration.asSignatureSimpleText(),
+                        methodByteCode -> methodByteCode.usingMethods().asSimpleText()
+                )
+                .filteredOn(tuple -> {
+                    Object methodDeclaration = tuple.toArray()[0];
+                    return methodDeclaration.equals("method(MethodArgument)") || methodDeclaration.equals("lambda()");
+                })
+                .containsExactlyInAnyOrder(
+                        tuple("method(MethodArgument)", "[InstructionField.invokeMethod(), UsedInstructionMethodReturn.chainedInvokeMethod()]"),
+                        tuple("lambda()", "[MethodInstruction.lambda$lambda$0(Object), Stream.empty(), Stream.forEach(Consumer)]")
+                );
+    }
+
+    private ByteCode exercise(Class<?> definitionClass) throws URISyntaxException {
+        Path path = Paths.get(definitionClass.getResource(definitionClass.getSimpleName().concat(".class")).toURI());
+
+        AsmByteCodeFactory sut = new AsmByteCodeFactory();
+        return sut.analyze(new ByteCodeSource(path));
+    }
+}

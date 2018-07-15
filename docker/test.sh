b2b190b6fae9d6ad26a3b4b3b6919afbde6d8dcc
irof <irof@hogedriven.net>
2018-07-15 23:31:56 +0900

    chore: dockerでのテストはcircleciでできているので

--- docker/test.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-git clone https://github.com/irof/Jig.git
-cd Jig
-./gradlew build
-java -jar package-diagram-cli/build/libs/package-diagram-cli.jar --output.diagram.name=package-diagram.png
-

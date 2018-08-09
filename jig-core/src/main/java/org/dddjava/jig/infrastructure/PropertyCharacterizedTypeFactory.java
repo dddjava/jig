f6d30cf8f50793b64fd2567b8c6bbb5af799d899
haljik <seiji.kawakami@sora-works.com>
2018-08-09 18:00:08 +0900

    prepare: モデル検出とリポジトリ検出のパターン指定をコンストラクタインジェクションに変更
    #140

--- jig-core/src/main/java/org/dddjava/jig/infrastructure/PropertyCharacterizedTypeFactory.java
+++ jig-core/src/main/java/org/dddjava/jig/infrastructure/PropertyCharacterizedTypeFactory.java
@@ -2,17 +2,28 @@ package org.dddjava.jig.infrastructure;
 
 import org.dddjava.jig.domain.model.characteristic.CharacterizedTypeFactory;
 import org.dddjava.jig.domain.model.implementation.bytecode.TypeByteCode;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PropertyCharacterizedTypeFactory implements CharacterizedTypeFactory {
 
-    @Value("${jig.model.pattern:.+\\.domain\\.model\\..+}")
-    String modelPattern = ".+\\.domain\\.model\\..+";
+    String modelPattern;
 
-    @Value("${jig.repository.pattern:.+Repository}")
-    String repositoryPattern = ".+Repository";
+    String repositoryPattern;
+
+    public PropertyCharacterizedTypeFactory() {
+        this(".+\\.domain\\.model\\..+", ".+Repository");
+    }
+
+    @Autowired
+    public PropertyCharacterizedTypeFactory(@Value("${jig.model.pattern:.+\\.domain\\.model\\..+}") String modelPattern,
+                                            @Value("${jig.repository.pattern:.+Repository}") String repositoryPattern) {
+        this.modelPattern = modelPattern;
+        this.repositoryPattern = repositoryPattern;
+    }
+
 
     @Override
     public boolean isModel(TypeByteCode typeByteCode) {

817b82707f0e9ae3145283882c4ddb78638e551f
haljik <seiji.kawakami@sora-works.com>
2018-08-14 23:26:43 +0900

    prepare: CharacterizedTypeFactoryの実装でModelPattern/RepositoryPatternを利用するように

--- jig-core/src/main/java/org/dddjava/jig/infrastructure/PropertyCharacterizedTypeFactory.java
+++ jig-core/src/main/java/org/dddjava/jig/infrastructure/PropertyCharacterizedTypeFactory.java
@@ -2,6 +2,8 @@ package org.dddjava.jig.infrastructure;
 
 import org.dddjava.jig.domain.model.characteristic.CharacterizedTypeFactory;
 import org.dddjava.jig.domain.model.implementation.bytecode.TypeByteCode;
+import org.dddjava.jig.infrastructure.configuration.ModelPattern;
+import org.dddjava.jig.infrastructure.configuration.RepositoryPattern;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -9,29 +11,33 @@ import org.springframework.stereotype.Component;
 @Component
 public class PropertyCharacterizedTypeFactory implements CharacterizedTypeFactory {
 
-    String modelPattern;
+    ModelPattern modelPattern;
 
-    String repositoryPattern;
+    RepositoryPattern repositoryPattern;
 
     public PropertyCharacterizedTypeFactory() {
-        this(".+\\.domain\\.model\\..+", ".+Repository");
+        this(new ModelPattern(), new RepositoryPattern());
     }
 
     @Autowired
     public PropertyCharacterizedTypeFactory(@Value("${jig.model.pattern:.+\\.domain\\.model\\..+}") String modelPattern,
                                             @Value("${jig.repository.pattern:.+Repository}") String repositoryPattern) {
+        this.modelPattern = new ModelPattern(modelPattern);
+        this.repositoryPattern = new RepositoryPattern(repositoryPattern);
+    }
+
+    public PropertyCharacterizedTypeFactory(ModelPattern modelPattern, RepositoryPattern repositoryPattern) {
         this.modelPattern = modelPattern;
         this.repositoryPattern = repositoryPattern;
     }
 
-
     @Override
     public boolean isModel(TypeByteCode typeByteCode) {
-        return typeByteCode.typeIdentifier().fullQualifiedName().matches(modelPattern);
+        return modelPattern.matches(typeByteCode);
     }
 
     @Override
     public boolean isRepository(TypeByteCode typeByteCode) {
-        return typeByteCode.typeIdentifier().fullQualifiedName().matches(repositoryPattern);
+        return repositoryPattern.matches(typeByteCode);
     }
 }

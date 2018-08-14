3d0912db6edc69dbaeadbbc025578e2ad65a2cfe
haljik <seiji.kawakami@sora-works.com>
2018-08-14 23:31:19 +0900

    prepare: IdentifierFormatterのフィールド型をOutputOmitPrefixに

--- jig-core/src/main/java/org/dddjava/jig/infrastructure/PrefixRemoveIdentifierFormatter.java
+++ jig-core/src/main/java/org/dddjava/jig/infrastructure/PrefixRemoveIdentifierFormatter.java
@@ -2,20 +2,27 @@ package org.dddjava.jig.infrastructure;
 
 import org.dddjava.jig.domain.model.declaration.namespace.PackageIdentifierFormatter;
 import org.dddjava.jig.domain.model.declaration.type.TypeIdentifierFormatter;
+import org.dddjava.jig.infrastructure.configuration.OutputOmitPrefix;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PrefixRemoveIdentifierFormatter implements TypeIdentifierFormatter, PackageIdentifierFormatter {
 
-    private final String prefixPattern;
+    private final OutputOmitPrefix prefixPattern;
 
+    @Autowired
     public PrefixRemoveIdentifierFormatter(@Value("${output.omit.prefix:}") String prefixPattern) {
+        this(new OutputOmitPrefix(prefixPattern));
+    }
+
+    public PrefixRemoveIdentifierFormatter(OutputOmitPrefix prefixPattern) {
         this.prefixPattern = prefixPattern;
     }
 
     @Override
     public String format(String fullQualifiedName) {
-        return fullQualifiedName.replaceFirst(prefixPattern, "");
+        return prefixPattern.format(fullQualifiedName);
     }
 }

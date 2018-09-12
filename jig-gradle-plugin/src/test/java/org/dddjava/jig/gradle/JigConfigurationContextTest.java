3e8e93f1ee8400f93a7bb6e78907d4c49619ac54
haljik <seiji.kawakami@sora-works.com>
2018-09-12 12:23:04 +0900

    test: GradlePluginのクラスファイルが存在しない場合の警告メッセージ生成のエラーを再現
    #151

---
+++ jig-gradle-plugin/src/test/java/org/dddjava/jig/gradle/JigConfigurationContextTest.java
@@ -0,0 +1,21 @@
+package org.dddjava.jig.gradle;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class JigConfigurationContextTest {
+
+    @Test
+    void モデル検出時の警告メッセージに設定で指定したモデルパターンが含まれること() {
+        String settingTo = ".+\\.domain_model\\..+";
+        JigConfig config = new JigConfig();
+        config.setModelPattern(settingTo);
+        JigConfigurationContext sut = new JigConfigurationContext(config);
+
+        String message = sut.modelDetectionWarningMessage();
+
+        assertThat(message).contains(String.format("modelPattern=%s", settingTo));
+    }
+
+}

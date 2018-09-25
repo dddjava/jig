361cd518647879c6a040396941f659b550aba034
irof <irof@hogedriven.net>
2018-09-25 21:41:01 +0900

    prepare: Progress以外の出力準備 refs #178

--- jig-annotation/src/main/java/org/dddjava/jig/annotation/JigNotes.java
+++ jig-annotation/src/main/java/org/dddjava/jig/annotation/JigNotes.java
@@ -2,13 +2,9 @@ package org.dddjava.jig.annotation;
 
 import java.lang.annotation.*;
 
-/**
- * 進捗状況
- */
 @Documented
 @Target({ElementType.TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.CLASS)
-@JigNote(name = "進捗")
-public @interface Progress {
-    String value();
+public @interface JigNotes {
+    JigNote[] value();
 }

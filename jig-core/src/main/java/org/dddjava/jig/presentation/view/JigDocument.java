a45896a663b09789576429a40de37e84801d8808
irof <irof@hogedriven.net>
2018-10-21 16:53:41 +0900

    docs: 用途を記述

--- jig-core/src/main/java/org/dddjava/jig/presentation/view/JigDocument.java
+++ jig-core/src/main/java/org/dddjava/jig/presentation/view/JigDocument.java
@@ -17,11 +17,11 @@ public enum JigDocument {
     ServiceMethodCallHierarchyDiagram("service-method-call-hierarchy"),
 
     /**
-     * パッケージ依存ダイアグラム
+     * パッケージ関連ダイアグラム
      *
-     * パッケージ間の依存を可視化する。
-     * トップレベルからの階層(depth)で丸めて複数出力する。
-     * 出力対象パッケージはモデルと判断されるもの。
+     * ビジネスルールのパッケージ関連を可視化する。
+     * トップレベルからの階層(depth)で丸めて複数出力される。
+     * パッケージの関連有無や方向からドメインを語れるかのウォークスルーに使用する。
      */
     PackageRelationDiagram("package-relation"),
 
@@ -29,6 +29,8 @@ public enum JigDocument {
      * ビジネスルール関連ダイアグラム
      *
      * ビジネスルール間の関連を可視化する。
+     * クラス名と依存線のみのクラス図。ある程度以上の規模になると大きくなりすぎて使いづらくなる。
+     * パッケージ関連ダイアグラムで把握できない場合の補助に使用する。
      */
     BusinessRuleRelationDiagram("business-rule-relation"),
 
@@ -36,6 +38,8 @@ public enum JigDocument {
      * 機能一覧
      *
      * 機能を提供するメソッドの一覧。
+     * 三層（プレゼンテーション層、アプリケーション層、データソース層）の情報を提供する。
+     * アプリケーションの状況把握に使用できる。
      *
      * 制限事項: {@link org.dddjava.jig.infrastructure.mybatis.MyBatisSqlReader}
      */
@@ -45,6 +49,7 @@ public enum JigDocument {
      * ビジネスルール一覧
      *
      * ビジネスルールを表すクラスの一覧。
+     * 用語集としてや、ビジネスルールの充足具合の把握などに使用できる。
      */
     BusinessRuleList("business-rule"),
 
@@ -58,21 +63,23 @@ public enum JigDocument {
     /**
      * 区分使用ダイアグラム
      *
-     * enumの使用クラスを可視化する。
+     * 区分を使用しているクラスを可視化する。
      */
     CategoryUsageDiagram("category-usage"),
 
     /**
      * 区分ダイアグラム
      *
-     * enumと値の1枚絵
+     * 区分と区分値を可視化する。
+     * 区分の充実はドメインの把握具合と密接に関わる。
      */
     CategoryDiagram("category"),
 
     /**
      * 真偽値サービス関連ダイアグラム
      *
-     * booleanを返すサービスと使用しているメソッドを可視化する。
+     * 真偽値を返すサービスと使用しているメソッドを可視化する。
+     * 真偽値にビジネスルールが埋もれていないかの検出に使用できる。
      */
     BooleanServiceDiagram("boolean-service");
 

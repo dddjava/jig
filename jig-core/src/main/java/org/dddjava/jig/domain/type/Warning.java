7a95cc3662860e8dc7eb469ec19424882a67c068
irof <irof@hogedriven.net>
2018-12-31 14:57:22 +0900

    fix: リソースキーに日本語無理そうなので修正

--- jig-core/src/main/java/org/dddjava/jig/domain/type/Warning.java
+++ jig-core/src/main/java/org/dddjava/jig/domain/type/Warning.java
@@ -6,19 +6,22 @@ import java.util.ResourceBundle;
  * ユーザーへの警告
  */
 public enum Warning {
-    クラスが見つからないので中断する通知(),
-    ハンドラメソッドが見つからないので出力されない通知(),
-    サービスメソッドが見つからないので出力されない通知(),
-    ビジネスルールが見つからないので出力されない通知(),
-    リポジトリが見つからないので出力されない通知(),
-    SQLが見つからないので出力されない通知(),
+    クラスが見つからないので中断する通知("warning.ClassNotFound"),
+    ハンドラメソッドが見つからないので出力されない通知("warning.RequestHandlerNotFound"),
+    サービスメソッドが見つからないので出力されない通知("warning.ServiceMethodNotFound"),
+    ビジネスルールが見つからないので出力されない通知("warning.BusinessRuleNotFound"),
+    リポジトリが見つからないので出力されない通知("warning.RepositoryNotFound"),
+    SQLが見つからないので出力されない通知("warning.SqlNotFound"),
     ;
 
-    Warning() {
+    String key;
+
+    Warning(String key) {
+        this.key = key;
     }
 
     public String text() {
         ResourceBundle resource = ResourceBundle.getBundle("jig-messages");
-        return resource.getString(name());
+        return resource.getString(key);
     }
 }

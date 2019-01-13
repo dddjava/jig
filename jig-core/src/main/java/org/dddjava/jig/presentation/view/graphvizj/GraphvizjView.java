7605f89d23a04fc3ff25abe106220194b57847ca
irof <irof@hogedriven.net>
2019-01-13 16:07:32 +0900

    feat: graphvizインストールを前提にする

--- jig-core/src/main/java/org/dddjava/jig/presentation/view/graphvizj/GraphvizjView.java
+++ jig-core/src/main/java/org/dddjava/jig/presentation/view/graphvizj/GraphvizjView.java
@@ -1,8 +1,13 @@
 package org.dddjava.jig.presentation.view.graphvizj;
 
 import guru.nidi.graphviz.engine.Graphviz;
+import guru.nidi.graphviz.engine.GraphvizCmdLineEngine;
+import guru.nidi.graphviz.engine.GraphvizException;
 import org.dddjava.jig.presentation.view.JigDocumentWriter;
 import org.dddjava.jig.presentation.view.JigView;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class GraphvizjView<T> implements JigView<T> {
     DotTextEditor<T> editor;
@@ -21,6 +26,26 @@ public class GraphvizjView<T> implements JigView<T> {
             jigDocumentWriter.skip();
             return;
         }
+
+        // コマンドラインのみにする
+        GraphvizCmdLineEngine graphvizCmdLineEngine = new GraphvizCmdLineEngine();
+
+        try {
+            Method doInit = GraphvizCmdLineEngine.class.getDeclaredMethod("doInit");
+            doInit.setAccessible(true);
+            doInit.invoke(graphvizCmdLineEngine);
+        } catch (NoSuchMethodException | IllegalAccessException e) {
+            // バージョンアップなどで内部メソッドが変更された場合に起こる
+            throw new IllegalStateException(e);
+        } catch (InvocationTargetException e) {
+            if (e.getTargetException() instanceof GraphvizException) {
+                throw new IllegalStateException(e.getTargetException().getMessage());
+            }
+            throw new IllegalStateException(e);
+        }
+
+        Graphviz.useEngine(graphvizCmdLineEngine);
+
         for (DotText dot : dotTexts.list()) {
             jigDocumentWriter.writeDebugText(dot.text(), dot.documentSuffix());
             jigDocumentWriter.writeDiagram(

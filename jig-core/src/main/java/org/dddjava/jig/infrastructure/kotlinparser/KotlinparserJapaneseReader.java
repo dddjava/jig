99b610d88b0c057f7a4f4b1478c87d641e0b76a6
orekyuu <orekyuu@gmail.com>
2019-05-22 20:58:05 +0900

    文字コード指定

--- jig-core/src/main/java/org/dddjava/jig/infrastructure/kotlinparser/KotlinparserJapaneseReader.java
+++ jig-core/src/main/java/org/dddjava/jig/infrastructure/kotlinparser/KotlinparserJapaneseReader.java
@@ -25,6 +25,7 @@ import org.jetbrains.kotlin.psi.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -141,7 +142,7 @@ public class KotlinparserJapaneseReader implements SourceCodeParser {
     }
 
     private String parseKotlinFile(KotlinSource source) {
-        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(source.toInputStream()))) {
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(source.toInputStream(), Charset.forName("utf8")))) {
             return bufferedReader.lines().collect(Collectors.joining("\n"));
         } catch (IOException e) {
             e.printStackTrace();

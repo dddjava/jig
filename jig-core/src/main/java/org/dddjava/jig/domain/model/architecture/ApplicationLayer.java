754f6e2d5850507f42b121b9ef1b13d469770360
irof <irof@hogedriven.net>
2019-06-02 17:05:43 +0900

    refactor: ApplicationLayer追加

---
+++ jig-core/src/main/java/org/dddjava/jig/domain/model/architecture/ApplicationLayer.java
@@ -0,0 +1,11 @@
+package org.dddjava.jig.domain.model.architecture;
+
+/**
+ * 機能のレイヤー。
+ * 三層＋ドメインモデルの三層部分を表す。
+ */
+public enum ApplicationLayer {
+    PRESENTATION,
+    APPLICATION,
+    DATASOURCE,
+}

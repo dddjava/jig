0ec14bead3ba71b5cae0d1264cc17bb9ed822fab
irof <irof@hogedriven.net>
2019-06-30 10:40:34 +0900

    fix: 1つでも相互依存があったら全部相互依存に判定されていた

--- jig-core/src/main/java/org/dddjava/jig/domain/model/fact/relation/packages/BidirectionalRelation.java
+++ jig-core/src/main/java/org/dddjava/jig/domain/model/fact/relation/packages/BidirectionalRelation.java
@@ -13,8 +13,8 @@ public class BidirectionalRelation {
     }
 
     public boolean matches(PackageRelation packageRelation) {
-        PackageIdentifier left = packageRelation.from;
-        PackageIdentifier right = packageRelation.to;
+        PackageIdentifier left = this.packageRelation.from;
+        PackageIdentifier right = this.packageRelation.to;
         return (left.equals(packageRelation.from()) && right.equals(packageRelation.to())) ||
                 (left.equals(packageRelation.to()) && right.equals(packageRelation.from()));
     }

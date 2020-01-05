c74f63e055bf593fc95c6b328bd0a90ee889d588
Yoshitaka Fujii <yoshiyoshifujii@gmail.com>
2020-01-05 20:29:09 +0900

    add comment

--- jig-core/src/main/java/org/dddjava/jig/domain/model/jigloaded/alias/ScalaSourceAliasReader.java
+++ jig-core/src/main/java/org/dddjava/jig/domain/model/jigloaded/alias/ScalaSourceAliasReader.java
@@ -2,6 +2,9 @@ package org.dddjava.jig.domain.model.jigloaded.alias;
 
 import org.dddjava.jig.domain.model.jigsource.source.code.scalacode.ScalaSources;
 
+/**
+ * ScalaSourceから別名を読み取る
+ */
 public interface ScalaSourceAliasReader {
 
     TypeAliases readAlias(ScalaSources sources);

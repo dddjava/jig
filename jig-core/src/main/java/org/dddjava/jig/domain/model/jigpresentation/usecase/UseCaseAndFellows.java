df21a477c403808b98588949ab97c02146b23a76
irof <irof.ocean@gmail.com>
2020-03-26 16:05:07 +0900

    ユースケース複合図にControllerを追加

--- jig-core/src/main/java/org/dddjava/jig/domain/model/jigpresentation/usecase/UseCaseAndFellows.java
+++ jig-core/src/main/java/org/dddjava/jig/domain/model/jigpresentation/usecase/UseCaseAndFellows.java
@@ -6,16 +6,23 @@ import org.dddjava.jig.domain.model.jigloaded.alias.TypeAlias;
 import org.dddjava.jig.domain.model.jigmodel.applications.services.ServiceAngle;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * ユースケースと愉快な仲間たち
  */
 public class UseCaseAndFellows {
     UseCase useCase;
+    List<TypeIdentifier> controllerTypes;
 
     UseCaseAndFellows(ServiceAngle serviceAngle) {
-        useCase = new UseCase(serviceAngle.serviceMethod());
+        this.useCase = new UseCase(serviceAngle.serviceMethod());
+        this.controllerTypes = serviceAngle.userControllerMethods().list().stream()
+                .map(methodDeclaration -> methodDeclaration.declaringType())
+                .distinct()
+                .collect(Collectors.toList());
     }
 
     public String dotText(AliasFinder aliasFinder) {
@@ -57,6 +64,16 @@ public class UseCaseAndFellows {
         for (TypeIdentifier otherType : otherTypes) {
             TypeAlias typeAlias = aliasFinder.find(otherType);
             sb.append(String.format("\"%s\"[label=\"%s\"];\n", otherType.fullQualifiedName(), typeAlias.asTextOrDefault(otherType.asSimpleText())));
+        }
+
+        for (TypeIdentifier controllerType : controllerTypes) {
+            // controllerの表示
+            TypeAlias typeAlias = aliasFinder.find(controllerType);
+            sb.append(String.format("\"%s\"[label=\"%s\",style=filled,fillcolor=lightgray,shape=Msquare];\n",
+                    controllerType.fullQualifiedName(), typeAlias.asTextOrDefault(controllerType.asSimpleText())));
+
+            // dotted, headあり
+            sb.append(String.format("\"%s\" -> \"%s\"[style=dashed,arrowhead=open];\n", controllerType.fullQualifiedName(), useCaseIdentifier));
         }
 
         return sb.toString();

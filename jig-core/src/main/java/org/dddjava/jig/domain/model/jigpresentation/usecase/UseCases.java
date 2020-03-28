5de8468e8c1006dca42b63004bcc770c4f1f96db
irof <irof.ocean@gmail.com>
2020-03-28 09:10:15 +0900

    remove

--- jig-core/src/main/java/org/dddjava/jig/domain/model/jigpresentation/usecase/UseCases.java
+++
@@ -1,8 +0,0 @@
-package org.dddjava.jig.domain.model.jigpresentation.usecase;
-
-import org.dddjava.jig.domain.model.jigmodel.applications.services.ServiceMethods;
-
-public class UseCases {
-
-    ServiceMethods serviceMethods;
-}

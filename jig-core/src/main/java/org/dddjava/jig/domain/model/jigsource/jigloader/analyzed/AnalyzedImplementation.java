405789bc08962eee264f31f5b6e9bcf5ad732868
irof <irof.ocean@gmail.com>
2020-06-11 18:01:24 +0900

    interface/classを統合

--- jig-core/src/main/java/org/dddjava/jig/domain/model/jigsource/jigloader/analyzed/AnalyzedImplementation.java
+++ jig-core/src/main/java/org/dddjava/jig/domain/model/jigsource/jigloader/analyzed/AnalyzedImplementation.java
@@ -3,18 +3,46 @@ package org.dddjava.jig.domain.model.jigsource.jigloader.analyzed;
 import org.dddjava.jig.domain.model.jigmodel.lowmodel.rdbaccess.Sqls;
 import org.dddjava.jig.domain.model.jigsource.file.Sources;
 
-/**
- * 解析した実装
- */
-public interface AnalyzedImplementation {
+import java.util.ArrayList;
+import java.util.List;
 
-    static AnalyzedImplementation generate(Sources sources, TypeFacts typeFacts, Sqls sqls) {
-        return new AnalyzedImplementationImpl(sources, typeFacts, sqls);
+public class AnalyzedImplementation {
+
+    Sources sources;
+    TypeFacts typeFacts;
+    Sqls sqls;
+
+    public AnalyzedImplementation(Sources sources, TypeFacts typeFacts, Sqls sqls) {
+        this.sources = sources;
+        this.typeFacts = typeFacts;
+        this.sqls = sqls;
     }
 
-    TypeFacts typeFacts();
+    public TypeFacts typeFacts() {
+        return typeFacts;
+    }
 
-    Sqls sqls();
+    public Sqls sqls() {
+        return sqls;
+    }
 
-    AnalyzeStatuses status();
+    public AnalyzeStatuses status() {
+        List<AnalyzeStatus> list = new ArrayList<>();
+
+        if (sources.nothingBinarySource()) {
+            list.add(AnalyzeStatus.バイナリソースなし);
+        }
+
+        if (sources.nothingTextSource()) {
+            list.add(AnalyzeStatus.テキストソースなし);
+        }
+
+        // binarySourceがあってtypeByteCodesがない（ASMの解析で失敗する）のは現状実行時エラーになるのでここでは考慮しない
+
+        if (sqls.status().not正常()) {
+            list.add(AnalyzeStatus.fromSqlReadStatus(sqls.status()));
+        }
+
+        return new AnalyzeStatuses(list);
+    }
 }

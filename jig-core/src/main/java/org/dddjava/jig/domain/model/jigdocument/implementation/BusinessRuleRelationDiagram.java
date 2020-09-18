8fedef7125175a1f77d07bb49e5ea3344d0dfb6a
irof <irof.ocean@gmail.com>
2020-09-19 00:37:15 +0900

    refactor

--- jig-core/src/main/java/org/dddjava/jig/domain/model/jigdocument/implementation/BusinessRuleRelationDiagram.java
+++ jig-core/src/main/java/org/dddjava/jig/domain/model/jigdocument/implementation/BusinessRuleRelationDiagram.java
@@ -12,7 +12,6 @@ import org.dddjava.jig.domain.model.jigmodel.lowmodel.declaration.package_.Packa
 import org.dddjava.jig.domain.model.jigmodel.lowmodel.declaration.type.TypeIdentifier;
 import org.dddjava.jig.domain.model.jigmodel.lowmodel.declaration.type.TypeIdentifiers;
 import org.dddjava.jig.domain.model.jigmodel.lowmodel.relation.class_.ClassRelation;
-import org.dddjava.jig.domain.model.jigmodel.lowmodel.relation.class_.ClassRelations;
 
 import java.util.List;
 import java.util.Map;
@@ -33,18 +32,24 @@ public class BusinessRuleRelationDiagram {
     }
 
     public DiagramSources relationDotText(JigDocumentContext jigDocumentContext, PackageIdentifierFormatter packageIdentifierFormatter) {
+        return diagramSources(packageIdentifierFormatter, businessRules, jigDocumentContext.documentName(JigDocument.BusinessRuleRelationDiagram));
+    }
 
-        if (businessRules.empty()) {
+    public DiagramSources coreRelationDotText(JigDocumentContext jigDocumentContext, PackageIdentifierFormatter packageIdentifierFormatter) {
+        return diagramSources(packageIdentifierFormatter, businessRules.filterCore(), jigDocumentContext.documentName(JigDocument.CoreBusinessRuleRelationDiagram));
+    }
+
+    private DiagramSources diagramSources(PackageIdentifierFormatter packageIdentifierFormatter, BusinessRules targetBusinessRules, DocumentName documentName) {
+        if (targetBusinessRules.empty()) {
             return DiagramSource.empty();
         }
-        TypeIdentifiers isolatedTypes = businessRules.isolatedTypes();
 
-        DocumentName documentName = jigDocumentContext.documentName(JigDocument.BusinessRuleRelationDiagram);
         StringJoiner graph = new StringJoiner("\n", "digraph \"" + documentName.label() + "\" {", "}")
                 .add("label=\"" + documentName.label() + "\";")
                 .add("node [shape=box,style=filled,fillcolor=lightgoldenrod];");
 
-        BusinessRulePackages businessRulePackages = businessRules.businessRulePackages();
+        TypeIdentifiers isolatedTypes = targetBusinessRules.isolatedTypes();
+        BusinessRulePackages businessRulePackages = targetBusinessRules.businessRulePackages();
         for (BusinessRulePackage businessRulePackage : businessRulePackages.list()) {
             PackageIdentifier packageIdentifier = businessRulePackage.packageIdentifier();
 
@@ -64,7 +69,7 @@ public class BusinessRuleRelationDiagram {
             graph.add(subgraph.toString());
         }
 
-        for (ClassRelation classRelation : businessRules.internalClassRelations().list()) {
+        for (ClassRelation classRelation : targetBusinessRules.internalClassRelations().list()) {
             graph.add(classRelation.dotText());
         }
 
@@ -102,48 +107,6 @@ public class BusinessRuleRelationDiagram {
             for (TypeIdentifier fromTypeIdentifier : entry.getValue().list()) {
                 graph.add(new ClassRelation(fromTypeIdentifier, entry.getKey().typeIdentifier()).dotText());
             }
-        }
-
-        return DiagramSource.createDiagramSource(documentName, graph.toString());
-    }
-
-    public DiagramSources coreRelationDotText(JigDocumentContext jigDocumentContext, PackageIdentifierFormatter packageIdentifierFormatter) {
-        if (businessRules.empty()) {
-            return DiagramSource.empty();
-        }
-
-        TypeIdentifiers isolatedTypes = businessRules.isolatedTypes();
-
-        DocumentName documentName = jigDocumentContext.documentName(JigDocument.CoreBusinessRuleRelationDiagram);
-        StringJoiner graph = new StringJoiner("\n", "digraph \"" + documentName.label() + "\" {", "}")
-                .add("label=\"" + documentName.label() + "\";")
-                .add("node [shape=box,style=filled,fillcolor=lightgoldenrod];");
-
-        BusinessRules coreBusinessRules = businessRules.filterCore();
-
-        BusinessRulePackages businessRulePackages = coreBusinessRules.businessRulePackages();
-        for (BusinessRulePackage businessRulePackage : businessRulePackages.list()) {
-            PackageIdentifier packageIdentifier = businessRulePackage.packageIdentifier();
-
-            Subgraph subgraph = new Subgraph(packageIdentifier.asText())
-                    .label(packageIdentifier.format(packageIdentifierFormatter))
-                    .fillColor("lemonchiffon").color("lightgoldenrod").borderWidth(2);
-
-            BusinessRules businessRules = businessRulePackage.businessRules();
-            for (BusinessRule businessRule : businessRules.list()) {
-                Node node = Node.businessRuleNodeOf(businessRule);
-                if (isolatedTypes.contains(businessRule.typeIdentifier())) {
-                    node.warning();
-                }
-                subgraph.add(node.asText());
-            }
-
-            graph.add(subgraph.toString());
-        }
-
-        ClassRelations classRelations = coreBusinessRules.internalClassRelations();
-        for (ClassRelation classRelation : classRelations.list()) {
-            graph.add(classRelation.dotText());
         }
 
         return DiagramSource.createDiagramSource(documentName, graph.toString());

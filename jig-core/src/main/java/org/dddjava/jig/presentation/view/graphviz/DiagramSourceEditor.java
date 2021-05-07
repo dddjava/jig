2a48561b3865a2d033bfad03df1b85ef8014ed94
irof <irof.ocean@gmail.com>
2021-05-07 23:41:20 +0900

    使用しなくなったクラス削除

--- jig-core/src/main/java/org/dddjava/jig/presentation/view/graphviz/DiagramSourceEditor.java
+++
@@ -1,8 +0,0 @@
-package org.dddjava.jig.presentation.view.graphviz;
-
-import org.dddjava.jig.domain.model.jigdocument.stationery.DiagramSources;
-
-public interface DiagramSourceEditor {
-
-    DiagramSources edit(Object model);
-}

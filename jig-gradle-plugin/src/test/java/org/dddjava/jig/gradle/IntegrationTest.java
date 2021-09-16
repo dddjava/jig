534bbe4fce46c7b3ecf9f8a1e7889de8b64beee5
irof <irof.ocean@gmail.com>
2021-09-16 19:24:03 +0900

    Gradleプラグインテストでのログ出力

--- jig-gradle-plugin/src/test/java/org/dddjava/jig/gradle/IntegrationTest.java
+++ jig-gradle-plugin/src/test/java/org/dddjava/jig/gradle/IntegrationTest.java
@@ -6,7 +6,10 @@ import org.gradle.testkit.runner.TaskOutcome;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
@@ -20,6 +23,7 @@ import static org.junit.jupiter.api.Assertions.*;
  * Gradleのサポート対象のバージョンでの動作確認
  */
 public class IntegrationTest {
+    static Logger logger = LoggerFactory.getLogger(IntegrationTest.class);
 
     @ParameterizedTest
     @EnumSource(SupportGradleVersion.class)
@@ -29,24 +33,25 @@ public class IntegrationTest {
         Path outputDir = path.resolve("sub-project/build/jig");
         String jigTask = ":sub-project:jigReports";
 
-        {
-            BuildResult result = runner.runTask("clean", "compileJava", jigTask, "--stacktrace");
-            assertAll("スタブプロジェクトへの適用でパッケージ図とビジネスルール一覧が出力されること",
-                    () -> {
-                        BuildTask buildTask = Objects.requireNonNull(result.task(jigTask));
-                        assertEquals(TaskOutcome.SUCCESS, buildTask.getOutcome());
-                    },
-                    () -> assertTrue(outputDir.resolve("package-relation-depth4.svg").toFile().exists()),
-                    () -> assertTrue(outputDir.resolve("business-rule.xlsx").toFile().exists())
-            );
-        }
+        BuildResult result = runner.runTask("clean", "compileJava", jigTask, "--stacktrace");
 
-        {
-            // cleanで build/jig が削除されること
-            assertTrue(Files.exists(outputDir));
-            runner.runTask("clean");
-            assertFalse(Files.exists(outputDir));
-        }
+        logger.warn("task results = {}", result.getTasks());
+
+        BuildTask buildTask = Objects.requireNonNull(result.task(jigTask));
+        assertEquals(TaskOutcome.SUCCESS, buildTask.getOutcome());
+
+        File outputDirectory = outputDir.toFile();
+        logger.warn("outputDir={}, exists={}, list={}", outputDir.normalize(), outputDirectory.exists(), outputDirectory.list());
+
+        assertAll("スタブプロジェクトへの適用でパッケージ図とビジネスルール一覧が出力されること",
+                () -> assertTrue(outputDir.resolve("package-relation-depth4.svg").toFile().exists()),
+                () -> assertTrue(outputDir.resolve("business-rule.xlsx").toFile().exists())
+        );
+
+        // cleanで build/jig が削除されること
+        assertTrue(Files.exists(outputDir));
+        runner.runTask("clean");
+        assertFalse(Files.exists(outputDir));
     }
 
     @Test

ea47ecad4820fca3bda0533dbbf5212427ca75fd
irof <irof.ocean@gmail.com>
2022-10-23 22:29:52 +0900

    comment

--- jig-core/src/main/java/org/dddjava/jig/domain/model/documents/documentformat/JigDocumentType.java
+++ jig-core/src/main/java/org/dddjava/jig/domain/model/documents/documentformat/JigDocumentType.java
@@ -1,7 +1,21 @@
 package org.dddjava.jig.domain.model.documents.documentformat;
 
+/**
+ * JIGドキュメントの大分類
+ */
 public enum JigDocumentType {
+    /**
+     * 概要
+     */
     SUMMARY,
+
+    /**
+     * 一覧
+     */
     LIST,
+
+    /**
+     * 図
+     */
     DIAGRAM
 }

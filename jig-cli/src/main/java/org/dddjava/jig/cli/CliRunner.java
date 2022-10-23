3ae3cc9437e3bdeb6e996bb22cf5ea804824f804
irof <irof.ocean@gmail.com>
2022-10-23 21:51:52 +0900

    CliRunnerを分離

---
+++ jig-cli/src/main/java/org/dddjava/jig/cli/CliRunner.java
@@ -0,0 +1,45 @@
+package org.dddjava.jig.cli;
+
+import org.dddjava.jig.domain.model.sources.file.SourcePaths;
+import org.dddjava.jig.infrastructure.configuration.Configuration;
+import org.dddjava.jig.presentation.controller.JigExecutor;
+import org.dddjava.jig.presentation.view.handler.HandleResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.lang.invoke.MethodHandles;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * JIG-CLI Runner
+ */
+@Component
+public class CliRunner {
+    private static Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    private final CliConfig cliConfig;
+
+    public CliRunner(CliConfig cliConfig) {
+        this.cliConfig = cliConfig;
+    }
+
+    void run(String... args) {
+        Configuration configuration = cliConfig.configuration();
+        logger.info("-- configuration -------------------------------------------\n{}\n------------------------------------------------------------", cliConfig.propertiesText());
+
+        long startTime = System.currentTimeMillis();
+        SourcePaths sourcePaths = cliConfig.rawSourceLocations();
+
+        List<HandleResult> handleResultList = JigExecutor.execute(configuration, sourcePaths);
+
+        String resultLog = handleResultList.stream()
+                .filter(HandleResult::success)
+                .map(handleResult -> handleResult.jigDocument() + " : " + handleResult.outputFilePathsText())
+                .collect(Collectors.joining("\n"));
+        logger.info("-- Output Complete {} ms -------------------------------------------\n{}\n------------------------------------------------------------",
+                System.currentTimeMillis() - startTime,
+                resultLog);
+    }
+}
--- jig-cli/src/main/java/org/dddjava/jig/cli/CommandLineApplication.java
+++ jig-cli/src/main/java/org/dddjava/jig/cli/CommandLineApplication.java
@@ -1,47 +1,17 @@
 package org.dddjava.jig.cli;
 
-import org.dddjava.jig.domain.model.sources.file.SourcePaths;
-import org.dddjava.jig.infrastructure.configuration.Configuration;
-import org.dddjava.jig.presentation.controller.JigExecutor;
-import org.dddjava.jig.presentation.view.handler.HandleResult;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 @SpringBootApplication
-public class CommandLineApplication implements CommandLineRunner {
-
-    private static final Logger logger = LoggerFactory.getLogger(CommandLineApplication.class);
+public class CommandLineApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CommandLineApplication.class, args);
-    }
+        var context = SpringApplication.run(CommandLineApplication.class, args);
 
-    @Autowired
-    CliConfig cliConfig;
+        var cliRunner = context.getBean(CliRunner.class);
+        cliRunner.run(args);
 
-    @Override
-    public void run(String... args) {
-        Configuration configuration = cliConfig.configuration();
-        logger.info("-- configuration -------------------------------------------\n{}\n------------------------------------------------------------", cliConfig.propertiesText());
-
-        long startTime = System.currentTimeMillis();
-        SourcePaths sourcePaths = cliConfig.rawSourceLocations();
-
-        List<HandleResult> handleResultList = JigExecutor.execute(configuration, sourcePaths);
-
-        String resultLog = handleResultList.stream()
-                .filter(HandleResult::success)
-                .map(handleResult -> handleResult.jigDocument() + " : " + handleResult.outputFilePathsText())
-                .collect(Collectors.joining("\n"));
-        logger.info("-- Output Complete {} ms -------------------------------------------\n{}\n------------------------------------------------------------",
-                System.currentTimeMillis() - startTime,
-                resultLog);
+        System.exit(SpringApplication.exit(context));
     }
 }

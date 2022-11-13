8de9d3145013eb37ac522c40a6aafabc025ae066
irof <irof.ocean@gmail.com>
2022-11-14 00:27:28 +0900

    Javaparserの動きが切り替わるテスト

--- jig-core/src/main/java/org/dddjava/jig/infrastructure/javaparser/JavaparserReader.java
+++ jig-core/src/main/java/org/dddjava/jig/infrastructure/javaparser/JavaparserReader.java
@@ -2,6 +2,8 @@ package org.dddjava.jig.infrastructure.javaparser;
 
 import com.github.javaparser.ParserConfiguration;
 import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.PackageDeclaration;
 import org.dddjava.jig.domain.model.parts.packages.PackageComment;
 import org.dddjava.jig.domain.model.parts.packages.PackageComments;
 import org.dddjava.jig.domain.model.sources.file.text.ReadableTextSource;
@@ -12,6 +14,7 @@ import org.dddjava.jig.infrastructure.configuration.JigProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,7 +26,6 @@ public class JavaparserReader implements JavaTextSourceReader {
     private static Logger LOGGER = LoggerFactory.getLogger(JavaparserReader.class);
 
     PackageInfoReader packageInfoReader = new PackageInfoReader();
-    ClassReader classReader = new ClassReader();
 
     public JavaparserReader(JigProperties properties) {
         if (Runtime.version().feature() >= 17) {
@@ -49,8 +51,8 @@ public class JavaparserReader implements JavaTextSourceReader {
     public TextSourceModel readClasses(ReadableTextSources readableTextSources) {
         return readableTextSources.list().stream()
                 .map(readableTextSource -> {
-                    try {
-                        return classReader.read(readableTextSource);
+                    try (InputStream inputStream = readableTextSource.toInputStream()) {
+                        return read(inputStream);
                     } catch (Exception e) {
                         LOGGER.warn("{} のソースコード読み取りに失敗しました（処理は続行します）", readableTextSource);
                         LOGGER.debug("{}読み取り失敗の詳細", readableTextSource, e);
@@ -59,5 +61,18 @@ public class JavaparserReader implements JavaTextSourceReader {
                 })
                 .reduce(TextSourceModel::merge)
                 .orElseGet(() -> TextSourceModel.empty());
+    }
+
+    TextSourceModel read(InputStream inputStream) {
+        CompilationUnit cu = StaticJavaParser.parse(inputStream);
+
+        String packageName = cu.getPackageDeclaration()
+                .map(PackageDeclaration::getNameAsString)
+                .map(name -> name + ".")
+                .orElse("");
+
+        ClassVisitor typeVisitor = new ClassVisitor(packageName);
+        cu.accept(typeVisitor, null);
+        return typeVisitor.toTextSourceModel();
     }
 }

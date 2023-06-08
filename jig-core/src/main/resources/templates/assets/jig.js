288a65cc8b5dc813d5c1d135521cf60470ae2db6
irof <irof.ocean@gmail.com>
2023-06-09 01:39:32 +0900

    用語集の機能拡充
    - 表示切り替え
    - フィルタ
    - ソート

--- jig-core/src/main/resources/templates/assets/jig.js
+++ jig-core/src/main/resources/templates/assets/jig.js
@@ -1,1 +1,63 @@
 Array.from(document.getElementsByClassName("markdown")).forEach(x => x.innerHTML = marked.parse(x.innerHTML))
+
+function toggleTableColumn(tableId, columnIndex) {
+    var table = document.getElementById(tableId);
+    var rows = table.rows;
+
+   for (var i = 0; i < rows.length; i++) {
+        var row = rows[i];
+        var cells = row.cells;
+        var cell = cells[columnIndex];
+
+        if (cell.classList.contains("hidden")) {
+            cell.classList.remove("hidden");
+        } else {
+            cell.classList.add("hidden");
+        }
+    }
+}
+
+function filterTable(tableId, filterInputId) {
+    var table = document.getElementById(tableId);
+    var rows = table.getElementsByTagName("tr");
+    var filterText = document.getElementById(filterInputId).value;
+
+    for (var i = 1; i < rows.length; i++) {
+        var row = rows[i];
+        var cells = row.getElementsByTagName("td");
+        var match = false;
+
+        for (var j = 0; j < cells.length; j++) {
+            var cell = cells[j];
+            if (cell) {
+                var cellText = cell.textContent || cell.innerText;
+                if (cellText.indexOf(filterText) > -1) {
+                    match = true;
+                    break;
+                }
+            }
+        }
+
+        if (match) {
+            row.style.display = "";
+        } else {
+            row.style.display = "none";
+        }
+    }
+}
+
+function sortTable(tableId, columnIndex) {
+    var table = document.getElementById(tableId);
+    var rows = Array.from(table.getElementsByTagName("tbody")[0].getElementsByTagName("tr"));
+
+    rows.sort(function (a, b) {
+        var aValue = a.getElementsByTagName("td")[columnIndex].textContent;
+        var bValue = b.getElementsByTagName("td")[columnIndex].textContent;
+        return aValue.localeCompare(bValue);
+    });
+
+    var tbody = table.getElementsByTagName("tbody")[0];
+    rows.forEach(function (row) {
+        tbody.appendChild(row);
+    });
+}

380154a3dd0561ab7f22b7496ff523d6d9d4f9ef
irof <irof.ocean@gmail.com>
2024-06-22 23:42:52 +0900

    add Javaparser package-info

---
+++ jig-core/src/main/java/org/dddjava/jig/infrastructure/javaparser/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Javaparserを使用してJavaファイルを読む
+ *
+ * JIGはコンパイルされたクラスファイルを基本とするが、コンパイルにより失われる情報もあるため、Javaファイルも読み取る必要がある。
+ * コメントはこちらで読み取るしかない。
+ */
+package org.dddjava.jig.infrastructure.javaparser;

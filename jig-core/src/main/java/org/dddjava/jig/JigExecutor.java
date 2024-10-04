85f1b26c55056b6ed50b8cead013950729b98861
irof <irof.ocean@gmail.com>
2024-10-04 20:13:28 +0900

    Remove unused logger from JigExecutor
    The logger instance was removed from the JigExecutor class because it was not being used. This change simplifies the code and eliminates unnecessary dependencies.

--- jig-core/src/main/java/org/dddjava/jig/JigExecutor.java
+++ jig-core/src/main/java/org/dddjava/jig/JigExecutor.java
@@ -4,13 +4,10 @@ import org.dddjava.jig.application.JigDocumentGenerator;
 import org.dddjava.jig.application.JigSourceReader;
 import org.dddjava.jig.domain.model.sources.file.SourcePaths;
 import org.dddjava.jig.infrastructure.configuration.Configuration;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 public class JigExecutor {
-    public static Logger logger = LoggerFactory.getLogger(JigExecutor.class);
 
     public static List<HandleResult> execute(Configuration configuration, SourcePaths sourcePaths) {
         JigSourceReader jigSourceReader = configuration.sourceReader();

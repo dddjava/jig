5d77998ac963df90eaaa4142a90f7c571efe4d25
irof <irof.ocean@gmail.com>
2024-10-04 20:12:32 +0900

    Refactor source reading and error handling logic
    Refactor `readSource` method to streamline error handling by consolidating read events and separating checks for source types. Removed redundant `readSourceFromPaths` method and integrated SQL source reading within the `readSource` method. This results in clearer, more maintainable code and better error logging.

--- jig-core/src/main/java/org/dddjava/jig/application/JigSourceReader.java
+++ jig-core/src/main/java/org/dddjava/jig/application/JigSourceReader.java
@@ -1,6 +1,5 @@
 package org.dddjava.jig.application;
 
-import org.dddjava.jig.JigExecutor;
 import org.dddjava.jig.domain.model.parts.classes.rdbaccess.Sqls;
 import org.dddjava.jig.domain.model.parts.packages.PackageComment;
 import org.dddjava.jig.domain.model.parts.packages.PackageComments;
@@ -12,7 +11,10 @@ import org.dddjava.jig.domain.model.sources.file.text.TextSources;
 import org.dddjava.jig.domain.model.sources.file.text.sqlcode.SqlSources;
 import org.dddjava.jig.domain.model.sources.jigfactory.TextSourceModel;
 import org.dddjava.jig.domain.model.sources.jigfactory.TypeFacts;
-import org.dddjava.jig.domain.model.sources.jigreader.*;
+import org.dddjava.jig.domain.model.sources.jigreader.FactReader;
+import org.dddjava.jig.domain.model.sources.jigreader.ReadStatus;
+import org.dddjava.jig.domain.model.sources.jigreader.SqlReader;
+import org.dddjava.jig.domain.model.sources.jigreader.TextSourceReader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -45,49 +47,31 @@ public class JigSourceReader {
     }
 
     public Optional<JigSource> readSource(SourcePaths sourcePaths) {
-        ReadStatuses status = readSourceFromPaths(sourcePaths);
-        if (status.hasError()) {
-            for (ReadStatus readStatus : status.listErrors()) {
-                JigExecutor.logger.error("{}", readStatus.localizedMessage());
-            }
-            return Optional.empty();
-        }
-        if (status.hasWarning()) {
-            for (ReadStatus readStatus : status.listWarning()) {
-                JigExecutor.logger.warn("{}", readStatus.localizedMessage());
-            }
-        }
+        List<ReadStatus> readEvents = new ArrayList<>();
 
-        var jigSource = new JigSource(jigSourceRepository.allTypeFacts());
-        return Optional.of(jigSource);
-    }
-
-    /**
-     * パスからソースを読み取る
-     */
-    public ReadStatuses readSourceFromPaths(SourcePaths sourcePaths) {
+        // ソースのチェック
         Sources source = sourceReader.readSources(sourcePaths);
-
-        readProjectData(source);
-        Sqls sqls = readSqlSource(source.sqlSources());
-
-        List<ReadStatus> list = new ArrayList<>();
-
-        if (source.nothingBinarySource()) {
-            list.add(ReadStatus.バイナリソースなし);
-        }
-
-        if (source.nothingTextSource()) {
-            list.add(ReadStatus.テキストソースなし);
-        }
-
+        if (source.nothingBinarySource()) readEvents.add(ReadStatus.バイナリソースなし);
+        if (source.nothingTextSource()) readEvents.add(ReadStatus.テキストソースなし);
         // binarySourceがあってtypeByteCodesがない（ASMの解析で失敗する）のは現状実行時エラーになるのでここでは考慮しない
 
-        if (sqls.status().not正常()) {
-            list.add(ReadStatus.fromSqlReadStatus(sqls.status()));
+        Sqls sqls = readSqlSource(source.sqlSources());
+        if (sqls.status().not正常()) readEvents.add(ReadStatus.fromSqlReadStatus(sqls.status()));
+
+        readEvents.forEach(readStatus -> {
+            if (readStatus.isError()) {
+                logger.error("{}", readStatus.localizedMessage());
+            } else {
+                logger.warn("{}", readStatus.localizedMessage());
+            }
+        });
+        // errorが1つでもあったら読み取り失敗としてSourceを返さない
+        if (readEvents.stream().anyMatch(event -> event.isError())) {
+            return Optional.empty();
         }
 
-        return new ReadStatuses(list);
+        var jigSource = readProjectData(source);
+        return Optional.of(jigSource);
     }
 
     /**

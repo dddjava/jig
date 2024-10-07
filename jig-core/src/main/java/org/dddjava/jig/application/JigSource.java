7f2c37df3fe951b619993f696e26662da8753c73
irof <irof.ocean@gmail.com>
2024-10-07 16:39:07 +0900

    remove MutableSource

--- jig-core/src/main/java/org/dddjava/jig/application/JigSource.java
+++ jig-core/src/main/java/org/dddjava/jig/application/JigSource.java
@@ -7,24 +7,21 @@ import org.dddjava.jig.domain.model.sources.jigfactory.TypeFacts;
 import java.util.HashMap;
 import java.util.Map;
 
-public record JigSource(TypeFacts typeFacts, MutableSource mutableSource) {
+public record JigSource(TypeFacts typeFacts, Map<Class<?>, Object> map) {
 
     public JigSource(TypeFacts typeFacts) {
-        this(typeFacts, new MutableSource(new HashMap<>()));
+        this(typeFacts, new HashMap<>());
     }
 
     public void addSqls(Sqls sqls) {
-        mutableSource().map().put(Sqls.class, sqls);
+        map().put(Sqls.class, sqls);
     }
 
     public Sqls sqls() {
-        return (Sqls) mutableSource().map().getOrDefault(Sqls.class, Sqls.empty());
+        return (Sqls) map().getOrDefault(Sqls.class, Sqls.empty());
     }
 
     public EnumModels enumModels() {
         return typeFacts().enumModels();
-    }
-
-    private record MutableSource(Map<Class<?>, Object> map) {
     }
 }

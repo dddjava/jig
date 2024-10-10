5a0a52f8f00460196f73882a3ff7e4d086f1980c
irof <irof.ocean@gmail.com>
2024-10-10 18:40:30 +0900

    convert record

--- jig-core/src/main/java/org/dddjava/jig/domain/model/parts/classes/method/MethodRelation.java
+++ jig-core/src/main/java/org/dddjava/jig/domain/model/parts/classes/method/MethodRelation.java
@@ -3,18 +3,7 @@ package org.dddjava.jig.domain.model.parts.classes.method;
 /**
  * メソッドの使用しているメソッド
  */
-public class MethodRelation {
-    MethodDeclaration from;
-    MethodDeclaration to;
-
-    public MethodRelation(MethodDeclaration from, MethodDeclaration to) {
-        this.from = from;
-        this.to = to;
-    }
-
-    MethodDeclaration from() {
-        return from;
-    }
+public record MethodRelation(MethodDeclaration from, MethodDeclaration to) {
 
     public boolean calleeMethodIs(MethodDeclaration calleeMethod) {
         return to.sameIdentifier(calleeMethod);

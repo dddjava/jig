7862947f8148a2051f351a7c45ee5b395ec531ec
irof <irof.ocean@gmail.com>
2024-10-10 21:44:54 +0900

    add comment

--- jig-core/src/main/java/org/dddjava/jig/JigContext.java
+++ jig-core/src/main/java/org/dddjava/jig/JigContext.java
@@ -1,6 +1,10 @@
 package org.dddjava.jig;
 
 public enum JigContext {
+    /**
+     * パッケージの略し方
+     * initial, numeric
+     */
     packageAbbreviationMode("initial");
 
     private final String value;

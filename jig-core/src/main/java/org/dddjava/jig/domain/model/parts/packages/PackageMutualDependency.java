ddd4b4e5e3fc586961ea463f690a6ce34496bebf
irof <irof.ocean@gmail.com>
2025-01-03 16:21:39 +0900

    PackageRelationは片方向であり、MutualDependencyのフィールドとしては不適切なので展開する

--- jig-core/src/main/java/org/dddjava/jig/domain/model/parts/packages/PackageMutualDependency.java
+++ jig-core/src/main/java/org/dddjava/jig/domain/model/parts/packages/PackageMutualDependency.java
@@ -4,21 +4,22 @@ package org.dddjava.jig.domain.model.parts.packages;
  * パッケージの相互依存
  */
 public class PackageMutualDependency {
-    PackageRelation packageRelation;
+
+    private final PackageIdentifier left;
+    private final PackageIdentifier right;
 
     public PackageMutualDependency(PackageRelation packageRelation) {
-        this.packageRelation = packageRelation;
+        left = packageRelation.from;
+        right = packageRelation.to;
     }
 
     public boolean matches(PackageRelation packageRelation) {
-        PackageIdentifier left = this.packageRelation.from;
-        PackageIdentifier right = this.packageRelation.to;
         return (left.contains(packageRelation.from()) && right.contains(packageRelation.to())) ||
                 (left.contains(packageRelation.to()) && right.contains(packageRelation.from()));
     }
 
     @Override
     public String toString() {
-        return packageRelation.from.asText() + " <-> " + packageRelation.to.asText();
+        return left.asText() + " <-> " + right.asText();
     }
 }

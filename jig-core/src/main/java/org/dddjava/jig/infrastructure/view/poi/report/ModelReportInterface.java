0bab6367abed168317eb53c5b81389ef04ede777
irof <irof.ocean@gmail.com>
2025-01-07 22:08:02 +0900

    無名クラスをネストクラスに変換

--- jig-core/src/main/java/org/dddjava/jig/infrastructure/view/poi/report/ModelReportInterface.java
+++ jig-core/src/main/java/org/dddjava/jig/infrastructure/view/poi/report/ModelReportInterface.java
@@ -17,66 +17,11 @@ public interface ModelReportInterface {
     boolean nothing();
 
     static ModelReportInterface fromInputs(Entrypoint entrypoint) {
-        return new ModelReportInterface() {
-
-            @Override
-            public void writeSheet(Workbook book, JigDocumentWriter jigDocumentWriter, ReportItemFormatter reportItemFormatter) {
-                writeSheet(book, "CONTROLLER", Entrypoint.reporter(), entrypoint.listRequestHandlerMethods());
-            }
-
-            @Override
-            public boolean nothing() {
-                return entrypoint.listRequestHandlerMethods().isEmpty();
-            }
-        };
+        return new MyModelReportInterface<>("CONTROLLER", Entrypoint.reporter(), entrypoint.listRequestHandlerMethods());
     }
 
     static ModelReportInterface fromTerm(Terms terms) {
-        return new ModelReportInterface() {
-
-            @Override
-            public void writeSheet(Workbook book, JigDocumentWriter jigDocumentWriter, ReportItemFormatter reportItemFormatter) {
-                writeSheet(book, "TERM", Terms.reporter(), terms.list());
-            }
-
-            @Override
-            public boolean nothing() {
-                return terms.list().isEmpty();
-            }
-        };
-    }
-
-    default <T> void writeSheet(Workbook book, String sheetName, List<Map.Entry<String, Function<T, Object>>> reporter, List<T> items) {
-        Sheet sheet = book.createSheet(sheetName);
-        writeHeader(sheet, reporter.stream().map(Map.Entry::getKey).toList());
-        List<Function<T, Object>> bodyFunctions = reporter.stream().map(Map.Entry::getValue).toList();
-
-        items.forEach(item -> {
-            Row row = sheet.createRow(sheet.getLastRowNum() + 1);
-            for (var i = 0; i < bodyFunctions.size(); i++) {
-                var value = bodyFunctions.get(i).apply(item);
-                if (value instanceof String stringValue) {
-                    Cell cell = row.createCell(i, CellType.STRING);
-                    cell.setCellValue(stringValue);
-                } else if (value instanceof Number numberValue) {
-                    Cell cell = row.createCell(i, CellType.NUMERIC);
-                    cell.setCellValue(numberValue.doubleValue());
-                } else {
-                    throw new UnsupportedOperationException("unsupported type " + value.getClass().getSimpleName());
-                }
-            }
-        });
-
-        applyAttribute(sheet, reporter.size());
-    }
-
-    default void writeHeader(Sheet sheet, List<String> header) {
-        Row row = sheet.createRow(0);
-        for (int i = 0; i < header.size(); i++) {
-            // headerは全てSTRINGで作る
-            Cell cell = row.createCell(i, CellType.STRING);
-            cell.setCellValue(header.get(i));
-        }
+        return new MyModelReportInterface<>("TERM", Terms.reporter(), terms.list());
     }
 
     default void applyAttribute(Sheet sheet, int columns) {
@@ -89,5 +34,61 @@ public interface ModelReportInterface {
                 0, sheet.getLastRowNum(),
                 0, columns - 1
         ));
+    }
+
+    class MyModelReportInterface<T> implements ModelReportInterface {
+
+        private final String sheetName;
+        private final List<Map.Entry<String, Function<T, Object>>> reporter;
+        private final List<T> items;
+
+        public MyModelReportInterface(String sheetName, List<Map.Entry<String, Function<T, Object>>> reporter, List<T> items) {
+            this.sheetName = sheetName;
+            this.reporter = reporter;
+            this.items = items;
+        }
+
+        @Override
+        public void writeSheet(Workbook book, JigDocumentWriter jigDocumentWriter, ReportItemFormatter reportItemFormatter) {
+            writeSheet(book, sheetName, reporter, items);
+        }
+
+        @Override
+        public boolean nothing() {
+            return items.isEmpty();
+        }
+
+        private void writeSheet(Workbook book, String sheetName, List<Map.Entry<String, Function<T, Object>>> reporter, List<T> items) {
+            Sheet sheet = book.createSheet(sheetName);
+            writeHeader(sheet, reporter.stream().map(Map.Entry::getKey).toList());
+            List<Function<T, Object>> bodyFunctions = reporter.stream().map(Map.Entry::getValue).toList();
+
+            items.forEach(item -> {
+                Row row = sheet.createRow(sheet.getLastRowNum() + 1);
+                for (var i = 0; i < bodyFunctions.size(); i++) {
+                    var value = bodyFunctions.get(i).apply(item);
+                    if (value instanceof String stringValue) {
+                        Cell cell = row.createCell(i, CellType.STRING);
+                        cell.setCellValue(stringValue);
+                    } else if (value instanceof Number numberValue) {
+                        Cell cell = row.createCell(i, CellType.NUMERIC);
+                        cell.setCellValue(numberValue.doubleValue());
+                    } else {
+                        throw new UnsupportedOperationException("unsupported type " + value.getClass().getSimpleName());
+                    }
+                }
+            });
+
+            applyAttribute(sheet, reporter.size());
+        }
+
+        private void writeHeader(Sheet sheet, List<String> header) {
+            Row row = sheet.createRow(0);
+            for (int i = 0; i < header.size(); i++) {
+                // headerは全てSTRINGで作る
+                Cell cell = row.createCell(i, CellType.STRING);
+                cell.setCellValue(header.get(i));
+            }
+        }
     }
 }

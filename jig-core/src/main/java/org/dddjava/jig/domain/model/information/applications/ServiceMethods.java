316179ab851a673e3617556f5a6dbff1ec916fe7
irof <irof.ocean@gmail.com>
2025-01-13 21:52:34 +0900

    未使用メソッド削除

--- jig-core/src/main/java/org/dddjava/jig/domain/model/information/applications/ServiceMethods.java
+++ jig-core/src/main/java/org/dddjava/jig/domain/model/information/applications/ServiceMethods.java
@@ -1,14 +1,10 @@
 package org.dddjava.jig.domain.model.information.applications;
 
 import org.dddjava.jig.domain.model.data.classes.method.MethodDeclaration;
-import org.dddjava.jig.domain.model.data.classes.method.MethodDeclarations;
 import org.dddjava.jig.domain.model.data.classes.method.MethodRelations;
-import org.dddjava.jig.domain.model.information.jigobject.class_.JigType;
 import org.dddjava.jig.domain.model.information.jigobject.class_.JigTypes;
-import org.dddjava.jig.domain.model.information.jigobject.member.JigMethod;
 
 import java.util.List;
-import java.util.Optional;
 
 import static java.util.stream.Collectors.toList;
 
@@ -39,28 +35,9 @@ public class ServiceMethods {
                 .collect(toList());
     }
 
-    public List<JigType> listJigTypes() {
-        return serviceJigTypes.list();
-    }
-
-    public MethodDeclarations toMethodDeclarations() {
-        return serviceJigTypes.stream()
-                .flatMap(jigType -> jigType.instanceMember().instanceMethods().stream())
-                .map(jigMethod -> jigMethod.declaration())
-                .collect(MethodDeclarations.collector());
-    }
-
     public boolean contains(MethodDeclaration methodDeclaration) {
         return serviceJigTypes.stream()
                 .flatMap(jigType -> jigType.instanceMember().instanceMethods().stream())
                 .anyMatch(jigMethod -> methodDeclaration.sameIdentifier(jigMethod.declaration()));
-    }
-
-    public Optional<JigMethod> find(MethodDeclaration usingMethod) {
-        return serviceJigTypes.stream()
-                .filter(jigType -> jigType.identifier().equals(usingMethod.declaringType()))
-                .flatMap(jigType -> jigType.instanceMember().instanceMethods().stream())
-                .filter(jigMethod -> jigMethod.declaration().sameIdentifier(usingMethod))
-                .findAny();
     }
 }

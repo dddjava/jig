688b1e30f73a583a1a1e311096db64ef752017c3
irof <irof.ocean@gmail.com>
2025-01-14 23:23:49 +0900

    テストメソッドの構造化

--- jig-core/src/test/java/org/dddjava/jig/infrastructure/asm/AsmFactReaderTest.java
+++ jig-core/src/test/java/org/dddjava/jig/infrastructure/asm/AsmFactReaderTest.java
@@ -15,6 +15,7 @@ import org.dddjava.jig.domain.model.information.jigobject.class_.TypeKind;
 import org.dddjava.jig.domain.model.information.jigobject.member.JigFields;
 import org.dddjava.jig.domain.model.information.jigobject.member.JigMethod;
 import org.dddjava.jig.domain.model.information.jigobject.member.JigMethods;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -53,216 +54,227 @@ import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class AsmFactReaderTest {
 
-    @Test
-    void フィールドに付与されているアノテーションと記述が取得できる() throws Exception {
-        JigType actual = JigType構築(Annotated.class);
+    @Nested
+    class クラス {
+        @Test
+        void クラス定義に使用している型が取得できる() throws Exception {
+            JigType actual = JigType構築(ClassDefinition.class);
 
-        JigFields jigFields = actual.instanceMember().instanceFields();
+            TypeIdentifiers identifiers = actual.usingTypes();
+            assertThat(identifiers.list())
+                    .contains(
+                            TypeIdentifier.from(ClassAnnotation.class),
+                            TypeIdentifier.from(SuperClass.class),
+                            TypeIdentifier.from(ImplementA.class),
+                            TypeIdentifier.from(ImplementB.class),
+                            TypeIdentifier.from(GenericsParameter.class)
+                    );
 
-        FieldAnnotation fieldAnnotation = jigFields.list().stream()
-                .filter(e -> e.fieldDeclaration().nameText().equals("field"))
-                .findFirst()
-                .flatMap(jigField -> jigField.fieldAnnotations().list().stream().findFirst())
-                .orElseThrow(AssertionError::new);
+            ParameterizedType parameterizedSuperType = actual.typeDeclaration().superType();
+            assertThat(parameterizedSuperType)
+                    .extracting(
+                            ParameterizedType::asSimpleText,
+                            ParameterizedType::typeIdentifier
+                    )
+                    .containsExactly(
+                            "SuperClass<Integer, Long>",
+                            TypeIdentifier.from(SuperClass.class)
+                    );
+        }
 
-        assertEquals(TypeIdentifier.from(VariableAnnotation.class), fieldAnnotation.annotationType());
+        @Test
+        void インタフェース定義に使用している型が取得できる() throws Exception {
+            JigType actual = JigType構築(InterfaceDefinition.class);
 
-        AnnotationDescription description = fieldAnnotation.description();
-        assertThat(description.asText())
-                .contains(
-                        "string=af",
-                        "arrayString=bf",
-                        "number=13",
-                        "clz=Ljava/lang/reflect/Field;",
-                        "arrayClz=[Ljava/lang/Object;, Ljava/lang/Object;]",
-                        "enumValue=DUMMY1",
-                        "annotation=Ljava/lang/Deprecated;[...]"
-                );
+            TypeIdentifiers identifiers = actual.usingTypes();
+            assertThat(identifiers.list())
+                    .contains(
+                            TypeIdentifier.from(ClassAnnotation.class),
+                            TypeIdentifier.from(Comparable.class),
+                            TypeIdentifier.from(GenericsParameter.class)
+                    );
 
-        assertThat(description.textOf("arrayString")).isEqualTo("bf");
+            ParameterizedType parameterizedType = actual.typeDeclaration().interfaceTypes().list().get(0);
+            assertThat(parameterizedType)
+                    .extracting(
+                            ParameterizedType::asSimpleText,
+                            ParameterizedType::typeIdentifier
+                    )
+                    .containsExactly(
+                            "Comparable<GenericsParameter>",
+                            TypeIdentifier.from(Comparable.class)
+                    );
+        }
+
+        @Test
+        void enumで使用している型が取得できる() throws Exception {
+            JigType actual = JigType構築(EnumDefinition.class);
+
+            TypeIdentifiers identifiers = actual.usingTypes();
+            assertThat(identifiers.list())
+                    .contains(
+                            TypeIdentifier.from(EnumField.class),
+                            TypeIdentifier.from(ConstructorArgument.class),
+                            TypeIdentifier.from(ClassReference.class)
+                    );
+        }
+
+        @ParameterizedTest
+        @MethodSource
+        void enumの種類が識別できる(Class<?> clz, boolean parameter, boolean behavior, boolean polymorphism) throws Exception {
+            JigType jigType = JigType構築(clz);
+            CategoryType categoryType = new CategoryType(jigType);
+
+            assertEquals(categoryType.hasParameter(), parameter);
+            assertEquals(categoryType.hasBehaviour(), behavior);
+            assertEquals(categoryType.isPolymorphism(), polymorphism);
+        }
+
+        static Stream<Arguments> enumの種類が識別できる() {
+            return Stream.of(
+                    Arguments.of(SimpleEnum.class, false, false, false),
+                    Arguments.of(BehaviourEnum.class, false, true, false),
+                    Arguments.of(ParameterizedEnum.class, true, false, false),
+                    Arguments.of(PolymorphismEnum.class, false, false, true),
+                    Arguments.of(RichEnum.class, true, true, true));
+        }
+
+        @MethodSource
+        @ParameterizedTest
+        void TypeKind判定(Class<?> targetType, TypeKind typeKind) throws Exception {
+            JigType actual = JigType構築(targetType);
+            assertEquals(typeKind, actual.typeKind());
+        }
+
+        static Stream<Arguments> TypeKind判定() {
+            return Stream.of(
+                    arguments(SimpleNumber.class, TypeKind.通常型),
+                    arguments(SimpleEnum.class, TypeKind.列挙型),
+                    arguments(RichEnum.class, TypeKind.抽象列挙型),
+                    // インタフェースと判定させたい
+                    arguments(HogeRepository.class, TypeKind.通常型),
+                    // アノテーションと判定させたい
+                    arguments(RuntimeRetainedAnnotation.class, TypeKind.通常型)
+            );
+        }
     }
 
-    @Test
-    void メソッドに付与されているアノテーションと記述が取得できる() throws Exception {
-        JigType actual = JigType構築(Annotated.class);
+    @Nested
+    class フィールド {
+        @Test
+        void フィールドに付与されているアノテーションと記述が取得できる() throws Exception {
+            JigType actual = JigType構築(Annotated.class);
 
-        JigMethod method = actual.instanceMethods().resolveMethodBySignature(new MethodSignature("method"));
-        MethodAnnotation methodAnnotation = method.methodAnnotations().list().get(0);
+            JigFields jigFields = actual.instanceMember().instanceFields();
 
-        assertThat(methodAnnotation.annotationType().fullQualifiedName()).isEqualTo(VariableAnnotation.class.getTypeName());
+            FieldAnnotation fieldAnnotation = jigFields.list().stream()
+                    .filter(e -> e.fieldDeclaration().nameText().equals("field"))
+                    .findFirst()
+                    .flatMap(jigField -> jigField.fieldAnnotations().list().stream().findFirst())
+                    .orElseThrow(AssertionError::new);
 
-        AnnotationDescription description = methodAnnotation.description();
-        assertThat(description.asText())
-                .contains(
-                        "string=am",
-                        "arrayString=[bm1, bm2]",
-                        "number=23",
-                        "clz=Ljava/lang/reflect/Method;",
-                        "enumValue=DUMMY2"
-                );
+            assertEquals(TypeIdentifier.from(VariableAnnotation.class), fieldAnnotation.annotationType());
+
+            AnnotationDescription description = fieldAnnotation.description();
+            assertThat(description.asText())
+                    .contains(
+                            "string=af",
+                            "arrayString=bf",
+                            "number=13",
+                            "clz=Ljava/lang/reflect/Field;",
+                            "arrayClz=[Ljava/lang/Object;, Ljava/lang/Object;]",
+                            "enumValue=DUMMY1",
+                            "annotation=Ljava/lang/Deprecated;[...]"
+                    );
+
+            assertThat(description.textOf("arrayString")).isEqualTo("bf");
+        }
+
+        @Test
+        void フィールド定義に使用している型が取得できる() throws Exception {
+            JigType jigType = JigType構築(FieldDefinition.class);
+
+            FieldDeclarations fieldDeclarations = jigType.instanceMember().fieldDeclarations();
+            String fieldsText = fieldDeclarations.toSignatureText();
+            assertEquals("[InstanceField instanceField, List genericFields, ArrayField[] arrayFields, Object obj]", fieldsText);
+
+            TypeIdentifiers identifiers = jigType.usingTypes();
+            assertThat(identifiers.list())
+                    .contains(
+                            TypeIdentifier.from(List.class),
+                            TypeIdentifier.from(stub.domain.model.relation.field.FieldAnnotation.class),
+                            TypeIdentifier.from(StaticField.class),
+                            TypeIdentifier.from(InstanceField.class),
+                            TypeIdentifier.from(GenericField.class),
+                            TypeIdentifier.valueOf(ArrayField.class.getName() + "[]"),
+                            TypeIdentifier.from(ArrayField.class),
+                            TypeIdentifier.from(ReferenceConstantOwnerAtFieldDefinition.class),
+                            TypeIdentifier.from(ReferenceConstantAtFieldDefinition.class)
+                    );
+        }
+
     }
 
-    @Test
-    void クラス定義に使用している型が取得できる() throws Exception {
-        JigType actual = JigType構築(ClassDefinition.class);
+    @Nested
+    class メソッド {
+        @Test
+        void メソッドに付与されているアノテーションと記述が取得できる() throws Exception {
+            JigType actual = JigType構築(Annotated.class);
 
-        TypeIdentifiers identifiers = actual.usingTypes();
-        assertThat(identifiers.list())
-                .contains(
-                        TypeIdentifier.from(ClassAnnotation.class),
-                        TypeIdentifier.from(SuperClass.class),
-                        TypeIdentifier.from(ImplementA.class),
-                        TypeIdentifier.from(ImplementB.class),
-                        TypeIdentifier.from(GenericsParameter.class)
-                );
+            JigMethod method = actual.instanceMethods().resolveMethodBySignature(new MethodSignature("method"));
+            MethodAnnotation methodAnnotation = method.methodAnnotations().list().get(0);
 
-        ParameterizedType parameterizedSuperType = actual.typeDeclaration().superType();
-        assertThat(parameterizedSuperType)
-                .extracting(
-                        ParameterizedType::asSimpleText,
-                        ParameterizedType::typeIdentifier
-                )
-                .containsExactly(
-                        "SuperClass<Integer, Long>",
-                        TypeIdentifier.from(SuperClass.class)
-                );
+            assertThat(methodAnnotation.annotationType().fullQualifiedName()).isEqualTo(VariableAnnotation.class.getTypeName());
+
+            AnnotationDescription description = methodAnnotation.description();
+            assertThat(description.asText())
+                    .contains(
+                            "string=am",
+                            "arrayString=[bm1, bm2]",
+                            "number=23",
+                            "clz=Ljava/lang/reflect/Method;",
+                            "enumValue=DUMMY2"
+                    );
+        }
+
+        @Test
+        void 戻り値のジェネリクスが取得できる() throws Exception {
+            JigType actual = JigType構築(InterfaceDefinition.class);
+
+            TypeIdentifiers identifiers = actual.usingTypes();
+            assertThat(identifiers.list())
+                    .contains(
+                            TypeIdentifier.from(List.class),
+                            TypeIdentifier.from(String.class)
+                    );
+
+            MethodReturn methodReturn = actual.instanceMethods()
+                    .resolveMethodBySignature(new MethodSignature("parameterizedListMethod"))
+                    .declaration().methodReturn();
+            ParameterizedType parameterizedType = methodReturn.parameterizedType();
+
+            assertThat(parameterizedType.asSimpleText()).isEqualTo("List<String>");
+        }
+
+        @Test
+        void メソッドでifやswitchを使用していると検出できる() throws Exception {
+            JigType actual = JigType構築(DecisionClass.class);
+
+            JigMethods jigMethods = actual.instanceMethods();
+            assertThat(jigMethods.list())
+                    .extracting(
+                            method -> method.declaration().methodSignature().asSimpleText(),
+                            method -> method.decisionNumber().asText())
+                    .containsExactlyInAnyOrder(
+                            tuple("分岐なしメソッド()", "0"),
+                            tuple("ifがあるメソッド()", "1"),
+                            tuple("switchがあるメソッド()", "1"),
+                            // forは ifeq と goto で構成されるある意味での分岐
+                            tuple("forがあるメソッド()", "1")
+                    );
+        }
     }
 
-    @Test
-    void インタフェース定義に使用している型が取得できる() throws Exception {
-        JigType actual = JigType構築(InterfaceDefinition.class);
-
-        TypeIdentifiers identifiers = actual.usingTypes();
-        assertThat(identifiers.list())
-                .contains(
-                        TypeIdentifier.from(ClassAnnotation.class),
-                        TypeIdentifier.from(Comparable.class),
-                        TypeIdentifier.from(GenericsParameter.class)
-                );
-
-        ParameterizedType parameterizedType = actual.typeDeclaration().interfaceTypes().list().get(0);
-        assertThat(parameterizedType)
-                .extracting(
-                        ParameterizedType::asSimpleText,
-                        ParameterizedType::typeIdentifier
-                )
-                .containsExactly(
-                        "Comparable<GenericsParameter>",
-                        TypeIdentifier.from(Comparable.class)
-                );
-    }
-
-    @Test
-    void 戻り値のジェネリクスが取得できる() throws Exception {
-        JigType actual = JigType構築(InterfaceDefinition.class);
-
-        TypeIdentifiers identifiers = actual.usingTypes();
-        assertThat(identifiers.list())
-                .contains(
-                        TypeIdentifier.from(List.class),
-                        TypeIdentifier.from(String.class)
-                );
-
-        MethodReturn methodReturn = actual.instanceMethods()
-                .resolveMethodBySignature(new MethodSignature("parameterizedListMethod"))
-                .declaration().methodReturn();
-        ParameterizedType parameterizedType = methodReturn.parameterizedType();
-
-        assertThat(parameterizedType.asSimpleText()).isEqualTo("List<String>");
-    }
-
-    @Test
-    void フィールド定義に使用している型が取得できる() throws Exception {
-        JigType jigType = JigType構築(FieldDefinition.class);
-
-        FieldDeclarations fieldDeclarations = jigType.instanceMember().fieldDeclarations();
-        String fieldsText = fieldDeclarations.toSignatureText();
-        assertEquals("[InstanceField instanceField, List genericFields, ArrayField[] arrayFields, Object obj]", fieldsText);
-
-        TypeIdentifiers identifiers = jigType.usingTypes();
-        assertThat(identifiers.list())
-                .contains(
-                        TypeIdentifier.from(List.class),
-                        TypeIdentifier.from(stub.domain.model.relation.field.FieldAnnotation.class),
-                        TypeIdentifier.from(StaticField.class),
-                        TypeIdentifier.from(InstanceField.class),
-                        TypeIdentifier.from(GenericField.class),
-                        TypeIdentifier.valueOf(ArrayField.class.getName() + "[]"),
-                        TypeIdentifier.from(ArrayField.class),
-                        TypeIdentifier.from(ReferenceConstantOwnerAtFieldDefinition.class),
-                        TypeIdentifier.from(ReferenceConstantAtFieldDefinition.class)
-                );
-    }
-
-    @Test
-    void メソッドでifやswitchを使用していると検出できる() throws Exception {
-        JigType actual = JigType構築(DecisionClass.class);
-
-        JigMethods jigMethods = actual.instanceMethods();
-        assertThat(jigMethods.list())
-                .extracting(
-                        method -> method.declaration().methodSignature().asSimpleText(),
-                        method -> method.decisionNumber().asText())
-                .containsExactlyInAnyOrder(
-                        tuple("分岐なしメソッド()", "0"),
-                        tuple("ifがあるメソッド()", "1"),
-                        tuple("switchがあるメソッド()", "1"),
-                        // forは ifeq と goto で構成されるある意味での分岐
-                        tuple("forがあるメソッド()", "1")
-                );
-    }
-
-    @Test
-    void enumで使用している型が取得できる() throws Exception {
-        JigType actual = JigType構築(EnumDefinition.class);
-
-        TypeIdentifiers identifiers = actual.usingTypes();
-        assertThat(identifiers.list())
-                .contains(
-                        TypeIdentifier.from(EnumField.class),
-                        TypeIdentifier.from(ConstructorArgument.class),
-                        TypeIdentifier.from(ClassReference.class)
-                );
-    }
-
-    @ParameterizedTest
-    @MethodSource
-    void enumの種類が識別できる(Class<?> clz, boolean parameter, boolean behavior, boolean polymorphism) throws Exception {
-        JigType jigType = JigType構築(clz);
-        CategoryType categoryType = new CategoryType(jigType);
-
-        assertEquals(categoryType.hasParameter(), parameter);
-        assertEquals(categoryType.hasBehaviour(), behavior);
-        assertEquals(categoryType.isPolymorphism(), polymorphism);
-    }
-
-    static Stream<Arguments> enumの種類が識別できる() {
-        return Stream.of(
-                Arguments.of(SimpleEnum.class, false, false, false),
-                Arguments.of(BehaviourEnum.class, false, true, false),
-                Arguments.of(ParameterizedEnum.class, true, false, false),
-                Arguments.of(PolymorphismEnum.class, false, false, true),
-                Arguments.of(RichEnum.class, true, true, true));
-    }
-
-    @MethodSource
-    @ParameterizedTest
-    void TypeKind判定(Class<?> targetType, TypeKind typeKind) throws Exception {
-        JigType actual = JigType構築(targetType);
-        assertEquals(typeKind, actual.typeKind());
-    }
-
-    static Stream<Arguments> TypeKind判定() {
-        return Stream.of(
-                arguments(SimpleNumber.class, TypeKind.通常型),
-                arguments(SimpleEnum.class, TypeKind.列挙型),
-                arguments(RichEnum.class, TypeKind.抽象列挙型),
-                // インタフェースと判定させたい
-                arguments(HogeRepository.class, TypeKind.通常型),
-                // アノテーションと判定させたい
-                arguments(RuntimeRetainedAnnotation.class, TypeKind.通常型)
-        );
-    }
 
     private JigType JigType構築(Class<?> clz) throws URISyntaxException, IOException {
         URL url = Objects.requireNonNull(clz.getResource(clz.getSimpleName().concat(".class")));

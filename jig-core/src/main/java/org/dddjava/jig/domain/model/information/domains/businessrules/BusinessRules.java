33020448fd7507eaa324d003de130e667f4693e5
irof <irof.ocean@gmail.com>
2025-01-19 15:55:20 +0900

    継承させて

--- jig-core/src/main/java/org/dddjava/jig/domain/model/information/domains/businessrules/BusinessRules.java
+++ jig-core/src/main/java/org/dddjava/jig/domain/model/information/domains/businessrules/BusinessRules.java
@@ -1,30 +1,13 @@
 package org.dddjava.jig.domain.model.information.domains.businessrules;
 
-import org.dddjava.jig.domain.model.information.jigobject.class_.JigType;
 import org.dddjava.jig.domain.model.information.jigobject.class_.JigTypes;
-
-import java.util.List;
 
 /**
  * ビジネスルール一覧
  */
-public class BusinessRules {
-
-    JigTypes jigTypes;
+public class BusinessRules extends JigTypes {
 
     public BusinessRules(JigTypes jigTypes) {
-        this.jigTypes = jigTypes;
-    }
-
-    public List<JigType> list() {
-        return jigTypes.list();
-    }
-
-    public boolean empty() {
-        return jigTypes.empty();
-    }
-
-    public JigTypes jigTypes() {
-        return jigTypes.jigTypes();
+        super(jigTypes.list());
     }
 }

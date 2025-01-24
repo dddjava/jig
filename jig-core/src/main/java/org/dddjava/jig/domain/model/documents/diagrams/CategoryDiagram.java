316f08c9f4e11cc2c34012cf708bbccc4247ef0b
irof <irof.ocean@gmail.com>
2025-01-24 16:38:37 +0900

    👀区分図のパッケージでのグルーピングと振る舞い有無での色分けをやめる

--- jig-core/src/main/java/org/dddjava/jig/domain/model/documents/diagrams/CategoryDiagram.java
+++ jig-core/src/main/java/org/dddjava/jig/domain/model/documents/diagrams/CategoryDiagram.java
@@ -1,20 +1,16 @@
 package org.dddjava.jig.domain.model.documents.diagrams;
 
 import org.dddjava.jig.domain.model.data.classes.field.StaticFieldDeclaration;
-import org.dddjava.jig.domain.model.data.classes.type.TypeIdentifier;
 import org.dddjava.jig.domain.model.documents.documentformat.DocumentName;
 import org.dddjava.jig.domain.model.documents.documentformat.JigDocument;
 import org.dddjava.jig.domain.model.documents.stationery.*;
 import org.dddjava.jig.domain.model.information.domains.categories.CategoryType;
 import org.dddjava.jig.domain.model.information.domains.categories.CategoryTypes;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.StringJoiner;
-import java.util.function.Function;
 
-import static java.util.stream.Collectors.toMap;
+import static java.util.stream.Collectors.joining;
 
 /**
  * 区分図
@@ -40,15 +36,8 @@ public class CategoryDiagram implements DiagramSourceWriter {
             return DiagramSource.empty();
         }
 
-        Map<TypeIdentifier, CategoryType> map = categoryTypes.list().stream()
-                .collect(toMap(CategoryType::typeIdentifier, Function.identity()));
-
-        PackageStructure packageStructure = PackageStructure.from(new ArrayList<>(map.keySet()));
-
-        String structureText = packageStructure.toDotText(
-                typeIdentifier -> {
-                    CategoryType categoryType = map.get(typeIdentifier);
-
+        String structureText = categoryTypes.list().stream()
+                .map(categoryType -> {
                     StringJoiner categoryValues = new StringJoiner("</td></tr><tr><td border=\"1\">", "<tr><td border=\"1\">", "</td></tr>");
 
                     List<StaticFieldDeclaration> list = categoryType.values().list();
@@ -62,12 +51,13 @@ public class CategoryDiagram implements DiagramSourceWriter {
                     }
                     String categoryName = categoryType.nodeLabel("<br/>");
 
-                    Node node = Node.typeOf(typeIdentifier)
+                    return Node.typeOf(categoryType.typeIdentifier())
                             .html(categoryName, "<table border=\"0\" cellspacing=\"0\"><tr><td>" + categoryName + "</td></tr>" + categoryValues + "</table>")
-                            .url(typeIdentifier, JigDocument.EnumSummary);
-                    return node.as(categoryType.hasBehaviour() ? NodeRole.主役 : NodeRole.準主役);
-                }
-        );
+                            .url(categoryType.typeIdentifier(), JigDocument.EnumSummary)
+                            .as(NodeRole.主役);
+                })
+                .map(Node::asText)
+                .collect(joining("\n"));
 
         DocumentName documentName = DocumentName.of(JigDocument.CategoryDiagram);
         return DiagramSource.createDiagramSource(

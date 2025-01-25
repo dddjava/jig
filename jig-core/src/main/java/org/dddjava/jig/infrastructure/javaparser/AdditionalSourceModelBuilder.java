34178bc2eebdf649147a88e3b97033bb1eaa90eb
irof <irof.ocean@gmail.com>
2025-01-25 16:48:46 +0900

    追加読み取りの実装を見直すにあたり動作していない作りかけを削除

--- jig-core/src/main/java/org/dddjava/jig/infrastructure/javaparser/AdditionalSourceModelBuilder.java
+++ jig-core/src/main/java/org/dddjava/jig/infrastructure/javaparser/AdditionalSourceModelBuilder.java
@@ -2,40 +2,13 @@ package org.dddjava.jig.infrastructure.javaparser;
 
 import com.github.javaparser.ast.ImportDeclaration;
 import com.github.javaparser.ast.PackageDeclaration;
-import org.dddjava.jig.domain.model.data.classes.type.TypeIdentifier;
-import org.dddjava.jig.domain.model.sources.additional.AdditionalTypeModel;
-import org.dddjava.jig.domain.model.sources.additional.TypeAlias;
-import org.dddjava.jig.domain.model.sources.additional.TypeDescription;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.ArrayList;
-import java.util.List;
 
 class AdditionalSourceModelBuilder {
     private static final Logger logger = LoggerFactory.getLogger(AdditionalSourceModelBuilder.class);
 
-    private final List<ImportDeclaration> importDeclarations = new ArrayList<>();
-
     private PackageDeclaration packageDeclaration;
-    private String name;
-
-    public AdditionalTypeModel build() {
-        var typeIdentifier = TypeIdentifier.valueOf(packageDeclaration != null
-                ? packageDeclaration.getNameAsString() + '.' + name
-                : name);
-        var imports = importDeclarations.stream()
-                .map(importDeclaration -> importDeclaration.getNameAsString())
-                .toList();
-
-        return new AdditionalTypeModel(
-                typeIdentifier,
-                imports,
-                new TypeAlias(typeIdentifier, ""),
-                new TypeDescription(typeIdentifier, ""),
-                List.of()
-        );
-    }
 
     public void setPackage(PackageDeclaration packageDeclaration) {
         if (this.packageDeclaration != null) {
@@ -48,10 +21,8 @@ class AdditionalSourceModelBuilder {
     }
 
     public void addImport(ImportDeclaration importDeclaration) {
-        importDeclarations.add(importDeclaration);
     }
 
     public void setTypeName(String name) {
-        this.name = name;
     }
 }

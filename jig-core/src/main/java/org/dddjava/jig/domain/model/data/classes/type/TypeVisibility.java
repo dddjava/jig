feedfce699def62713bb271afccf9db771850d5d
irof <irof.ocean@gmail.com>
2025-01-25 16:58:04 +0900

    seeタグで書いておく

--- jig-core/src/main/java/org/dddjava/jig/domain/model/data/classes/type/TypeVisibility.java
+++ jig-core/src/main/java/org/dddjava/jig/domain/model/data/classes/type/TypeVisibility.java
@@ -5,7 +5,7 @@ package org.dddjava.jig.domain.model.data.classes.type;
  *
  * ソースコードではpublic,protected,privateが記述できますが、バイトコードではpublicか否かしかありません。
  *
- * https://docs.oracle.com/javase/specs/jvms/se17/html/jvms-4.html#jvms-4.1-200-E.1
+ * <a href="https://docs.oracle.com/javase/specs/jvms/se17/html/jvms-4.html#jvms-4.1-200-E.1">JVMS/Chapter 4. The class File Format/4.1. The ClassFile Structure/Table 4.1-B. Class access and property modifiers</a>
  */
 public enum TypeVisibility {
     PUBLIC,

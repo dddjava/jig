2b9ba917ac1ea7387e399c9defb1cf9e4922b8d1
irof <irof.ocean@gmail.com>
2025-01-26 15:57:35 +0900

    rename

--- jig-core/src/main/java/org/dddjava/jig/infrastructure/javaparser/JavaparserReader.java
+++ jig-core/src/main/java/org/dddjava/jig/infrastructure/javaparser/JavaparserReader.java
@@ -71,10 +71,10 @@ public class JavaparserReader implements JavaSourceReader {
                         .map(PackageDeclaration::getNameAsString)
                         .map(name -> name + ".")
                         .orElse("");
-                JavaparserClassVisitor typeVisitor = new JavaparserClassVisitor(packageName);
+                JavaparserClassVisitor classVisitor = new JavaparserClassVisitor(packageName);
                 JavaSourceDataBuilder builder = new JavaSourceDataBuilder();
-                cu.accept(typeVisitor, builder);
-                return typeVisitor.javaSourceModel();
+                cu.accept(classVisitor, builder);
+                return classVisitor.javaSourceModel();
             }
         } catch (Exception e) { // IOException以外にJavaparserの例外もキャッチする
             logger.warn("{} の読み取りに失敗しました。このファイルに必要な情報がある場合は欠落します。処理は続行します。", path, e);

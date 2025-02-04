63fbf2923e09816a8ccde2d163fc4d4db2627872
irof <irof.ocean@gmail.com>
2025-02-04 09:12:53 +0900

    📝リンク追加

--- jig-core/src/main/java/org/dddjava/jig/domain/model/data/types/JigTypeKind.java
+++ jig-core/src/main/java/org/dddjava/jig/domain/model/data/types/JigTypeKind.java
@@ -3,6 +3,9 @@ package org.dddjava.jig.domain.model.data.types;
 /**
  * 実装上の型の種類。
  * Java言語の型宣言におけるクラス（class, enum, record）とインタフェース（interface, @interface）に対応する。
+ *
+ * @see <a href="https://docs.oracle.com/javase/specs/jls/se17/html/jls-8.html">Classes</a>
+ * @see <a href="https://docs.oracle.com/javase/specs/jls/se17/html/jls-9.html">Interfaces</a>
  */
 public enum JigTypeKind {
     CLASS,

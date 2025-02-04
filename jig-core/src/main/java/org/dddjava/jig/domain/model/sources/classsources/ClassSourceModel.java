b89cf930818df54caa9152dc2c8de44be1706541
irof <irof.ocean@gmail.com>
2025-02-04 20:29:03 +0900

    未使用削除

--- jig-core/src/main/java/org/dddjava/jig/domain/model/sources/classsources/ClassSourceModel.java
+++ jig-core/src/main/java/org/dddjava/jig/domain/model/sources/classsources/ClassSourceModel.java
@@ -6,8 +6,4 @@ import java.util.Collection;
  * classファイル由来のソース
  */
 public record ClassSourceModel(Collection<ClassDeclaration> classDeclarations) {
-
-    public static ClassSourceModel from(Collection<ClassDeclaration> classDeclarations) {
-        return new ClassSourceModel(classDeclarations);
-    }
 }

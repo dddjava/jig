af660b6cfcfd6ab7f97cf18098dd228af540bf3f
irof <irof.ocean@gmail.com>
2025-02-06 22:06:10 +0900

    JigXxxDescription廃止

--- jig-core/src/main/java/org/dddjava/jig/domain/model/information/type/JigTypeTerms.java
+++ jig-core/src/main/java/org/dddjava/jig/domain/model/information/type/JigTypeTerms.java
@@ -1,14 +1,10 @@
 package org.dddjava.jig.domain.model.information.type;
 
-import org.dddjava.jig.adapter.html.dialect.JigTypeDescription;
 import org.dddjava.jig.domain.model.data.term.Term;
 
 import java.util.Collection;
 
 public record JigTypeTerms(Term term, Collection<Term> memberTerms) {
-    public JigTypeDescription jigTypeDescription() {
-        return new JigTypeDescription(term.title(), term.description());
-    }
 
     public boolean markedCore() {
         return typeTerm().title().startsWith("*");

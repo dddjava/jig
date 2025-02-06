b2df853a198df096651a7fc723aa573a3b3e546b
irof <irof.ocean@gmail.com>
2025-02-06 23:01:49 +0900

    Fieldに関することはAsmFieldVisitorで行う

--- jig-core/src/main/java/org/dddjava/jig/infrastructure/asm/AsmClassVisitor.java
+++ jig-core/src/main/java/org/dddjava/jig/infrastructure/asm/AsmClassVisitor.java
@@ -1,14 +1,10 @@
 package org.dddjava.jig.infrastructure.asm;
 
-import org.dddjava.jig.domain.model.data.classes.annotation.FieldAnnotation;
-import org.dddjava.jig.domain.model.data.classes.field.FieldDeclaration;
-import org.dddjava.jig.domain.model.data.classes.field.FieldType;
 import org.dddjava.jig.domain.model.data.types.*;
 import org.dddjava.jig.domain.model.sources.classsources.JigMemberBuilder;
 import org.dddjava.jig.domain.model.sources.classsources.JigMethodBuilder;
 import org.objectweb.asm.*;
 import org.objectweb.asm.signature.SignatureReader;
-import org.objectweb.asm.signature.SignatureVisitor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -132,44 +128,7 @@ class AsmClassVisitor extends ClassVisitor {
 
     @Override
     public FieldVisitor visitField(int access, String name, String descriptor, String signature, Object value) {
-        // インスタンスフィールド
-        FieldType fieldType;
-        TypeIdentifier fieldTypeIdentifier = typeDescriptorToIdentifier(descriptor);
-        if (signature == null) {
-            fieldType = new FieldType(fieldTypeIdentifier);
-        } else {
-            ArrayList<TypeIdentifier> typeParameters = new ArrayList<>();
-            new SignatureReader(signature).accept(
-                    new SignatureVisitor(AsmClassVisitor.this.api) {
-                        @Override
-                        public SignatureVisitor visitTypeArgument(char wildcard) {
-                            if (wildcard == '=') {
-                                return new SignatureVisitor(this.api) {
-                                    @Override
-                                    public void visitClassType(String name1) {
-                                        typeParameters.add(TypeIdentifier.valueOf(name1));
-                                    }
-                                };
-                            }
-                            return super.visitTypeArgument(wildcard);
-                        }
-                    }
-            );
-            TypeIdentifiers typeIdentifiers = new TypeIdentifiers(typeParameters);
-            fieldType = new FieldType(fieldTypeIdentifier, typeIdentifiers);
-        }
-
-        return new AsmFieldVisitor(this.api, it -> {
-            if ((access & Opcodes.ACC_STATIC) == 0) {
-                FieldDeclaration fieldDeclaration = jigMemberBuilder.addInstanceField(typeIdentifier, fieldType, name);
-                it.annotations.forEach(annotation -> {
-                    jigMemberBuilder.addFieldAnnotation(new FieldAnnotation(annotation, fieldDeclaration));
-                });
-            } else if (!name.equals("$VALUES")) {
-                // staticフィールドのうち、enumにコンパイル時に作成される $VALUES は除く
-                jigMemberBuilder.addStaticField(typeIdentifier, fieldTypeIdentifier, name);
-            }
-        });
+        return AsmFieldVisitor.from(this.api, access, name, descriptor, signature, this.typeIdentifier, this.jigMemberBuilder);
     }
 
     @Override
--- jig-core/src/main/java/org/dddjava/jig/infrastructure/asm/AsmFieldVisitor.java
+++ jig-core/src/main/java/org/dddjava/jig/infrastructure/asm/AsmFieldVisitor.java
@@ -1,11 +1,15 @@
 package org.dddjava.jig.infrastructure.asm;
 
 import org.dddjava.jig.domain.model.data.classes.annotation.Annotation;
+import org.dddjava.jig.domain.model.data.classes.annotation.FieldAnnotation;
+import org.dddjava.jig.domain.model.data.classes.field.FieldDeclaration;
+import org.dddjava.jig.domain.model.data.classes.field.FieldType;
 import org.dddjava.jig.domain.model.data.types.TypeIdentifier;
-import org.objectweb.asm.AnnotationVisitor;
-import org.objectweb.asm.Attribute;
-import org.objectweb.asm.FieldVisitor;
-import org.objectweb.asm.TypePath;
+import org.dddjava.jig.domain.model.data.types.TypeIdentifiers;
+import org.dddjava.jig.domain.model.sources.classsources.JigMemberBuilder;
+import org.objectweb.asm.*;
+import org.objectweb.asm.signature.SignatureReader;
+import org.objectweb.asm.signature.SignatureVisitor;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,6 +27,46 @@ class AsmFieldVisitor extends FieldVisitor {
         super(api);
         this.finisher = finisher;
         this.annotations = new ArrayList<>();
+    }
+
+    static AsmFieldVisitor from(final int api, int access, String name, String descriptor, String signature, TypeIdentifier typeIdentifier, JigMemberBuilder jigMemberBuilder) {
+        FieldType fieldType;
+        TypeIdentifier fieldTypeIdentifier = AsmClassVisitor.typeDescriptorToIdentifier(descriptor);
+        if (signature == null) {
+            fieldType = new FieldType(fieldTypeIdentifier);
+        } else {
+            ArrayList<TypeIdentifier> typeParameters = new ArrayList<>();
+            new SignatureReader(signature).accept(
+                    new SignatureVisitor(api) {
+                        @Override
+                        public SignatureVisitor visitTypeArgument(char wildcard) {
+                            if (wildcard == '=') {
+                                return new SignatureVisitor(this.api) {
+                                    @Override
+                                    public void visitClassType(String name1) {
+                                        typeParameters.add(TypeIdentifier.valueOf(name1));
+                                    }
+                                };
+                            }
+                            return super.visitTypeArgument(wildcard);
+                        }
+                    }
+            );
+            TypeIdentifiers typeIdentifiers = new TypeIdentifiers(typeParameters);
+            fieldType = new FieldType(fieldTypeIdentifier, typeIdentifiers);
+        }
+
+        return new AsmFieldVisitor(api, it -> {
+            if ((access & Opcodes.ACC_STATIC) == 0) {
+                FieldDeclaration fieldDeclaration = jigMemberBuilder.addInstanceField(typeIdentifier, fieldType, name);
+                it.annotations.forEach(annotation -> {
+                    jigMemberBuilder.addFieldAnnotation(new FieldAnnotation(annotation, fieldDeclaration));
+                });
+            } else if (!name.equals("$VALUES")) {
+                // staticフィールドのうち、enumにコンパイル時に作成される $VALUES は除く
+                jigMemberBuilder.addStaticField(typeIdentifier, fieldTypeIdentifier, name);
+            }
+        });
     }
 
     @Override

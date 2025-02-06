63a6e19051f99b597f39ea7b2140d61c175033dc
irof <irof.ocean@gmail.com>
2025-02-06 21:07:00 +0900

    📝コメント追加

--- jig-core/src/main/java/org/dddjava/jig/domain/model/data/types/JigAnnotationInstanceElement.java
+++ jig-core/src/main/java/org/dddjava/jig/domain/model/data/types/JigAnnotationInstanceElement.java
@@ -1,5 +1,11 @@
 package org.dddjava.jig.domain.model.data.types;
 
+/**
+ * JLSにおける `ElementValuePair`
+ * @param name
+ * @param value
+ * @see <a href="https://docs.oracle.com/javase/specs/jls/se17/html/jls-9.html#jls-ElementValuePair">...</a>
+ */
 public record JigAnnotationInstanceElement(String name, Object value) {
 
     public boolean matchName(String[] elementNames) {

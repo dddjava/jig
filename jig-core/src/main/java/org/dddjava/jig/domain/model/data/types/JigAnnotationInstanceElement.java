49dcec2a0692ed432e3677c23b3ef93f67b7d75f
irof <irof.ocean@gmail.com>
2025-02-06 21:09:53 +0900

    📝コメント追加

--- jig-core/src/main/java/org/dddjava/jig/domain/model/data/types/JigAnnotationInstanceElement.java
+++ jig-core/src/main/java/org/dddjava/jig/domain/model/data/types/JigAnnotationInstanceElement.java
@@ -2,8 +2,8 @@ package org.dddjava.jig.domain.model.data.types;
 
 /**
  * JLSにおける `ElementValuePair`
- * @param name
- * @param value
+ * @param name elementの名前
+ * @param value 現状は単純な値にだけ対応。アノテーションが入ったりもするのでどこまで考慮したものかと。
  * @see <a href="https://docs.oracle.com/javase/specs/jls/se17/html/jls-9.html#jls-ElementValuePair">...</a>
  */
 public record JigAnnotationInstanceElement(String name, Object value) {

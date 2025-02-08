a763f0e155b927506164db72279e18404b2bd4d8
irof <irof.ocean@gmail.com>
2025-02-09 00:49:53 +0900

    不要になったFunctionalInterfaceを削除

--- jig-core/src/main/java/org/dddjava/jig/domain/model/data/classes/method/JigMethodFinder.java
+++
@@ -1,8 +0,0 @@
-package org.dddjava.jig.domain.model.data.classes.method;
-
-import java.util.Optional;
-
-public interface JigMethodFinder {
-
-    Optional<JigMethod> find(MethodIdentifier methodIdentifier);
-}

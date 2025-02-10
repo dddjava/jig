719fd5c3ae4d129e1cfec846a376b162aa27f631
irof <irof.ocean@gmail.com>
2025-02-10 22:23:10 +0900

    正規表現の事前コンパイル

--- jig-core/src/main/java/org/dddjava/jig/domain/model/information/validations/Validations.java
+++ jig-core/src/main/java/org/dddjava/jig/domain/model/information/validations/Validations.java
@@ -7,6 +7,7 @@ import org.dddjava.jig.domain.model.information.type.JigTypes;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -14,6 +15,7 @@ import java.util.stream.Stream;
  * バリデーション一覧
  */
 public class Validations {
+    private static final Pattern ANNOTATION_PATTERN = Pattern.compile("((javax|jakarta).validation|org.hibernate.validator).+");
 
     List<Validation> list;
 
@@ -34,12 +36,13 @@ public class Validations {
                 .map(MethodAnnotations::list)
                 .flatMap(List::stream)
                 // TODO 正規表現の絞り込みをやめる
-                .filter(annotation -> annotation.annotationType().fullQualifiedName().matches("((javax|jakarta).validation|org.hibernate.validator).+"))
+                .filter(annotation -> ANNOTATION_PATTERN.matcher(annotation.annotationType().fullQualifiedName()).matches())
                 .map(ValidationAnnotatedMember::new)
                 .map(Validation::new);
         Stream<Validation> fieldStream = jigType.jigTypeMembers().jigFieldHeaders().stream()
                 .flatMap(jigFieldHeader -> jigFieldHeader.jigFieldAttribute().declarationAnnotations().stream()
-                        .filter(jigAnnotationReference -> jigAnnotationReference.id().fullQualifiedName().matches("((javax|jakarta).validation|org.hibernate.validator).+"))
+                        // TODO 正規表現の絞り込みをやめる
+                        .filter(jigAnnotationReference -> ANNOTATION_PATTERN.matcher(jigAnnotationReference.id().fullQualifiedName()).matches())
                         .map(jigAnnotationReference -> {
                             return new Validation(
                                     jigType.identifier(),

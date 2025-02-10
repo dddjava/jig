c46853d79748a8e066de309a12e544c486f8f24f
irof <irof.ocean@gmail.com>
2025-02-10 22:34:08 +0900

    使用しなくなったFieldAnnotation削除

--- jig-core/src/main/java/org/dddjava/jig/domain/model/data/classes/field/JigField.java
+++ jig-core/src/main/java/org/dddjava/jig/domain/model/data/classes/field/JigField.java
@@ -1,9 +1,5 @@
 package org.dddjava.jig.domain.model.data.classes.field;
 
-import org.dddjava.jig.domain.model.data.classes.annotation.Annotation;
-import org.dddjava.jig.domain.model.data.classes.annotation.AnnotationDescription;
-import org.dddjava.jig.domain.model.data.classes.annotation.FieldAnnotation;
-import org.dddjava.jig.domain.model.data.classes.annotation.FieldAnnotations;
 import org.dddjava.jig.domain.model.data.classes.type.ParameterizedType;
 import org.dddjava.jig.domain.model.data.members.JigFieldHeader;
 import org.dddjava.jig.domain.model.data.types.JigTypeReference;
@@ -12,12 +8,10 @@ import org.dddjava.jig.domain.model.data.types.TypeIdentifier;
 public class JigField {
     private final JigFieldHeader jigFieldHeader;
     FieldDeclaration fieldDeclaration;
-    FieldAnnotations fieldAnnotations;
 
-    public JigField(JigFieldHeader jigFieldHeader, FieldDeclaration fieldDeclaration, FieldAnnotations fieldAnnotations) {
+    public JigField(JigFieldHeader jigFieldHeader, FieldDeclaration fieldDeclaration) {
         this.jigFieldHeader = jigFieldHeader;
         this.fieldDeclaration = fieldDeclaration;
-        this.fieldAnnotations = fieldAnnotations;
     }
 
     public JigTypeReference jigTypeReference() {
@@ -32,7 +26,7 @@ public class JigField {
     }
 
     public static JigField from(JigFieldHeader jigFieldHeader) {
-        // 互換のため無理矢理JigFieldHeaderから生成している状態。FieldDeclarationやFieldAnnotationsの使用箇所を直せばもっと素直にできるはず
+        // 互換のため無理矢理JigFieldHeaderから生成している状態。FieldDeclarationの使用箇所を直せばもっと素直にできるはず
         var fieldDeclaration = new FieldDeclaration(
                 jigFieldHeader.id().declaringTypeIdentifier(),
                 new FieldType(jigReferenceToParameterizedType(jigFieldHeader.jigTypeReference())),
@@ -40,30 +34,12 @@ public class JigField {
         );
         return new JigField(
                 jigFieldHeader,
-                fieldDeclaration,
-                new FieldAnnotations(
-                        jigFieldHeader.jigFieldAttribute().declarationAnnotations().stream()
-                                .map(jigAnnotationReference -> {
-                                    var description = new AnnotationDescription();
-                                    jigAnnotationReference.elements()
-                                            .forEach(element -> description.addParam(element.name(), element.valueAsString()));
-                                    return new FieldAnnotation(
-                                            new Annotation(
-                                                    jigAnnotationReference.id(),
-                                                    description
-                                            ),
-                                            fieldDeclaration
-                                    );
-                                })
-                                .toList()));
+                fieldDeclaration
+        );
     }
 
     public TypeIdentifier typeIdentifier() {
         return jigFieldHeader.jigTypeReference().id();
-    }
-
-    public FieldAnnotations fieldAnnotations() {
-        return fieldAnnotations;
     }
 
     public String nameText() {

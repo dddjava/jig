e675d3699f30acf68c6f334ffcb1525beaa970fb
irof <irof.ocean@gmail.com>
2025-02-12 09:40:51 +0900

    MethodDeclarationの脱MethodIdentifier

--- jig-core/src/main/java/org/dddjava/jig/domain/model/data/classes/method/MethodDeclaration.java
+++ jig-core/src/main/java/org/dddjava/jig/domain/model/data/classes/method/MethodDeclaration.java
@@ -12,15 +12,14 @@ public class MethodDeclaration {
     private final MethodReturn methodReturn;
     private final JigMethodIdentifier jigMethodIdentifier;
     private final TypeIdentifier declaringType;
-
-    MethodIdentifier methodIdentifier;
+    private final MethodSignature methodSignature;
 
     public MethodDeclaration(TypeIdentifier declaringType, MethodSignature methodSignature, MethodReturn methodReturn) {
         this.jigMethodIdentifier = JigMethodIdentifier.from(declaringType, methodSignature.methodName(),
                 methodSignature.arguments().stream().map(ParameterizedType::typeIdentifier).toList());
         this.methodReturn = methodReturn;
         this.declaringType = declaringType;
-        this.methodIdentifier = new MethodIdentifier(this.declaringType, methodSignature);
+        this.methodSignature = methodSignature;
     }
 
     /**
@@ -51,7 +50,7 @@ public class MethodDeclaration {
     }
 
     public MethodSignature methodSignature() {
-        return methodIdentifier.methodSignature();
+        return methodSignature;
     }
 
     public MethodReturn methodReturn() {

be0fa64d9528e7b17d60b1d7057c7c408bc7bc9c
irof <irof.ocean@gmail.com>
2025-02-15 14:00:24 +0900

    📝コメント

--- jig-core/src/main/java/org/dddjava/jig/domain/model/information/types/JigTypeTerms.java
+++ jig-core/src/main/java/org/dddjava/jig/domain/model/information/types/JigTypeTerms.java
@@ -4,6 +4,9 @@ import org.dddjava.jig.domain.model.data.term.Term;
 
 import java.util.Collection;
 
+/**
+ * 型およびメンバの用語を保持する
+ */
 public record JigTypeTerms(Term term, Collection<Term> memberTerms) {
 
     public boolean markedCore() {

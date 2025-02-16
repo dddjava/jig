b8d7d0805c3ec3f73ef4e231abdfafe100f7261f
irof <irof.ocean@gmail.com>
2025-02-16 22:47:27 +0900

    📝コメント

--- jig-core/src/main/java/org/dddjava/jig/domain/model/data/members/JigMethodFlag.java
+++ jig-core/src/main/java/org/dddjava/jig/domain/model/data/members/JigMethodFlag.java
@@ -7,17 +7,50 @@ package org.dddjava.jig.domain.model.data.members;
  */
 public enum JigMethodFlag {
     SYNCHRONIZED,
+    /**
+     * コンパイラによって生成されるブリッジメソッド
+     */
     BRIDGE,
+    /**
+     * 可変長引数を持つ
+     */
     VARARGS,
+    /**
+     * nativeメソッド
+     */
     NATIVE,
+    /**
+     * abstractメソッド
+     */
     ABSTRACT,
+    /**
+     * strictfpメソッド。廃止されているが定義としてあるので列挙はしておく。
+     */
     STRICT,
+    /**
+     * コンパイラによって生成される合成メソッド
+     */
     SYNTHETIC,
 
     // -- JIG独自フラグ
+    /**
+     * インスタンスイニシャライザおよびコンストラクタ
+     */
     INITIALIZER,
+    /**
+     * クラスイニシャライザ
+     */
     STATIC_INITIALIZER,
+    /**
+     * enumを定義した際に生成されるvalueOfなど
+     */
     ENUM_SUPPORT,
+    /**
+     * lambda式を記述した際にコンパイラによって生成される
+     */
     LAMBDA_SUPPORT,
+    /**
+     * recordを定義した際に生成されるアクセサ
+     */
     RECORD_COMPONENT_ACCESSOR
 }

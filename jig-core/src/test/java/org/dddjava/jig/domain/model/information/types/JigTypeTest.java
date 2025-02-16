2fea0305d7a11f53654db89fd345034039ba4c18
irof <irof.ocean@gmail.com>
2025-02-16 16:52:54 +0900

    🟥複数値のアノテーションの場合に {} がついていて邪魔で落ちるテスト

---
+++ jig-core/src/test/java/org/dddjava/jig/domain/model/information/types/JigTypeTest.java
@@ -0,0 +1,47 @@
+package org.dddjava.jig.domain.model.information.types;
+
+import org.dddjava.jig.domain.model.data.types.TypeIdentifier;
+import org.junit.jupiter.api.Test;
+import org.springframework.web.bind.annotation.RequestMapping;
+import testing.TestSupport;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class JigTypeTest {
+
+    @Test
+    void アノテーションの値取得_String() {
+        var jigType = TestSupport.buildJigType(MyAnnotatedClass.class);
+
+        var actual = jigType.annotationValueOf(TypeIdentifier.from(RequestMapping.class), "name")
+                .orElseThrow(); // 取れなければテスト失敗でよい
+
+        assertEquals("hoge", actual);
+    }
+
+    @Test
+    void アノテーションの値取得_String配列の単一値() {
+        var jigType = TestSupport.buildJigType(MyAnnotatedClass.class);
+
+        var actual = jigType.annotationValueOf(TypeIdentifier.from(RequestMapping.class), "value")
+                .orElseThrow(); // 取れなければテスト失敗でよい
+
+        assertEquals("{id}", actual);
+    }
+
+    @Test
+    void アノテーションの値取得_aliasなど片方だけとればいいものは最初にマッチしたものがとれる() {
+        var jigType = TestSupport.buildJigType(MyAnnotatedClass.class);
+
+        var actual = jigType.annotationValueOf(TypeIdentifier.from(RequestMapping.class), "none", "value", "path")
+                .orElseThrow(); // 取れなければテスト失敗でよい
+
+        // pathの値がとれている。
+        // つまり引数の順ではなくアノテーションに書いた順番になっているが、一つしか指定できないものでの使用を想定しているのでよしとする。
+        assertEquals("/hoge", actual);
+    }
+
+    @RequestMapping(name = "hoge", path = "/hoge", value = "{id}")
+    private static class MyAnnotatedClass {
+    }
+}

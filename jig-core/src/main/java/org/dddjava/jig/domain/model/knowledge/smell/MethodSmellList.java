f052b8f0f6d4d47d172255fd86d7309c327af725
irof <irof.ocean@gmail.com>
2025-02-21 00:45:11 +0900

    収集時点の並び順には意味がないのでCollectionに変更

--- jig-core/src/main/java/org/dddjava/jig/domain/model/knowledge/smell/MethodSmellList.java
+++ jig-core/src/main/java/org/dddjava/jig/domain/model/knowledge/smell/MethodSmellList.java
@@ -2,6 +2,7 @@ package org.dddjava.jig.domain.model.knowledge.smell;
 
 import org.dddjava.jig.domain.model.information.types.JigTypes;
 
+import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -9,7 +10,7 @@ import java.util.stream.Collectors;
 /**
  * メソッドの不吉なにおい一覧
  */
-public record MethodSmellList(List<MethodSmell> list) {
+public record MethodSmellList(Collection<MethodSmell> smells) {
 
     public static MethodSmellList from(JigTypes jigTypes) {
         return new MethodSmellList(jigTypes.stream()
@@ -20,7 +21,7 @@ public record MethodSmellList(List<MethodSmell> list) {
     }
 
     public List<MethodSmell> list() {
-        return list.stream()
+        return smells.stream()
                 .sorted(Comparator.comparing(methodSmell -> methodSmell.method().jigMethodIdentifier().value()))
                 .collect(Collectors.toList());
     }

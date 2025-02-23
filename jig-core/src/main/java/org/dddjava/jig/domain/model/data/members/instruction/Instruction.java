2d81915289374f2381a2b30b289df058b8c0e9f0
irof <irof.ocean@gmail.com>
2025-02-23 10:43:28 +0900

    sealed

--- jig-core/src/main/java/org/dddjava/jig/domain/model/data/members/instruction/Instruction.java
+++ jig-core/src/main/java/org/dddjava/jig/domain/model/data/members/instruction/Instruction.java
@@ -3,5 +3,6 @@ package org.dddjava.jig.domain.model.data.members.instruction;
 /// コードで実行される命令
 /// 現在、命令に共通のドメインロジックはないためメソッドを持たないが、
 /// [Instructions] で扱えるものを制限するためにマーカーインタフェースとして置く。
-public interface Instruction {
+public sealed interface Instruction
+        permits ClassReference, FieldInstruction, InvokeDynamicInstruction, InvokedMethod, SimpleInstruction {
 }

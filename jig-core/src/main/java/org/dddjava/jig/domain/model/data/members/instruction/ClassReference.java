003d5b020271ef339568ae38ad73464adec1f648
irof <irof.ocean@gmail.com>
2025-02-26 09:57:10 +0900

    📝追加

--- jig-core/src/main/java/org/dddjava/jig/domain/model/data/members/instruction/ClassReference.java
+++ jig-core/src/main/java/org/dddjava/jig/domain/model/data/members/instruction/ClassReference.java
@@ -4,6 +4,11 @@ import org.dddjava.jig.domain.model.data.types.TypeIdentifier;
 
 import java.util.stream.Stream;
 
+/**
+ * クラス参照
+ *
+ * .classなどでの使用。型引数は付かないので [TypeIdentifier] で扱う。
+ */
 public record ClassReference(TypeIdentifier typeIdentifier) implements Instruction {
 
     @Override

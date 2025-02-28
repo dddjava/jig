5cc9ed1a80055af9aad905696d0c5dca78ac8617
irof <irof.ocean@gmail.com>
2025-03-01 01:37:20 +0900

    マルチプロジェクト構成のテストを作り直し
    Gradle8でProjectBuilderを使用したマルチプロジェクト構築がうまくいかなくなったため、TestKitを使う方に切り替え。

--- jig-gradle-plugin/src/test/java/jig/JigPluginFunctionalTest.java
+++ jig-gradle-plugin/src/test/java/jig/JigPluginFunctionalTest.java
@@ -79,8 +79,90 @@ public class JigPluginFunctionalTest {
         assertTrue(testProjectDir.resolve(Path.of("build", "jig", "index.html")).toFile().isFile());
     }
 
+    @ParameterizedTest
+    @MethodSource("supportGradleVersion")
+    void マルチプロジェクト構成でソースディレクトリを収集できる(String version) throws IOException {
+        settingsGradle("""
+                rootProject.name = 'my-test'
+                include 'a', 'b', 'c', 'c-a', 'c-b', 'c-c', 'd'
+                """);
+        // aにプラグイン適用
+        buildGradle("a", """
+                plugins {
+                    id 'java'
+                    id 'org.dddjava.jig-gradle-plugin'
+                }
+                dependencies {
+                    implementation project(':b');
+                    implementation project(':c');
+                    implementation project(':d');
+                }
+                """);
+        // bは非javaプロジェクト。
+        buildGradle("b", """
+                plugins {
+                }
+                """);
+        // cはjavaプロジェクトかつc-a,c-b,c-cに依存する
+        // 複数階層とimplementation以外を拾わない検証
+        buildGradle("c", """
+                plugins {
+                    id 'java'
+                }
+                dependencies {
+                    testImplementation project(':c-a');
+                    implementation project(':c-b');
+                    runtimeOnly project(':c-c');
+                }
+                """);
+        buildGradle("c-a", "plugins { id 'java' }");
+        buildGradle("c-b", "plugins { id 'java' }");
+        buildGradle("c-c", "plugins { id 'java' }");
+        // dはjavaプロジェクト
+        // 複数のjavaプロジェクトに依存しても取得できる検証用
+        buildGradle("d", """
+                plugins {
+                     id 'java'
+                }
+                """);
+
+        var result = runner(version).build();
+
+        var taskResult = Objects.requireNonNull(result.task(":a:jigReports"));
+        assertEquals(TaskOutcome.SUCCESS, taskResult.getOutcome());
+        assertTrue(result.getOutput().contains("[JIG] all JIG documents completed: "), result.getOutput());
+
+        // jig-coreのread paths... の中に出力されているのでそこで検証する。
+        // TODO plugin側で解決したパスを出力してそこで検証する形にしたい
+
+        String output = result.getOutput();
+        // 含まれるもの
+        // FIXME java pluginが適用されていないbが入っているのは想定外
+        assertAll(Stream.of("a", "b", "c", "c-b", "d")
+                .flatMap(project -> Stream.of(
+                        project + "/src/main/java",
+                        project + "/build/classes/java/main",
+                        project + "/build/resources/main"
+                ))
+                .map(path -> () -> assertTrue(output.contains(path), path)));
+        // 含まれないもの
+        assertAll(Stream.of("c-a", "c-c")
+                .flatMap(project -> Stream.of(
+                        project + "/src/main/java",
+                        project + "/build/classes/java/main",
+                        project + "/build/resources/main"
+                ))
+                .map(path -> () -> assertFalse(output.contains(path), path)));
+    }
+
     private void settingsGradle(String buildScript) throws IOException {
         Files.writeString(testProjectDir.resolve("settings.gradle"), buildScript);
+    }
+
+    private void buildGradle(String path, String buildScript) throws IOException {
+        Path subprojectDir = testProjectDir.resolve(path);
+        Files.createDirectories(subprojectDir);
+        Files.writeString(subprojectDir.resolve("build.gradle"), buildScript);
     }
 
     private void buildGradle(String buildScript) throws IOException {

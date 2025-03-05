cd2b3e0ee8ce279ca94b84541f6828f091093f79
irof <irof.ocean@gmail.com>
2025-03-06 00:51:37 +0900

    使っていないので削除

--- jig-core/src/main/java/org/dddjava/jig/domain/model/sources/classsources/RecordComponentDefinition.java
+++
@@ -1,9 +0,0 @@
-package org.dddjava.jig.domain.model.sources.classsources;
-
-import org.dddjava.jig.domain.model.data.types.TypeIdentifier;
-
-public record RecordComponentDefinition(
-        String name,
-        TypeIdentifier typeIdentifier
-) {
-}

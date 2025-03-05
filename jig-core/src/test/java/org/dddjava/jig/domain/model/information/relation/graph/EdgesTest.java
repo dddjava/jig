c5089f51dd150b2b701daf81d4b6bedbee16a77f
irof <irof.ocean@gmail.com>
2025-03-05 16:47:25 +0900

    テストも移動

--- jig-core/src/test/java/org/dddjava/jig/domain/model/information/relation/graph/EdgesTest.java
+++ jig-core/src/test/java/org/dddjava/jig/domain/model/information/relation/graph/EdgesTest.java
@@ -1,7 +1,5 @@
-package org.dddjava.jig.domain.model.information.relation;
+package org.dddjava.jig.domain.model.information.relation.graph;
 
-import org.dddjava.jig.domain.model.information.relation.graph.Edge;
-import org.dddjava.jig.domain.model.information.relation.graph.Edges;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;

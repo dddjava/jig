1f246835ff888e824940bf8651e7a9a0c5931962
irof <irof.ocean@gmail.com>
2025-03-08 19:06:42 +0900

    packageRelationがedgeを持つようにする

--- jig-core/src/main/java/org/dddjava/jig/domain/model/information/relation/packages/PackageRelation.java
+++ jig-core/src/main/java/org/dddjava/jig/domain/model/information/relation/packages/PackageRelation.java
@@ -2,11 +2,24 @@ package org.dddjava.jig.domain.model.information.relation.packages;
 
 import org.dddjava.jig.domain.model.data.packages.PackageDepth;
 import org.dddjava.jig.domain.model.data.packages.PackageIdentifier;
+import org.dddjava.jig.domain.model.information.relation.graph.Edge;
 
 /**
  * パッケージの依存関係
  */
-public record PackageRelation(PackageIdentifier from, PackageIdentifier to) {
+public record PackageRelation(Edge<PackageIdentifier> edge) {
+
+    public PackageRelation(PackageIdentifier from, PackageIdentifier to) {
+        this(new Edge<>(from, to));
+    }
+
+    public PackageIdentifier from() {
+        return edge.from();
+    }
+
+    public PackageIdentifier to() {
+        return edge.to();
+    }
 
     public PackageRelation applyDepth(PackageDepth packageDepth) {
         return new PackageRelation(from().applyDepth(packageDepth), to().applyDepth(packageDepth));

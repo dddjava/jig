7287ea12f08f36ae9a7a9253e3e76149a98e260a
irof <irof.ocean@gmail.com>
2025-03-12 13:36:59 +0900

    add assertion

--- jig-core/src/test/java/org/dddjava/jig/JigExecutorTest.java
+++ jig-core/src/test/java/org/dddjava/jig/JigExecutorTest.java
@@ -1,11 +1,15 @@
 package org.dddjava.jig;
 
+import org.dddjava.jig.domain.model.documents.documentformat.JigDocument;
 import org.dddjava.jig.domain.model.sources.SourceBasePaths;
 import org.dddjava.jig.infrastructure.configuration.Configuration;
 import org.junit.jupiter.api.Test;
 import testing.JigServiceTest;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @JigServiceTest
 class JigExecutorTest {
@@ -16,6 +20,10 @@ class JigExecutorTest {
                 configuration,
                 SourceBasePaths
         );
+
+        List<JigDocument> actualDocuments = actual.stream().map(handleResult -> handleResult.jigDocument).toList();
+        // canonicalのすべてが処理されている
+        assertTrue(actualDocuments.containsAll(JigDocument.canonical()), actualDocuments::toString);
 
         for (HandleResult handleResult : actual) {
             assertFalse(handleResult.failure(), () -> handleResult.toString());

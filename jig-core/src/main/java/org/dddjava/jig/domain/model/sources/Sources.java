465b8a80f029120c8e885826cf44d9a90c61155a
irof <irof.ocean@gmail.com>
2025-03-14 13:03:17 +0900

    convert to record

--- jig-core/src/main/java/org/dddjava/jig/domain/model/sources/Sources.java
+++ jig-core/src/main/java/org/dddjava/jig/domain/model/sources/Sources.java
@@ -6,21 +6,7 @@ import org.dddjava.jig.domain.model.sources.javasources.JavaSources;
 /**
  * 生ソース
  */
-public class Sources {
-
-    private final SourceBasePaths sourceBasePaths;
-    private final JavaSources javaSources;
-    private final ClassSources classSources;
-
-    public Sources(SourceBasePaths sourceBasePaths, JavaSources javaSources, ClassSources classSources) {
-        this.sourceBasePaths = sourceBasePaths;
-        this.javaSources = javaSources;
-        this.classSources = classSources;
-    }
-
-    public JavaSources javaSources() {
-        return javaSources;
-    }
+public record Sources(SourceBasePaths sourceBasePaths, JavaSources javaSources, ClassSources classSources) {
 
     public boolean emptyClassSources() {
         return classSources.nothing();
@@ -28,13 +14,5 @@ public class Sources {
 
     public boolean emptyJavaSources() {
         return javaSources.nothing();
-    }
-
-    public ClassSources classSources() {
-        return classSources;
-    }
-
-    public SourceBasePaths sourceBasePaths() {
-        return sourceBasePaths;
     }
 }

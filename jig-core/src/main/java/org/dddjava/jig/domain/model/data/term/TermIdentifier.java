44fe0e3f1a7ccf2d3d167bec07087297d3f3a612
irof <irof.ocean@gmail.com>
2025-03-18 20:47:01 +0900

    record

--- jig-core/src/main/java/org/dddjava/jig/domain/model/data/term/TermIdentifier.java
+++ jig-core/src/main/java/org/dddjava/jig/domain/model/data/term/TermIdentifier.java
@@ -1,30 +1,9 @@
 package org.dddjava.jig.domain.model.data.term;
 
-import java.util.Objects;
-
 /**
  * 用語の識別子
  */
-public class TermIdentifier {
-
-    final String value;
-
-    public TermIdentifier(String value) {
-        this.value = value;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        TermIdentifier that = (TermIdentifier) o;
-        return Objects.equals(value, that.value);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(value);
-    }
+public record TermIdentifier(String value) {
 
     public String asText() {
         return value;

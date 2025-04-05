991e169c99808eb5ef5fb4dbabb3b7935a69e772
irof <irof.ocean@gmail.com>
2025-04-05 21:36:19 +0900

    convert to record

--- jig-core/src/main/java/org/dddjava/jig/domain/model/documents/stationery/AdditionalText.java
+++ jig-core/src/main/java/org/dddjava/jig/domain/model/documents/stationery/AdditionalText.java
@@ -1,18 +1,9 @@
 package org.dddjava.jig.domain.model.documents.stationery;
 
-public class AdditionalText {
-    String value;
-
-    public AdditionalText(String value) {
-        this.value = value;
-    }
+public record AdditionalText(String value) {
 
     public static AdditionalText empty() {
         return new AdditionalText(null);
-    }
-
-    public String value() {
-        return value;
     }
 
     public boolean enable() {

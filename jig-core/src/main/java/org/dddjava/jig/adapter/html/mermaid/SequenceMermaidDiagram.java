ea2bae29f1388c7ca6bc0869d1c91e69770ac8a9
irof <irof.ocean@gmail.com>
2025-04-27 23:21:31 +0900

    htmlから呼ぶためのやつ

--- jig-core/src/main/java/org/dddjava/jig/adapter/html/mermaid/SequenceMermaidDiagram.java
+++ jig-core/src/main/java/org/dddjava/jig/adapter/html/mermaid/SequenceMermaidDiagram.java
@@ -1,6 +1,7 @@
 package org.dddjava.jig.adapter.html.mermaid;
 
 import org.dddjava.jig.domain.model.data.types.TypeIdentifier;
+import org.dddjava.jig.domain.model.information.members.JigMethod;
 import org.dddjava.jig.domain.model.information.members.JigMethodDeclaration;
 
 import java.util.stream.Collectors;
@@ -9,6 +10,10 @@ import java.util.stream.Collectors;
  * メソッド呼び出しのシーケンス図を生成する
  */
 public class SequenceMermaidDiagram {
+
+    public String textFor(JigMethod jigMethod) {
+        return mermaidSequenceDiagram(jigMethod.jigMethodDeclaration());
+    }
 
     /**
      * メソッド定義からMermaid形式のシーケンス図を生成する
@@ -25,21 +30,21 @@ public class SequenceMermaidDiagram {
             TypeIdentifier returnType = methodCall.returnType();
 
             // Format arguments
-            String args = methodCall.argumentTypes().isEmpty() ? 
-                          "()" : 
-                          "(" + methodCall.argumentTypes().stream()
-                                .map(TypeIdentifier::asSimpleText)
-                                .collect(Collectors.joining(", ")) + ")";
+            String args = methodCall.argumentTypes().isEmpty() ?
+                    "()" :
+                    "(" + methodCall.argumentTypes().stream()
+                            .map(TypeIdentifier::asSimpleText)
+                            .collect(Collectors.joining(", ")) + ")";
 
             // Add call line
             sb.append("    ").append(caller.asSimpleText())
-              .append(" ->>+ ").append(callee.asSimpleText())
-              .append(": ").append(methodName).append(args).append("\n");
+                    .append(" ->>+ ").append(callee.asSimpleText())
+                    .append(": ").append(methodName).append(args).append("\n");
 
             // Add return line
             sb.append("    ").append(callee.asSimpleText())
-              .append(" -->>- ").append(caller.asSimpleText())
-              .append(": ").append(returnType.asSimpleText()).append("\n");
+                    .append(" -->>- ").append(caller.asSimpleText())
+                    .append(": ").append(returnType.asSimpleText()).append("\n");
         });
 
         return sb.toString();

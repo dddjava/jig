4cce721416d66dfc7a688ac490f3d54462226954
irof <irof.ocean@gmail.com>
2025-05-24 20:43:31 +0900

    LambdaExpressionCallがDynamicMethodCallをそのまま保持するようにしておく

--- jig-core/src/main/java/org/dddjava/jig/domain/model/data/members/instruction/LambdaExpressionCall.java
+++ jig-core/src/main/java/org/dddjava/jig/domain/model/data/members/instruction/LambdaExpressionCall.java
@@ -2,43 +2,40 @@ package org.dddjava.jig.domain.model.data.members.instruction;
 
 import org.dddjava.jig.domain.model.data.types.TypeIdentifier;
 
-import java.util.List;
 import java.util.stream.Stream;
 
 /**
  * Lambda式呼び出し
  *
  * {@link DynamicMethodCall} の特化型で、呼び出されているLambdaの {@link Instructions} を保持する。
- *
- * @param methodCall lambda式のコンパイルにより生成される合成メソッド。実装依存だが `lambda$methodName$0` のような名前になる。
+ * originのmethodCallはlambda式のコンパイルにより生成される合成メソッド。実装依存だが `lambda$methodName$0` のような名前になる。
  */
-public record LambdaExpressionCall(MethodCall methodCall, TypeIdentifier returnType,
-                                   List<TypeIdentifier> argumentTypes,
-                                   Instructions instructions) implements Instruction {
+public record LambdaExpressionCall(DynamicMethodCall origin,
+                                   Instructions lambdaExpressionInstructions) implements Instruction {
     public static LambdaExpressionCall from(DynamicMethodCall origin, Instructions instructions) {
-        return new LambdaExpressionCall(origin.methodCall(), origin.returnType(), origin.argumentTypes(), instructions);
+        return new LambdaExpressionCall(origin, instructions);
     }
 
     @Override
     public Stream<MethodCall> findMethodCall() {
-        return Stream.of(methodCall());
+        return Stream.of(origin.methodCall());
     }
 
     @Override
     public Stream<MethodCall> lambdaInlinedMethodCallStream() {
         // methodCallは合成メソッドなので使用しない
-        return instructions.lambdaInlinedMethodCallStream();
+        return lambdaExpressionInstructions.lambdaInlinedMethodCallStream();
     }
 
     @Override
     public Stream<TypeIdentifier> streamAssociatedTypes() {
         return Stream.of(
-                methodCall().streamAssociatedTypes(),
-                argumentTypes().stream(),
+                origin.methodCall().streamAssociatedTypes(),
+                origin.argumentTypes().stream(),
                 // voidは返さなくていいと思うんだ
-                Stream.of(returnType()),
+                Stream.of(origin.returnType()),
                 // lambdaの中身
-                instructions.associatedTypeStream()
+                lambdaExpressionInstructions.associatedTypeStream()
         ).flatMap(stream -> stream);
     }
 }

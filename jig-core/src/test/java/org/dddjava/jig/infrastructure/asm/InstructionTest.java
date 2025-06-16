4c362db8c6490d3ad8cfc1e8a55e7e2d92cc054a
irof <irof.ocean@gmail.com>
2025-06-16 17:41:45 +0900

    共通のヘルパーメソッドを使用

--- jig-core/src/test/java/org/dddjava/jig/infrastructure/asm/InstructionTest.java
+++ jig-core/src/test/java/org/dddjava/jig/infrastructure/asm/InstructionTest.java
@@ -1,6 +1,5 @@
 package org.dddjava.jig.infrastructure.asm;
 
-import org.dddjava.jig.domain.model.information.members.JigMethod;
 import org.junit.jupiter.api.Test;
 import stub.domain.model.relation.MethodInstructionTestStub;
 import testing.TestSupport;
@@ -11,7 +10,7 @@ public class InstructionTest {
 
     @Test
     void メソッドの使用しているメソッドが取得できる_通常のメソッド呼び出し() throws Exception {
-        var jigMethod = jigMethod("method(MethodArgument)");
+        var jigMethod = TestSupport.JigMethod準備(MethodInstructionTestStub.class, "method");
         assertEquals(
                 "[InstructionField.invokeMethod(), UsedInstructionMethodReturn.chainedInvokeMethod()]",
                 jigMethod.usingMethods().asSimpleTextSorted()
@@ -20,7 +19,7 @@ public class InstructionTest {
 
     @Test
     void メソッドの使用しているメソッドが取得できる_メソッド参照() throws Exception {
-        var jigMethod = jigMethod("methodRef()");
+        var jigMethod = TestSupport.JigMethod準備(MethodInstructionTestStub.class, "methodRef");
         assertEquals(
                 "[MethodReference.referenceMethod()]",
                 jigMethod.usingMethods().asSimpleTextSorted()
@@ -29,18 +28,11 @@ public class InstructionTest {
 
     @Test
     void メソッドの使用しているメソッドが取得できる_lambda式() throws Exception {
-        var jigMethod = jigMethod("lambda()");
+        var jigMethod = TestSupport.JigMethod準備(MethodInstructionTestStub.class, "lambda");
         assertEquals(
                 "[MethodInstructionTestStub.lambda$lambda$0(Object), Stream.empty(), Stream.forEach(Consumer)]",
                 jigMethod.usingMethods().asSimpleTextSorted()
         );
 
-    }
-
-    private static JigMethod jigMethod(String anObject) {
-        var jigType = TestSupport.buildJigType(MethodInstructionTestStub.class);
-        return jigType.instanceJigMethodStream()
-                .filter(jigMethod -> jigMethod.nameAndArgumentSimpleText().equals(anObject))
-                .findAny().orElseThrow();
     }
 }

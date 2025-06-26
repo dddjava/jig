6fc3bbe6b2da64538a777922e0c78c0ffef02c0a
irof <irof.ocean@gmail.com>
2025-06-26 17:54:53 +0900

    📝

--- jig-core/src/main/java/org/dddjava/jig/domain/model/documents/stationery/JigDocumentContext.java
+++ jig-core/src/main/java/org/dddjava/jig/domain/model/documents/stationery/JigDocumentContext.java
@@ -8,6 +8,11 @@ import org.dddjava.jig.domain.model.documents.documentformat.JigDocument;
 import java.nio.file.Path;
 import java.util.List;
 
+/**
+ * ドキュメント出力時のコンテキスト
+ *
+ * ドキュメント出力の主体となるインスタンスが持っていない情報（特に用語）を提供する役割
+ */
 public interface JigDocumentContext {
 
     Term packageTerm(PackageIdentifier packageIdentifier);

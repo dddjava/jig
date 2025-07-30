b7fbac56883061214e3fc18098a858c38562e9ce
irof <irof.ocean@gmail.com>
2025-07-30 19:27:08 +0900

    Gradleのverifyタスクを廃止する。
    本バージョンではタスク自体は削除せずWARNログ出力。

--- jig-gradle-plugin/src/main/java/org/dddjava/jig/gradle/VerifyJigEnvironmentTask.java
+++ jig-gradle-plugin/src/main/java/org/dddjava/jig/gradle/VerifyJigEnvironmentTask.java
@@ -1,29 +1,13 @@
 package org.dddjava.jig.gradle;
 
-import org.dddjava.jig.adapter.diagram.DotCommandRunner;
 import org.gradle.api.DefaultTask;
-import org.gradle.api.logging.Logger;
 import org.gradle.api.tasks.TaskAction;
 
 public class VerifyJigEnvironmentTask extends DefaultTask {
 
     @TaskAction
+    @Deprecated
     void verify() {
-        try {
-            DotCommandRunner dotCommandRunner = new DotCommandRunner();
-            dotCommandRunner.verify();
-        } catch (RuntimeException e) {
-            Logger logger = getLogger();
-            logger.warn("-- JIG ERROR -----------------------------------------------");
-            logger.warn("+ 実行可能なGraphvizが見つけられませんでした。");
-            logger.warn("+ dotにPATHが通っているか確認してください。");
-            logger.warn("+ JIGは図の出力にGraphvizを使用しています。");
-            logger.warn("+ ");
-            logger.warn("+ Graphvizは以下から入手できます。");
-            logger.warn("+     https://www.graphviz.org/");
-            logger.warn("------------------------------------------------------------");
-
-            throw e;
-        }
+        getLogger().warn("verifyタスクは廃止になります。");
     }
 }

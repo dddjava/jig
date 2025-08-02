3df78672c79103161a1fbef3644b087c96d38f24
irof <irof.ocean@gmail.com>
2025-08-03 00:08:19 +0900

    inputs/outputsの定義整理

--- jig-core/src/main/java/org/dddjava/jig/domain/model/information/inputs/package-info.java
+++ jig-core/src/main/java/org/dddjava/jig/domain/model/information/inputs/package-info.java
@@ -1,6 +1,10 @@
 /**
- * 入力アダプタ
+ * 入力
  *
- * HTTP APIのエンドポイントとなるController、キューなどのListener、スケジューラーなど、アプリケーションとして処理の起点となるところ。
+ * - アダプタ: 外部からの入力を受け付け、ポートに繋げるもの。Controllerなどのクラス。
+ * - エントリーポイント: アプリケーションの起点。main、ControllerのRequestMapping、Listener、Scheduledなどのメソッド
+ *
+ * - ポート: Serviceクラス
+ * - ユースケース: メソッド
  */
 package org.dddjava.jig.domain.model.information.inputs;
--- jig-core/src/main/java/org/dddjava/jig/domain/model/information/outputs/package-info.java
+++ jig-core/src/main/java/org/dddjava/jig/domain/model/information/outputs/package-info.java
@@ -1,6 +1,10 @@
 /**
- * 出力アダプタ
+ * 出力
  *
- * データベースアクセスや外部API呼び出しなど。
+ * - アダプタ: 外部と実際やりとりする部分。Repositoryの実装クラス。
+ * - インボケーション: DBアクセスや外部API呼び出しなどを実際行うメソッド。
+ *
+ * - ポート: Repositoryインタフェース
+ * - ゲートウェイ: メソッド
  */
 package org.dddjava.jig.domain.model.information.outputs;

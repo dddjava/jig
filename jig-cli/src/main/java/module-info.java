102d21fcc1b57225014cf42f73aee275e2245538
irof <irof.ocean@gmail.com>
2025-08-05 14:40:39 +0900

    add module-info

---
+++ jig-cli/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module org.dddjava.jig.cli {
+    requires org.dddjava.jig.core;
+    requires org.jspecify;
+    requires org.slf4j;
+    requires spring.beans;
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+    requires spring.context;
+}
---
+++ jig-core/src/main/java/module-info.java
@@ -0,0 +1,19 @@
+module org.dddjava.jig.core {
+    exports org.dddjava.jig;
+    exports org.dddjava.jig.domain.model.sources;
+    exports org.dddjava.jig.infrastructure.configuration;
+    exports org.dddjava.jig.domain.model.documents.documentformat;
+
+    requires com.github.benmanes.caffeine;
+    requires com.github.javaparser.core;
+    requires java.logging;
+    requires micrometer.core;
+    requires micrometer.registry.prometheus;
+    requires org.apache.commons.compress;
+    requires org.apache.poi.ooxml;
+    requires org.apache.poi.poi;
+    requires org.mybatis;
+    requires org.objectweb.asm;
+    requires org.slf4j;
+    requires thymeleaf;
+}

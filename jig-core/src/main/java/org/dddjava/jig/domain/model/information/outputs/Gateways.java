99fad217bd302cf3d805d3ea4294b9ca3f44d156
irof <irof.ocean@gmail.com>
2025-08-10 23:44:38 +0900

    Gatewaysの前段でCollectionなのでListじゃなくす
    そのうえでlist()を実装

--- jig-core/src/main/java/org/dddjava/jig/domain/model/information/outputs/Gateways.java
+++ jig-core/src/main/java/org/dddjava/jig/domain/model/information/outputs/Gateways.java
@@ -3,6 +3,8 @@ package org.dddjava.jig.domain.model.information.outputs;
 import org.dddjava.jig.domain.model.information.members.JigMethod;
 import org.dddjava.jig.domain.model.information.members.UsingMethods;
 
+import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
 
 import static java.util.stream.Collectors.collectingAndThen;
@@ -11,11 +13,17 @@ import static java.util.stream.Collectors.toList;
 /**
  * 出力ポートのゲートウェイ
  */
-public record Gateways(List<JigMethod> list) {
+public record Gateways(Collection<JigMethod> values) {
 
     public Gateways filter(UsingMethods usingMethods) {
-        return list.stream()
+        return values.stream()
                 .filter(method -> usingMethods.invokedMethodStream().anyMatch(invokedMethod -> invokedMethod.jigMethodIdIs(method.jigMethodId())))
                 .collect(collectingAndThen(toList(), Gateways::new));
+    }
+
+    public List<JigMethod> list() {
+        return values.stream()
+                .sorted(Comparator.comparing(JigMethod::fqn))
+                .toList();
     }
 }

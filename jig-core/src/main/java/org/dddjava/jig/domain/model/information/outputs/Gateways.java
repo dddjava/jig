e4980f0c452eee488a1cdc0ec84b4b7696759494
irof <irof.ocean@gmail.com>
2025-08-10 23:39:55 +0900

    record

--- jig-core/src/main/java/org/dddjava/jig/domain/model/information/outputs/Gateways.java
+++ jig-core/src/main/java/org/dddjava/jig/domain/model/information/outputs/Gateways.java
@@ -11,21 +11,11 @@ import static java.util.stream.Collectors.toList;
 /**
  * 出力ポートのゲートウェイ
  */
-public class Gateways {
-
-    List<JigMethod> list;
-
-    Gateways(List<JigMethod> list) {
-        this.list = list;
-    }
+public record Gateways(List<JigMethod> list) {
 
     public Gateways filter(UsingMethods usingMethods) {
         return list.stream()
                 .filter(method -> usingMethods.invokedMethodStream().anyMatch(invokedMethod -> invokedMethod.jigMethodIdIs(method.jigMethodId())))
                 .collect(collectingAndThen(toList(), Gateways::new));
-    }
-
-    public List<JigMethod> list() {
-        return list;
     }
 }

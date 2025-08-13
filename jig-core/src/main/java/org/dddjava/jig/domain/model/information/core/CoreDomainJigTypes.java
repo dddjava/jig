e0112e008143be3c4e4794d0d530c6d1327a31d5
irof <irof.ocean@gmail.com>
2025-08-13 23:50:08 +0900

    📝

--- jig-core/src/main/java/org/dddjava/jig/domain/model/information/core/CoreDomainJigTypes.java
+++ jig-core/src/main/java/org/dddjava/jig/domain/model/information/core/CoreDomainJigTypes.java
@@ -2,6 +2,9 @@ package org.dddjava.jig.domain.model.information.core;
 
 import org.dddjava.jig.domain.model.information.types.JigTypes;
 
+/**
+ * CoreDomainにフィルタリングされたJigTypes
+ */
 public record CoreDomainJigTypes(JigTypes jigTypes) {
     public boolean empty() {
         return jigTypes().empty();

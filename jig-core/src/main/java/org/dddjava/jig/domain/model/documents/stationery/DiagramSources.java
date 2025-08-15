7459126c3974b82b9741d9c2dfa29df70000d04f
irof <irof.ocean@gmail.com>
2025-08-15 23:09:58 +0900

    未使用メソッド削除

--- jig-core/src/main/java/org/dddjava/jig/domain/model/documents/stationery/DiagramSources.java
+++ jig-core/src/main/java/org/dddjava/jig/domain/model/documents/stationery/DiagramSources.java
@@ -1,14 +1,9 @@
 package org.dddjava.jig.domain.model.documents.stationery;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.function.Consumer;
 
 public record DiagramSources(List<DiagramSource> list) {
-
-    public static DiagramSources empty() {
-        return new DiagramSources(Collections.emptyList());
-    }
 
     public static DiagramSources of(List<DiagramSource> diagramSources) {
         return new DiagramSources(diagramSources);

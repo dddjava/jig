1bcf2a2dffea7846160b0563a2d1e0219d15e2dc
irof <irof.ocean@gmail.com>
2025-08-29 14:25:17 +0900

    convert to record

--- jig-core/src/main/java/org/dddjava/jig/domain/model/data/packages/PackageDepth.java
+++ jig-core/src/main/java/org/dddjava/jig/domain/model/data/packages/PackageDepth.java
@@ -3,16 +3,7 @@ package org.dddjava.jig.domain.model.data.packages;
 /**
  * パッケージの深さ
  */
-public class PackageDepth {
-    int value;
-
-    public PackageDepth(int value) {
-        this.value = value;
-    }
-
-    public int value() {
-        return value;
-    }
+public record PackageDepth(int value) {
 
     @Override
     public String toString() {

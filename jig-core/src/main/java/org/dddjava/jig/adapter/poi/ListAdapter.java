d5ac1677e16c7f4721812d9e199ff73e0291aca8
irof <irof.ocean@gmail.com>
2025-09-13 21:10:15 +0900

    整理のために１ファイルに寄せておく

--- jig-core/src/main/java/org/dddjava/jig/adapter/poi/ListAdapter.java
+++ jig-core/src/main/java/org/dddjava/jig/adapter/poi/ListAdapter.java
@@ -11,7 +11,7 @@ import org.dddjava.jig.domain.model.data.types.TypeId;
 import org.dddjava.jig.domain.model.documents.documentformat.JigDocument;
 import org.dddjava.jig.domain.model.documents.stationery.JigDocumentContext;
 import org.dddjava.jig.domain.model.information.JigRepository;
-import org.dddjava.jig.domain.model.information.inputs.HttpEntrypointPath;
+import org.dddjava.jig.domain.model.information.inputs.Entrypoint;
 import org.dddjava.jig.domain.model.information.inputs.InputAdapters;
 import org.dddjava.jig.domain.model.information.members.JigMethod;
 import org.dddjava.jig.domain.model.information.relation.types.TypeRelationships;
@@ -162,7 +162,7 @@ public class ListAdapter {
                                 .sorted()
                                 .collect(STREAM_COLLECTOR)),
                         ReportItem.ofNumber("循環的複雑度", item -> item.jigMethod().instructions().cyclomaticComplexity()),
-                        ReportItem.ofString("パス", item -> HttpEntrypointPath.from(item).pathText())
+                        ReportItem.ofString("パス", item -> Entrypoint.HttpEntrypointPath.from(item).pathText())
                 ), inputAdapters.listEntrypoint()),
                 new ReportSheet<>("SERVICE", List.of(
                         ReportItem.ofString("パッケージ名", item -> item.serviceMethod().declaringType().packageId().asText()),

3bde99c83f76bc56aea1219ec01e24afb12754f6
irof <irof.ocean@gmail.com>
2025-10-01 19:18:10 +0900

    📝

--- jig-core/src/test/java/stub/domain/model/relation/field/to_primitive_constant/ConstantFieldHolder.java
+++ jig-core/src/test/java/stub/domain/model/relation/field/to_primitive_constant/ConstantFieldHolder.java
@@ -1,5 +1,10 @@
 package stub.domain.model.relation.field.to_primitive_constant;
 
+/**
+ * インライン化される参照
+ *
+ * この定数フィールドの参照はコンパイル時にインライン化されるため、このクラスへの関連は検出されない。
+ */
 public class ConstantFieldHolder {
     public static final int INT_CONSTANT = 1;
     public static final String STRING_CONSTANT = "STRING";

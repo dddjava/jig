f6d443e70b75e6b432890e3a01b698e2332e4177
irof <irof.ocean@gmail.com>
2025-10-03 21:04:32 +0900

    📝

--- jig-core/src/main/java/org/dddjava/jig/domain/model/information/relation/graph/package-info.java
+++ jig-core/src/main/java/org/dddjava/jig/domain/model/information/relation/graph/package-info.java
@@ -2,5 +2,7 @@
  * グラフ
  *
  * ノードを両端とするEdgeの集合をグラフと呼ぶ。
+ * ノードとしてパッケージ、クラス、メソッド、フィールドを想定している。
+ * 1つのグラフに含まれるノード同士は同じ型で、重複を省いたりソートしたりするために `Comparable` を実装するものとする。
  */
 package org.dddjava.jig.domain.model.information.relation.graph;

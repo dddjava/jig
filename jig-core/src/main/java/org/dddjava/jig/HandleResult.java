f9dae803f85184fab84b46cbe0133572dfc96cd0
irof <irof.ocean@gmail.com>
2025-10-14 22:00:37 +0900

    インタフェースの抽出

--- jig-core/src/main/java/org/dddjava/jig/HandleResult.java
+++ jig-core/src/main/java/org/dddjava/jig/HandleResult.java
@@ -1,93 +1,29 @@
 package org.dddjava.jig;
 
 import org.dddjava.jig.domain.model.documents.documentformat.JigDocument;
-import org.jspecify.annotations.Nullable;
 
 import java.nio.file.Path;
 import java.util.Collections;
 import java.util.List;
 
-import static java.util.stream.Collectors.joining;
-
-public class HandleResult {
-
-    private final JigDocument jigDocument;
-    private final List<Path> outputFilePaths;
-    @Nullable
-    private final String failureMessage;
-
-    private HandleResult(JigDocument jigDocument, List<Path> outputFilePaths, @Nullable String failureMessage) {
-        this.jigDocument = jigDocument;
-        this.outputFilePaths = outputFilePaths;
-        this.failureMessage = failureMessage;
+public interface HandleResult {
+    static HandleResult withException(JigDocument jigDocument, Exception e) {
+        return new HandleResultImpl(jigDocument, Collections.emptyList(), e.getMessage());
     }
 
-    public static HandleResult withException(JigDocument jigDocument, Exception e) {
-        return new HandleResult(jigDocument, Collections.emptyList(), e.getMessage());
+    static HandleResult withOutput(JigDocument jigDocument, List<Path> outputFilePaths) {
+        return new HandleResultImpl(jigDocument, outputFilePaths, outputFilePaths.isEmpty() ? "skip" : null);
     }
 
-    public static HandleResult withOutput(JigDocument jigDocument, List<Path> outputFilePaths) {
-        return new HandleResult(jigDocument, outputFilePaths, outputFilePaths.isEmpty() ? "skip" : null);
-    }
+    boolean isOutputDiagram();
 
-    public boolean isOutputDiagram() {
-        // TODO JigDocumentによって固定になっているが、実際の出力結果によって制御する
-        return switch (jigDocument()) {
-            case PackageRelationDiagram,
-                 BusinessRuleRelationDiagram,
-                 CategoryDiagram,
-                 CategoryUsageDiagram,
-                 ServiceMethodCallHierarchyDiagram -> true;
-            case ApplicationList,
-                 BusinessRuleList,
-                 DomainSummary,
-                 ApplicationSummary,
-                 UsecaseSummary,
-                 EntrypointSummary,
-                 RepositorySummary,
-                 EnumSummary,
-                 TermList,
-                 Insight,
-                 Sequence,
-                 Glossary,
-                 PackageSummary -> false;
-        };
-    }
+    boolean failure();
 
-    boolean failure() {
-        return !success()
-                // 現状、skipかどうかはfailureMessageで見るしかない
-                && !"skip".equals(failureMessage);
-    }
+    String outputFilePathsText();
 
-    public String outputFilePathsText() {
-        return outputFilePaths.stream()
-                .map(Path::toAbsolutePath)
-                .map(Path::normalize)
-                .map(Path::toString)
-                .collect(joining(", ", "[ ", " ]"));
-    }
+    boolean success();
 
-    public boolean success() {
-        return failureMessage == null;
-    }
+    JigDocument jigDocument();
 
-    public JigDocument jigDocument() {
-        return jigDocument;
-    }
-
-    public List<String> outputFileNames() {
-        return outputFilePaths.stream()
-                .map(Path::getFileName)
-                .map(Path::toString)
-                .toList();
-    }
-
-    @Override
-    public String toString() {
-        if (success()) {
-            return String.format("%s: %s", jigDocument(), outputFileNames());
-        }
-        return String.format("%s: %s", jigDocument(), failureMessage);
-    }
+    List<String> outputFileNames();
 }
---
+++ jig-core/src/main/java/org/dddjava/jig/HandleResultImpl.java
@@ -0,0 +1,90 @@
+package org.dddjava.jig;
+
+import org.dddjava.jig.domain.model.documents.documentformat.JigDocument;
+import org.jspecify.annotations.Nullable;
+
+import java.nio.file.Path;
+import java.util.List;
+
+import static java.util.stream.Collectors.joining;
+
+public class HandleResultImpl implements HandleResult {
+
+    private final JigDocument jigDocument;
+    private final List<Path> outputFilePaths;
+    @Nullable
+    private final String failureMessage;
+
+    HandleResultImpl(JigDocument jigDocument, List<Path> outputFilePaths, @Nullable String failureMessage) {
+        this.jigDocument = jigDocument;
+        this.outputFilePaths = outputFilePaths;
+        this.failureMessage = failureMessage;
+    }
+
+    @Override
+    public boolean isOutputDiagram() {
+        // TODO JigDocumentによって固定になっているが、実際の出力結果によって制御する
+        return switch (jigDocument()) {
+            case PackageRelationDiagram,
+                 BusinessRuleRelationDiagram,
+                 CategoryDiagram,
+                 CategoryUsageDiagram,
+                 ServiceMethodCallHierarchyDiagram -> true;
+            case ApplicationList,
+                 BusinessRuleList,
+                 DomainSummary,
+                 ApplicationSummary,
+                 UsecaseSummary,
+                 EntrypointSummary,
+                 RepositorySummary,
+                 EnumSummary,
+                 TermList,
+                 Insight,
+                 Sequence,
+                 Glossary,
+                 PackageSummary -> false;
+        };
+    }
+
+    @Override
+    public boolean failure() {
+        return !success()
+                // 現状、skipかどうかはfailureMessageで見るしかない
+                && !"skip".equals(failureMessage);
+    }
+
+    @Override
+    public String outputFilePathsText() {
+        return outputFilePaths.stream()
+                .map(Path::toAbsolutePath)
+                .map(Path::normalize)
+                .map(Path::toString)
+                .collect(joining(", ", "[ ", " ]"));
+    }
+
+    @Override
+    public boolean success() {
+        return failureMessage == null;
+    }
+
+    @Override
+    public JigDocument jigDocument() {
+        return jigDocument;
+    }
+
+    @Override
+    public List<String> outputFileNames() {
+        return outputFilePaths.stream()
+                .map(Path::getFileName)
+                .map(Path::toString)
+                .toList();
+    }
+
+    @Override
+    public String toString() {
+        if (success()) {
+            return String.format("%s: %s", jigDocument(), outputFileNames());
+        }
+        return String.format("%s: %s", jigDocument(), failureMessage);
+    }
+}

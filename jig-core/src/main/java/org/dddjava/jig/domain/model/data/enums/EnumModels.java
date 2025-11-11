3fe2f39ae0b9c66fbc0c17782d4e03308ace2399
irof <irof.ocean@gmail.com>
2025-11-11 22:16:54 +0900

    定義された順序を持っていないのでCollectionに変更

--- jig-core/src/main/java/org/dddjava/jig/domain/model/data/enums/EnumModels.java
+++ jig-core/src/main/java/org/dddjava/jig/domain/model/data/enums/EnumModels.java
@@ -2,17 +2,17 @@ package org.dddjava.jig.domain.model.data.enums;
 
 import org.dddjava.jig.domain.model.data.types.TypeId;
 
-import java.util.List;
+import java.util.Collection;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
  * Enum固有で取得するモデル
  */
-public record EnumModels(List<EnumModel> list) {
+public record EnumModels(Collection<EnumModel> values) {
 
     public Map<TypeId, EnumModel> toMap() {
-        return list.stream()
+        return values.stream()
                 .collect(Collectors.toMap(enumModel -> enumModel.typeId, enumModel -> enumModel));
     }
 }
--- jig-core/src/main/java/org/dddjava/jig/domain/model/sources/javasources/JavaSourceModel.java
+++ jig-core/src/main/java/org/dddjava/jig/domain/model/sources/javasources/JavaSourceModel.java
@@ -3,6 +3,7 @@ package org.dddjava.jig.domain.model.sources.javasources;
 import org.dddjava.jig.domain.model.data.enums.EnumModel;
 import org.dddjava.jig.domain.model.data.enums.EnumModels;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -11,13 +12,13 @@ import java.util.stream.Stream;
  */
 public class JavaSourceModel {
 
-    private final List<EnumModel> enumModels;
+    private final Collection<EnumModel> enumModels;
 
-    private JavaSourceModel(List<EnumModel> enumModels) {
+    private JavaSourceModel(Collection<EnumModel> enumModels) {
         this.enumModels = enumModels;
     }
 
-    public static JavaSourceModel from(List<EnumModel> enumModels) {
+    public static JavaSourceModel from(Collection<EnumModel> enumModels) {
         return new JavaSourceModel(enumModels);
     }
 

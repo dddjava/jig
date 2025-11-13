d8c423e0a935ca11f11c632455ec52e8da5d9a29
irof <irof.ocean@gmail.com>
2025-11-13 21:40:20 +0900

    record

--- jig-core/src/main/java/org/dddjava/jig/domain/model/data/rdbaccess/MyBatisStatement.java
+++ jig-core/src/main/java/org/dddjava/jig/domain/model/data/rdbaccess/MyBatisStatement.java
@@ -3,28 +3,9 @@ package org.dddjava.jig.domain.model.data.rdbaccess;
 /**
  * SQL
  */
-public class MyBatisStatement {
-
-    MyBatisStatementId myBatisStatementId;
-    Query query;
-    SqlType sqlType;
-
-    public MyBatisStatement(MyBatisStatementId myBatisStatementId, Query query, SqlType sqlType) {
-        this.myBatisStatementId = myBatisStatementId;
-        this.query = query;
-        this.sqlType = sqlType;
-    }
-
-    public SqlType sqlType() {
-        return sqlType;
-    }
+public record MyBatisStatement(MyBatisStatementId myBatisStatementId, Query query, SqlType sqlType) {
 
     public Tables tables() {
         return query.extractTable(sqlType);
-
-    }
-
-    public MyBatisStatementId myBatisStatementId() {
-        return myBatisStatementId;
     }
 }

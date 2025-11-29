21f34a911eee0c8911f80b6b50229fc2fb14086e
irof <irof.ocean@gmail.com>
2025-11-29 20:49:30 +0900

    📝

--- jig-core/src/main/java/org/dddjava/jig/HandleResult.java
+++ jig-core/src/main/java/org/dddjava/jig/HandleResult.java
@@ -5,6 +5,9 @@ import org.dddjava.jig.domain.model.documents.documentformat.JigDocument;
 import java.nio.file.Path;
 import java.util.List;
 
+/**
+ * JigDocumentごとの処理結果
+ */
 public interface HandleResult {
     static HandleResult withException(JigDocument jigDocument, Exception e) {
         return new HandleResultImpl(jigDocument, e);
@@ -14,13 +17,25 @@ public interface HandleResult {
         return new HandleResultImpl(jigDocument, outputFilePaths);
     }
 
-    boolean isOutputDiagram();
-
-    String outputFilePathsText();
-
-    boolean success();
-
     JigDocument jigDocument();
 
+    /**
+     * 出力されたものがダイアグラムか否か
+     */
+    boolean isOutputDiagram();
+
+    /**
+     * 成否
+     */
+    boolean success();
+
+    /**
+     * 出力されたファイルの絶対パスリストを結合した文字列
+     */
+    String outputFilePathsText();
+
+    /**
+     * 出力されたファイル名のリスト
+     */
     List<String> outputFileNames();
 }

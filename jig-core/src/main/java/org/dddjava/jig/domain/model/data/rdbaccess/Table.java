9b643605596009efee3255e7a2d9e8e67c501268
irof <irof.ocean@gmail.com>
2025-12-09 23:33:41 +0900

    convert to record

--- jig-core/src/main/java/org/dddjava/jig/domain/model/data/rdbaccess/Table.java
+++ jig-core/src/main/java/org/dddjava/jig/domain/model/data/rdbaccess/Table.java
@@ -3,15 +3,5 @@ package org.dddjava.jig.domain.model.data.rdbaccess;
 /**
  * テーブル
  */
-public class Table {
-
-    String value;
-
-    public Table(String value) {
-        this.value = value;
-    }
-
-    String name() {
-        return value;
-    }
+public record Table(String name) {
 }

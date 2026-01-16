0a843889d20af5f773ad4cc516efb3626c50d675
irof <irof.ocean@gmail.com>
2026-01-16 23:58:28 +0900

    動的要素をJSONで書き出していくトライの準備

--- jig-core/src/main/java/org/dddjava/jig/adapter/thymeleaf/PackageSummaryView.java
+++ jig-core/src/main/java/org/dddjava/jig/adapter/thymeleaf/PackageSummaryView.java
@@ -10,6 +10,7 @@ import java.nio.file.Path;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * パッケージ概要
@@ -27,9 +28,20 @@ public class PackageSummaryView {
     public List<Path> write(Path outputDirectory, JigPackages jigPackages) {
         JigDocumentWriter jigDocumentWriter = new JigDocumentWriter(jigDocument, outputDirectory);
 
+        var packagesJson = jigPackages.listPackage().stream()
+                .map(packageInfo -> """
+                        {"fqn": "%s", "name": "%s", "description": "%s"}
+                        """.formatted(
+                        escape(packageInfo.fqn()),
+                        escape(packageInfo.label()),
+                        escape(packageInfo.term().description())))
+                .collect(Collectors.joining(",", "[", "]"));
+
+
         Map<String, Object> contextMap = Map.of(
                 "title", jigDocumentWriter.jigDocument().label(),
-                "packages", jigPackages.listPackage()
+                "packages", jigPackages.listPackage(),
+                "packagesJson", packagesJson
         );
 
         Context context = new Context(Locale.ROOT, contextMap);
@@ -38,5 +50,9 @@ public class PackageSummaryView {
         jigDocumentWriter.writeTextAs(".html",
                 writer -> templateEngine.process(template, context, writer));
         return jigDocumentWriter.outputFilePaths();
+    }
+
+    private String escape(String string) {
+        return string.replace("\n", "\\n");
     }
 }

83ab9199a77a3827b91eeb4af9285eb1aaaa5b85
irof <irof.ocean@gmail.com>
2026-02-08 19:44:41 +0900

    refactor: 未使用関数削除
    term.htmlで使っていたもの

--- jig-core/src/main/resources/templates/assets/jig.js
+++ jig-core/src/main/resources/templates/assets/jig.js
@@ -1,51 +1,5 @@
 Array.from(document.getElementsByClassName("markdown")).forEach(x => x.innerHTML = marked.parse(x.innerHTML))
 
-function toggleTableColumn(tableId, columnIndex) {
-    var table = document.getElementById(tableId);
-    var rows = table.rows;
-
-    for (var i = 0; i < rows.length; i++) {
-        var row = rows[i];
-        var cells = row.cells;
-        var cell = cells[columnIndex];
-
-        if (cell.classList.contains("hidden")) {
-            cell.classList.remove("hidden");
-        } else {
-            cell.classList.add("hidden");
-        }
-    }
-}
-
-function filterTable(tableId, filterInputId) {
-    var table = document.getElementById(tableId);
-    var rows = table.getElementsByTagName("tr");
-    var filterText = document.getElementById(filterInputId).value;
-
-    for (var i = 1; i < rows.length; i++) {
-        var row = rows[i];
-        var cells = row.getElementsByTagName("td");
-        var match = false;
-
-        for (var j = 0; j < cells.length; j++) {
-            var cell = cells[j];
-            if (cell) {
-                var cellText = cell.textContent || cell.innerText;
-                if (cellText.indexOf(filterText) > -1) {
-                    match = true;
-                    break;
-                }
-            }
-        }
-
-        if (match) {
-            row.style.display = "";
-        } else {
-            row.style.display = "none";
-        }
-    }
-}
-
 function sortTable(event) {
     const headerColumn = event.target;
     const columnIndex = Array.from(headerColumn.parentNode.children).indexOf(headerColumn);

3c18ebc105a4aa2b87346aaaf67678e5fd53b44e
irof <irof.ocean@gmail.com>
2026-02-25 14:46:53 +0900

    docs: 出力ポートのコメントを充実

--- jig-core/src/main/java/org/dddjava/jig/domain/model/information/outputs/OutputPort.java
+++ jig-core/src/main/java/org/dddjava/jig/domain/model/information/outputs/OutputPort.java
@@ -5,11 +5,24 @@ import org.dddjava.jig.domain.model.information.types.JigType;
 import java.util.stream.Stream;
 
 /**
- * 出力ポートとなるクラス
+ * 出力ポート
+ *
+ * 通常は操作のみを定義しているRepositoryインタフェース。
+ * ポートはアダプタのことは知らない。
  */
 public record OutputPort(JigType jigType) {
+    public OutputPort {
+        // 「インタフェースである」を条件としたいが、
+        // 小さいアプリケーションではポート兼アダプタはありえるので許容。
+    }
 
+    /**
+     * ポートの操作を舐めるストリームを取得する
+     *
+     * すべてのインスタンスメソッドを操作として返しているが、それでいいのか？という疑問はある。
+     */
     public Stream<OutputPortOperation> operationStream() {
-        return jigType().instanceJigMethodStream().map(OutputPortOperation::new);
+        return jigType().instanceJigMethodStream()
+                .map(OutputPortOperation::new);
     }
 }

30951fd638a0eccaa865d96eba4510ee2f11ecbb
irof <irof.ocean@gmail.com>
2026-03-19 18:01:16 +0900

    test: SpringDataUtilTestをパラメタライズドテストに書き換え
    AGENT: Junie

--- jig-core/src/test/java/org/dddjava/jig/domain/model/information/outputs/springdata/SpringDataUtilTest.java
+++ jig-core/src/test/java/org/dddjava/jig/domain/model/information/outputs/springdata/SpringDataUtilTest.java
@@ -2,32 +2,40 @@ package org.dddjava.jig.domain.model.information.outputs.springdata;
 
 import org.dddjava.jig.domain.model.data.persistence.PersistenceOperationType;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class SpringDataUtilTest {
 
+    @ParameterizedTest
+    @CsvSource({
+            "findSomething, SELECT",
+            "readSomething, SELECT",
+            "getSomething, SELECT",
+            "querySomething, SELECT",
+            "countSomething, SELECT",
+            "existsSomething, SELECT",
+            "saveSomething, INSERT",
+            "insertSomething, INSERT",
+            "createSomething, INSERT",
+            "addSomething, INSERT",
+            "updateSomething, UPDATE",
+            "setSomething, UPDATE",
+            "deleteSomething, DELETE",
+            "removeSomething, DELETE",
+    })
+    void testInferOperationType(String methodName, PersistenceOperationType expectedType) {
+        Optional<PersistenceOperationType> actual = SpringDataUtil.inferOperationType(methodName);
+        assertEquals(expectedType, actual.orElseThrow());
+    }
+
     @Test
-    void testInferOperationType() {
-        assertEquals(PersistenceOperationType.SELECT, SpringDataUtil.inferOperationType("findSomething").get());
-        assertEquals(PersistenceOperationType.SELECT, SpringDataUtil.inferOperationType("readSomething").get());
-        assertEquals(PersistenceOperationType.SELECT, SpringDataUtil.inferOperationType("getSomething").get());
-        assertEquals(PersistenceOperationType.SELECT, SpringDataUtil.inferOperationType("querySomething").get());
-        assertEquals(PersistenceOperationType.SELECT, SpringDataUtil.inferOperationType("countSomething").get());
-        assertEquals(PersistenceOperationType.SELECT, SpringDataUtil.inferOperationType("existsSomething").get());
-
-        assertEquals(PersistenceOperationType.INSERT, SpringDataUtil.inferOperationType("saveSomething").get());
-        assertEquals(PersistenceOperationType.INSERT, SpringDataUtil.inferOperationType("insertSomething").get());
-        assertEquals(PersistenceOperationType.INSERT, SpringDataUtil.inferOperationType("createSomething").get());
-        assertEquals(PersistenceOperationType.INSERT, SpringDataUtil.inferOperationType("addSomething").get());
-
-        assertEquals(PersistenceOperationType.UPDATE, SpringDataUtil.inferOperationType("updateSomething").get());
-        assertEquals(PersistenceOperationType.UPDATE, SpringDataUtil.inferOperationType("setSomething").get());
-
-        assertEquals(PersistenceOperationType.DELETE, SpringDataUtil.inferOperationType("deleteSomething").get());
-        assertEquals(PersistenceOperationType.DELETE, SpringDataUtil.inferOperationType("removeSomething").get());
-
+    void testUnknownMethod() {
         assertTrue(SpringDataUtil.inferOperationType("unknownMethod").isEmpty());
     }
 }

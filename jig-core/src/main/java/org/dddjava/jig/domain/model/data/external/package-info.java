bff9c9f2bab0b8233dcaeb57ddf9000a41b1f543
irof <irof.ocean@gmail.com>
2026-03-19 11:21:51 +0900

    docs: add package-info.java
    AGENT: Copilot

---
+++ jig-core/src/main/java/org/dddjava/jig/domain/model/data/external/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * 外部ソースから解析したデータ
+ *
+ * JavaParser、MyBatis等の外部ツール、ライブラリから取得したデータを扱う。
+ */
+package org.dddjava.jig.domain.model.data.external;
---
+++ jig-core/src/main/java/org/dddjava/jig/domain/model/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * JIGのドメインモデル
+ *
+ * バイトコードから解析した基本的なデータと、それを意味付けした情報を扱う。
+ */
+package org.dddjava.jig.domain.model;

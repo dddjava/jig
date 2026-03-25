cd7e8fead5daeaf87e19c7bf02eb5623c48e5834
irof <irof.ocean@gmail.com>
2026-03-25 22:34:11 +0900

    refactor: 今後使っていきそうなので移動

--- jig-core/src/main/java/org/dddjava/jig/adapter/html/DomainSummaryAdapter.java
+++ jig-core/src/main/java/org/dddjava/jig/adapter/html/DomainSummaryAdapter.java
@@ -4,18 +4,13 @@ import org.dddjava.jig.adapter.HandleDocument;
 import org.dddjava.jig.adapter.JigDocumentWriter;
 import org.dddjava.jig.adapter.json.Json;
 import org.dddjava.jig.adapter.json.JsonObjectBuilder;
+import org.dddjava.jig.adapter.json.JsonSupport;
 import org.dddjava.jig.application.JigService;
 import org.dddjava.jig.domain.model.data.enums.EnumModel;
 import org.dddjava.jig.domain.model.data.enums.EnumModels;
-import org.dddjava.jig.domain.model.data.types.JigTypeArgument;
-import org.dddjava.jig.domain.model.data.types.JigTypeReference;
 import org.dddjava.jig.domain.model.documents.documentformat.JigDocument;
 import org.dddjava.jig.domain.model.documents.stationery.JigDocumentContext;
 import org.dddjava.jig.domain.model.information.JigRepository;
-import org.dddjava.jig.domain.model.information.members.JigField;
-import org.dddjava.jig.domain.model.information.members.JigMethod;
-import org.dddjava.jig.domain.model.information.relation.types.TypeRelationship;
-import org.dddjava.jig.domain.model.information.relation.types.TypeRelationships;
 import org.dddjava.jig.domain.model.information.types.JigType;
 import org.dddjava.jig.domain.model.information.types.JigTypeValueKind;
 import org.dddjava.jig.domain.model.information.types.JigTypes;
@@ -92,7 +87,7 @@ public class DomainSummaryAdapter {
 
     private JsonObjectBuilder buildTypeJson(JigType jigType, EnumModels enumModels) {
         List<JsonObjectBuilder> fields = jigType.instanceJigFields().fields().stream()
-                .map(this::buildFieldJson)
+                .map(JsonSupport::buildFieldJson)
                 .toList();
 
         List<JsonObjectBuilder> methods = jigType.instanceJigMethods()
@@ -100,7 +95,7 @@ public class DomainSummaryAdapter {
                 .excludeNotNoteworthyObjectMethod()
                 .listRemarkable()
                 .stream()
-                .map(this::buildMethodJson)
+                .map(JsonSupport::buildMethodJson)
                 .toList();
 
         List<JsonObjectBuilder> staticMethods = jigType.staticJigMethods()
@@ -108,7 +103,7 @@ public class DomainSummaryAdapter {
                 .excludeNotNoteworthyObjectMethod()
                 .listRemarkable()
                 .stream()
-                .map(this::buildMethodJson)
+                .map(JsonSupport::buildMethodJson)
                 .toList();
 
         JsonObjectBuilder builder = Json.object("fqn", jigType.fqn())
@@ -145,30 +140,4 @@ public class DomainSummaryAdapter {
                 .and("parameterNames", Json.array(parameterNames));
     }
 
-    private JsonObjectBuilder buildFieldJson(JigField field) {
-        return Json.object("name", field.nameText())
-                .and("typeRef", buildTypeRef(field.jigTypeReference()))
-                .and("isDeprecated", field.isDeprecated());
-    }
-
-    private JsonObjectBuilder buildMethodJson(JigMethod jigMethod) {
-        return Json.object("fqn", jigMethod.fqn())
-                .and("visibility", jigMethod.visibility())
-                .and("parameterTypeRefs", Json.arrayObjects(jigMethod.parameterTypeStream()
-                        .map(this::buildTypeRef)
-                        .toList()))
-                .and("returnTypeRef", buildTypeRef(jigMethod.returnType()))
-                .and("isDeprecated", jigMethod.isDeprecated());
-    }
-
-    private JsonObjectBuilder buildTypeRef(JigTypeReference jigTypeReference) {
-        var obj = Json.object("fqn", jigTypeReference.fqn());
-        // 型引数がない場合は fqn だけのオブジェクトにする
-        if (jigTypeReference.typeArgumentList().isEmpty()) return obj;
-
-        return obj.and("typeArgumentRefs", Json.arrayObjects(jigTypeReference.typeArgumentList().stream()
-                .map(JigTypeArgument::jigTypeReference)
-                .map(this::buildTypeRef)
-                .toList()));
-    }
 }
--- jig-core/src/main/java/org/dddjava/jig/adapter/json/JsonSupport.java
+++ jig-core/src/main/java/org/dddjava/jig/adapter/json/JsonSupport.java
@@ -1,5 +1,10 @@
 package org.dddjava.jig.adapter.json;
 
+import org.dddjava.jig.domain.model.data.types.JigTypeArgument;
+import org.dddjava.jig.domain.model.data.types.JigTypeReference;
+import org.dddjava.jig.domain.model.information.members.JigField;
+import org.dddjava.jig.domain.model.information.members.JigMethod;
+
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -50,5 +55,32 @@ public final class JsonSupport {
         return map.entrySet().stream()
                 .map(e -> "\"%s\":%s".formatted(escape(e.getKey()), e.getValue()))
                 .collect(Collectors.joining(",", "{", "}"));
+    }
+
+    public static JsonObjectBuilder buildFieldJson(JigField field) {
+        return Json.object("name", field.nameText())
+                .and("typeRef", buildTypeRef(field.jigTypeReference()))
+                .and("isDeprecated", field.isDeprecated());
+    }
+
+    public static JsonObjectBuilder buildMethodJson(JigMethod jigMethod) {
+        return Json.object("fqn", jigMethod.fqn())
+                .and("visibility", jigMethod.visibility())
+                .and("parameterTypeRefs", Json.arrayObjects(jigMethod.parameterTypeStream()
+                        .map(JsonSupport::buildTypeRef)
+                        .toList()))
+                .and("returnTypeRef", buildTypeRef(jigMethod.returnType()))
+                .and("isDeprecated", jigMethod.isDeprecated());
+    }
+
+    private static JsonObjectBuilder buildTypeRef(JigTypeReference jigTypeReference) {
+        var obj = Json.object("fqn", jigTypeReference.fqn());
+        // 型引数がない場合は fqn だけのオブジェクトにする
+        if (jigTypeReference.typeArgumentList().isEmpty()) return obj;
+
+        return obj.and("typeArgumentRefs", Json.arrayObjects(jigTypeReference.typeArgumentList().stream()
+                .map(JigTypeArgument::jigTypeReference)
+                .map(JsonSupport::buildTypeRef)
+                .toList()));
     }
 }

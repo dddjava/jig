ab622eb851b154b9074821fe09e22e64f323bb19
irof <irof.ocean@gmail.com>
2026-04-03 11:20:54 +0900

    refactor: 廃止したExcel出力のメソッドなどを除去

--- jig-core/src/main/java/org/dddjava/jig/adapter/JigDocumentWriter.java
+++ jig-core/src/main/java/org/dddjava/jig/adapter/JigDocumentWriter.java
@@ -1,21 +1,19 @@
 package org.dddjava.jig.adapter;
 
 import org.dddjava.jig.domain.model.documents.documentformat.JigDocument;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import java.io.*;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.UncheckedIOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import java.util.function.BiConsumer;
-import java.util.function.Consumer;
 
 public class JigDocumentWriter {
-    private static final Logger logger = LoggerFactory.getLogger(JigDocumentWriter.class);
 
     JigDocument jigDocument;
     Path outputDirectory;
@@ -26,25 +24,6 @@ public class JigDocumentWriter {
         this.outputDirectory = outputDirectory;
     }
 
-    public void writeXlsx(OutputStreamWriter writer) {
-        String fileName = jigDocument.fileName() + ".xlsx";
-        write(writer, fileName);
-    }
-
-    public void writeTextAs(String extension, Consumer<Writer> consumer) {
-        String fileName = jigDocument.fileName() + extension;
-        Path outputFilePath = outputDirectory.resolve(fileName);
-        try (OutputStream out = Files.newOutputStream(outputFilePath);
-             OutputStream outputStream = new BufferedOutputStream(out);
-             Writer writer = new java.io.OutputStreamWriter(outputStream, StandardCharsets.UTF_8)
-        ) {
-            consumer.accept(writer);
-        } catch (IOException e) {
-            throw new UncheckedIOException(e);
-        }
-        writtenDocuments.add(outputFilePath);
-    }
-
     public void write(OutputStreamWriter writer, String fileName) {
         Path outputFilePath = outputDirectory.resolve(fileName);
         try (OutputStream outputStream = new BufferedOutputStream(Files.newOutputStream(outputFilePath))) {
@@ -53,14 +32,6 @@ public class JigDocumentWriter {
             throw new UncheckedIOException(e);
         }
         writtenDocuments.add(outputFilePath);
-    }
-
-    public void writePath(BiConsumer<Path, List<Path>> biConsumer) {
-        biConsumer.accept(outputDirectory, writtenDocuments);
-    }
-
-    public void markSkip() {
-        logger.info("出力対象がないため {} をスキップしました。", jigDocument);
     }
 
     public List<Path> outputFilePaths() {

44553034bd4045ff6ec5e5756def235d2f5dc8f3
irof <irof.ocean@gmail.com>
2026-04-14 15:59:57 +0900

    refactor: シンボルは出力都合なので除去

--- jig-core/src/main/java/org/dddjava/jig/domain/model/data/members/JigMemberVisibility.java
+++ jig-core/src/main/java/org/dddjava/jig/domain/model/data/members/JigMemberVisibility.java
@@ -4,16 +4,10 @@ package org.dddjava.jig.domain.model.data.members;
  * メンバの可視性
  */
 public enum JigMemberVisibility {
-    PUBLIC("+"),
-    PROTECTED("#"),
-    PACKAGE("~"),
-    PRIVATE("-");
-
-    final String symbol;
-
-    JigMemberVisibility(String symbol) {
-        this.symbol = symbol;
-    }
+    PUBLIC,
+    PROTECTED,
+    PACKAGE,
+    PRIVATE;
 
     public boolean isPublic() {
         return this == PUBLIC;
